032b1a388629694075e1827fc649fdf80433546e
Per Nordlöw <per.nordlow@gmail.com>
2016-04-12 12:43:23 +0200

    Better comment for hybridSort

--- src/sortn.d
+++ src/sortn.d
@@ -122,7 +122,8 @@ body
     return s.assumeSorted;
 }
 
-/** Hybrid sort `r`.
+/** Hybrid sort `r` using sortUpTo for if length of `r` is less than or equal to
+    `sortUpToMaxLength` and `std.algorithm.sorting.sort` otherwise.
  */
 auto hybridSort(alias less = "a < b", Range)(Range r) // TODO uint or size_t?
     if (isRandomAccessRange!Range)

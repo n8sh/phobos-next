f1de36ddf693f55cc01952d99adf021831272211
Per Nordlöw <per.nordlow@gmail.com>
2016-04-15 21:26:48 +0200

    Add stub

--- src/trie.d
+++ src/trie.d
@@ -4,7 +4,7 @@
  */
 module trie;
 
-import std.traits : isIntegral, isSomeString;
+import std.traits : isIntegral, isSomeString, allSatisfy;
 import std.range : ElementType;
 
 enum isTrieableElementType(T) = isIntegral!T || isSomeChar!T;
@@ -12,15 +12,29 @@ enum isTrieType(T) = (isTrieableElementType ||
                       (isArray!T &&
                        isTrieableElementType!(ElementType!T)));
 
-/** Radix Tree.
+/** Radix Tree storing keys of types `Keys`.
     See also: https://en.wikipedia.org/wiki/Radix_tree
  */
-struct RadixTree(Value = void, Keys...)
+struct RadixTree(Value, Keys...)
+    if (allSatisfy!(isTrieType, Keys))
 {
+}
+alias RadixTrie = RadixTree;
+alias CompactPrefixTree = RadixTree;
 
-    bool contains(T) const;
+/// Instantiator.
+auto radixTreeSet(Value, Keys...)(Keys keys)
+{
+    return RadixTree!(Value, Keys)(keys);
+}
+
+/// Instantiator.
+auto radixTreeSet(Keys...)(Keys key)
+{
+    return RadixTree!(void, Keys)(keys);
 }
 
 @safe pure nothrow unittest
 {
+    auto rt = radixTreeSet!(uint)();
 }

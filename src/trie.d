277de305f2924245b0e637060e4cc6f580a1b3d9
Per Nordlöw <per.nordlow@gmail.com>
2016-04-17 22:27:56 +0200

    Next try

--- src/trie.d
+++ src/trie.d
@@ -8,10 +8,12 @@
 module trie;
 
 import std.meta : AliasSeq;
-import std.traits : isIntegral, isSomeChar, isSomeString, isArray, allSatisfy, anySatisfy;
+import std.traits : isIntegral, isSomeChar, isSomeString, isScalarType, isArray, allSatisfy, anySatisfy;
 import std.range : isInputRange, ElementType;
 
-enum isFixedTrieableKeyType(T) = isIntegral!T || isSomeChar!T;
+import dbg : dln;
+
+enum isFixedTrieableKeyType(T) = isScalarType!T;
 
 enum isTrieableKey(T) = (isFixedTrieableKeyType!T ||
                          (isInputRange!T &&
@@ -38,11 +40,13 @@ enum NodePacking
 struct RadixTree(Key, Value)
     if (allSatisfy!(isTrieableKey, Key))
 {
+    import bitop_ex : UnsignedOfSameSizeAs;
     enum isSet = is(Value == void);
     enum hasValue = !isSet;
 
     enum R = 4;        // radix in number of bits, typically either 1, 2, 4 or 8
     enum M = 2^^R;     // branch-multiplicity, typically either 2, 4, 16 or 256
+    enum partMask = M - 1;
 
     /// `true` if tree has fixed depth.
     enum isFixed = isFixedTrieableKeyType!Key;
@@ -50,16 +54,38 @@ struct RadixTree(Key, Value)
     /// `true` if tree has binary branch.
     enum isBinary = R == 2;
 
+    alias Branch = typeof(root);
+
+    @trusted:
+
     static if (isSet)
     {
         void insert(Key key)
         {
-            auto current = _root;
+            makeRoot;
+
+            auto current = root;
             enum maxDepth = 8*Key.sizeof / R;
-            foreach (i; iota!(0, maxDepth)) // static
+
+            foreach (chunkIx; iota!(0, maxDepth)) // foreach chunk
             {
-                enum bitShift = i*R;
-                pragma(msg, bitShift);
+                enum last = chunkIx == maxDepth - 1;
+                enum bitShift = chunkIx*R;
+                const u = *(cast(UnsignedOfSameSizeAs!Key*)(&key)); // TODO functionize and reuse here and in intsort.d
+                const uint partValue = (u >> bitShift) & partMask;
+                // dln(Key.stringof, " key = ", key, "; chunkIx:", chunkIx, "; partValue:", partValue);
+
+                static if (last) // the last iteration
+                {
+                }
+                else
+                {
+                    if (current.nexts[partValue] is null)
+                    {
+                        current.nexts[partValue] = new Branch;
+                    }
+                    current = current.nexts[partValue];
+                }
             }
         }
         bool contains(Key key) const
@@ -71,7 +97,7 @@ struct RadixTree(Key, Value)
     {
         void insert(Key key, Value value)
         {
-            auto current = _root;
+            makeRoot;
         }
         Value contains(Key key) const
         {
@@ -79,7 +105,12 @@ struct RadixTree(Key, Value)
         }
     }
 
-    private BranchNode!(M, Value)* _root;
+    void makeRoot()
+    {
+        if (root is null) { root = new Branch; }
+    }
+
+    private BranchNode!(M, Value) root;
 }
 alias RadixTrie = RadixTree;
 alias CompactPrefixTree = RadixTree;
@@ -114,20 +145,25 @@ auto radixTreeSet(Key)() { return RadixTree!(Key, void)(); }
     }
 }
 
-/** Non-Bottom (Leaf) Node referencing sub-`BranchNode`s or `LeafNode`s. */
-struct BranchNode(size_t M, Value = void)
+/** Non-bottom branch node referencing sub-`BranchNode`s or `LeafNode`s. */
+private class BranchNode(size_t M, Value = void)
 {
     /// Indicates that only child at this index is occupied.
-    static BranchNode!M* oneSet = cast(BranchNode!M*)1;
+    // TODO make this work: static auto oneSet = cast(typeof(this))(cast(void*)1UL);
 
     /// Indicates that all children are occupied (typically only for fixed-sized types).
-    static BranchNode!M* allSet = cast(BranchNode!M*)size_t.max;
+    // TODO make this work: static auto allSet = cast(BranchNode!M)cast(void*)size_t.max;
 
-    BranchNode!M*[M] nexts;
+    BranchNode!M[M] nexts;
+
+    static if (!is(Value == void))
+    {
+        Value value;
+    }
 }
 
-/** Bottom-Most Leaf Node optionnally storing `Value`. */
-struct LeafNode(Value = void)
+/** Bottom-most leaf node optionally storing `Value`. */
+private struct LeafNode(Value = void)
 {
     static if (!is(Value == void))
     {

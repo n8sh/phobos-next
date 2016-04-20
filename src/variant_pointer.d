872545e08f3e4a4f27480359dd21142f3d908ccd
Per Nordlöw <per.nordlow@gmail.com>
2016-04-20 15:15:38 +0200

    First working version

--- src/variant_pointer.d
+++ src/variant_pointer.d
@@ -1,5 +1,10 @@
 module variant_pointer;
 
+version(unittest)
+{
+    import dbg : dln;
+}
+
 /** A variant pointer to either of `Types`.
 
     Realizes a very lightweight version of polymorphism packed inside one single
@@ -8,59 +13,98 @@ module variant_pointer;
  */
 struct VariantPointer(Types...)
 {
-    enum maxCount = (void*).alignof; // lookup pointer alignment
+    alias S = size_t;
     private enum N = Types.length; // useful local shorthand
+
+    enum typeBits = 8;               // number of bits used to represent type
+    enum maxTypeCount = 2^^typeBits; // maximum of different types
+    enum typeShift = 8*S.sizeof - typeBits;
+    enum typeMask = cast(S)(maxTypeCount - 1) << typeShift;
 
     import std.meta : staticIndexOf;
     enum tixOf(T) = staticIndexOf!(T, Types); // TODO cast to ubyte if N is <= 256
 
     enum bool allows(T) = tixOf!T >= 0;
-    static assert(Types.length <= maxCount, "Can only represent 8 different types");
-    union
-    {
-        void* _raw;
-    }
+    static assert(N <= maxTypeCount, "Can only represent 8 different types");
+
     static assert(this.sizeof == (void*).sizeof); // should have same size as pointer
 
-    this(T)(T* value) @safe @nogc nothrow
-        if (allows!T)
-    in
+    extern (D) S toHash() const @trusted pure nothrow
     {
-        assert((cast(size_t)value & (maxCount-1)) == 0);
+        import core.internal.hash : hashOf;
+        return _raw.hashOf;
     }
-    body
+
+    pure nothrow @nogc:
+
+    this(T)(T* value)
+        if (allows!T)
     {
         init(value);
     }
 
-    auto opAssign(T)(T* that) @trusted @nogc nothrow
+    auto opAssign(T)(T* that)
         if (allows!T)
-    in
-    {
-        assert((cast(size_t)that & (maxCount-1)) == 0);
-    }
-    body
     {
         init(that);
         return this;
     }
 
-    private void init(T)(T that) @trusted @nogc nothrow
+    private void init(T)(T* that)
+    in
     {
-        _raw = cast(void*)that;
-        // _tix = cast(Ix)tixOf!U; // set type tag
+        assert(!(cast(S)that & typeMask));
     }
+    body
+    {
+        _raw = (cast(S)that | // pointer in lower part
+                (cast(S)(tixOf!T) << typeShift)); // use higher bits for type information
+    }
+
+    private bool isOfType(T)() const nothrow @nogc
+    {
+        return ((_raw & typeMask) >> typeShift) == tixOf!T;
+    }
+
+    @property inout(T)* peek(T)() inout @trusted @nogc nothrow
+    {
+        static if (!is(T == void))
+            static assert(allows!T, "Cannot store a " ~ T.stringof ~ " in a " ~ name);
+        if (!isOfType!T) return null;
+        return cast(inout T*)(cast(S)_raw & ~typeMask);
+    }
+
+    private S _raw;
 }
 
 pure nothrow unittest
 {
-    alias VP = VariantPointer!(byte, short, int, long,
-                               float, double, real, char);
-    VP x = cast(byte*)16;
-    x = cast(short*)32;
+    import std.meta : AliasSeq;
 
-    // TODO how to check this?
-    // import std.exception : assertThrown;
-    // assertThrown(x = cast(short*)1);
-    // assertThrown(x = cast(short*)15);
+    alias Types = AliasSeq!(byte, short, int, long,
+                            float, double, real, char);
+
+    alias VP = VariantPointer!Types;
+
+    VP x;
+
+    foreach (T; Types)
+    {
+        T a = 73;
+
+        x = &a;
+
+        foreach (U; Types)
+        {
+            static if (is(T == U))
+            {
+                assert(x.peek!U);
+                assert(*(x.peek!U) == a);
+            }
+            else
+            {
+                assert(!x.peek!U);
+            }
+        }
+    }
 }

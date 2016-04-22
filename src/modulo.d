e3db5c0b3e3afec18359f2fb25c05754bbe81079
Per Nordlöw <per.nordlow@gmail.com>
2016-04-22 20:19:40 +0200

    First try

---
+++ src/modulo.d
@@ -0,0 +1,32 @@
+module modulo;                  // haha ;)
+
+/** Module type within inclusive value range `(0 .. m-1)`.
+
+    Similar to Ada's modulo type `0 mod m`.
+ */
+struct Mod(size_t m, T = uint)
+{
+    static assert(m <= 2^^(8*T.sizeof));
+
+    this(T value)
+    in
+    {
+        assert(value < m); // TODO use enforce instead?
+    }
+    body
+    {
+        this._value = value;
+    }
+
+    private T _value;
+}
+
+///
+unittest
+{
+    Mod!(8, ubyte) ub8_6 = 6;
+    Mod!(8, ubyte) ub8_7 = 7;
+
+    Mod!(8, uint) ui8 = 7;
+    Mod!(256, ubyte) ub256 = 255;
+}

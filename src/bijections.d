7bddadea5baed679cd239873b1d3160ea3cfd126
Per Nordlöw <per.nordlow@gmail.com>
2016-05-05 12:14:06 +0200

    Add test

--- src/bijections.d
+++ src/bijections.d
@@ -2,7 +2,7 @@ module bijections;
 
 import std.meta : AliasSeq, staticIndexOf, Unqual;
 
-import std.traits : isUnsigned, isSigned, isIntegral, Unsigned, Signed, isNumeric;
+import std.traits : isUnsigned, isSigned, isIntegral, Unsigned, Signed, isNumeric, isSomeChar;
 
 /** List of types that are bijectable to builtin integral types. */
 alias IntegralBijectableTypes = AliasSeq!(char, wchar, dchar,
@@ -39,7 +39,7 @@ auto bijectToUnsigned(T)(T a) @trusted pure nothrow
 {
     auto bijectToUnsigned(T)(T a, bool descending)
     {
-        immutable ua = bijectToUnsigned(a);
+        immutable ua = a.bijectToUnsigned;
         return descending ? ua.max-ua : ua;
     }
 
@@ -70,6 +70,32 @@ auto bijectToUnsigned(T)(T a) @trusted pure nothrow
     @safe pure nothrow uint   iff(uint f) { return f ^            (((f >> (32-1)) - 1) | 0x80000000); }
     @safe pure nothrow ulong  ff(ulong f) { return f ^ (-cast(long) (f >> (64-1))      | 0x8000000000000000); }
     @safe pure nothrow ulong iff(ulong f) { return f ^            (((f >> (64-1)) - 1) | 0x8000000000000000); }
+
+    @trusted pure nothrow void bijectFromUnsigned(ubyte a, ref char b) { b = *cast(typeof(b)*)(&a); }
+    @trusted pure nothrow void bijectFromUnsigned(ushort a, ref wchar b) { b = *cast(typeof(b)*)(&a); }
+    @trusted pure nothrow void bijectFromUnsigned(ulong a, ref dchar b) { b = *cast(typeof(b)*)(&a); }
+
     @trusted pure nothrow void bijectFromUnsigned(uint a,  ref float  b) { uint  t = iff(a); b = *cast(float*)(&t); }
     @trusted pure nothrow void bijectFromUnsigned(ulong a, ref double b) { ulong t = iff(a); b = *cast(double*)(&t); }
+}
+
+@safe @nogc pure nothrow unittest
+{
+    const n = 1_000_000;
+    import std.range : iota;
+    foreach (const i; 0.iota(n))
+    {
+        foreach (T; AliasSeq!(char, wchar, dchar,
+                              ubyte, ushort, uint, ulong,
+                              float, double))
+        {
+            const T x = cast(T)i;
+            const y = x.bijectToUnsigned;
+            // pragma(msg, "T:", T);
+            // pragma(msg, "typeof(x):", typeof(x));
+            // pragma(msg, "typeof(y):", typeof(y));
+            T z; y.bijectFromUnsigned(z);
+            assert(x == z);
+        }
+    }
 }

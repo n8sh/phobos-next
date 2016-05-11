a373f2deae13b8bb62f73e938f1ad3ec68839aac
Per Nordlöw <per.nordlow@gmail.com>
2016-05-11 16:49:14 +0200

    Add see also

--- src/traits_ex.d
+++ src/traits_ex.d
@@ -723,6 +723,7 @@ alias enumConstants = enumMembers;
     TODO implement with an array and a bitset indiciating if it has been found yet or not
 
     See also: http://forum.dlang.org/post/ziappmtvucmuefphblse@forum.dlang.org
+    See also: http://forum.dlang.org/post/awihyvzjswwayeqtklly@forum.dlang.org
     See also: http://forum.dlang.org/thread/bspwlfypfishykezzocx@forum.dlang.org#post-dguqnroxbfewerepomwq:40forum.dlang.org
 */
 auto uniqueEnumMembers(T)()

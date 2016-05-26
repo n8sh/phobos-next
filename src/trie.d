73859f6d668760acce9ab33a38c6f25bbda686f4
Per Nordlöw <per.nordlow@gmail.com>
2016-05-26 10:57:15 +0200

    Add comment

--- src/trie.d
+++ src/trie.d
@@ -1261,7 +1261,7 @@ static private Key!span remapKey(TypedKey, uint span = 8)(in TypedKey typedKey)
             }
         }
 
-        return key.dup; // TODO avoid allocation this
+        return key.dup; // TODO avoid this GC-allocation
     }
     else static if (is(Unqual!TypedKey == string))
     {

f81bd1170aaa14597d3601a4bb5c7b2899f2a760
Per Nordlöw <per.nordlow@gmail.com>
2016-06-27 22:03:19 +0200

    Remove TODO

--- src/trie.d
+++ src/trie.d
@@ -5,11 +5,6 @@
 
     TODO Add Branch-hint allocation flag and re-benchmark construction of radixTreeSet with 10000000 uints
 
-    TODO Move
-    - local structs
-    - `alias Node(Value = void)`
-    out of `RawRadixTree`.
-
     TODO Add sortedness to `IxsN` and make `IxsN.contains()` use `binarySearch()`. Make use of `sortn`.
 
     TODO Make the GC aware of all Value scalars and arrays:

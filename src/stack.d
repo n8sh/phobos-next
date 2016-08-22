f510e729c3d9df8f8f65f28da81fac3d5e097554
Per Nordlöw <per.nordlow@gmail.com>
2016-08-22 11:38:43 +0200

    Add tests

--- src/stack.d
+++ src/stack.d
@@ -7,6 +7,7 @@ struct Stack(T)
     import std.array: Appender, appender;
 
     ref inout(T) top() inout { return _app.data[$ - 1]; };
+    alias back = top;
 
     bool empty() const { return _app.data.length == 0; }
 
@@ -17,9 +18,31 @@ struct Stack(T)
     void pop()
     {
         assert(!empty);
-        _app.shrinkTo(_app.data.length - 1);
+        try
+        {
+            _app.shrinkTo(_app.data.length - 1);
+        }
+        catch (Exception e)
+        {
+            assert(false, "Assertion was thrown");
+        }
     }
 
     private Appender!(T[]) _app; // TODO replace with array_ex.d
     alias _app this;
+}
+
+@safe pure nothrow /* TODO @nogc */ unittest
+{
+    alias T = int;
+
+    Stack!T s;
+    assert(s.empty);
+
+    s.push(13);
+    assert(!s.empty);
+    assert(s.top == 13);
+
+    s.pop();
+    assert(s.empty);
 }

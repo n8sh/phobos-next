5ea20f114e96b5f879027ed2070c8c84b5731893
Per Nordlöw <per.nordlow@gmail.com>
2016-08-22 12:25:05 +0200

    Fix

--- src/stack.d
+++ src/stack.d
@@ -9,25 +9,30 @@ struct Stack(T)
     ref inout(T) back() inout { return _store[$ - 1]; };
     alias top = back;
 
-    /** Push element `t`. */
+    /** Push element `t` to stack. */
     void pushBack(in T t) @safe { _store.pushBack(t); }
 
-    /** Pop element. */
+    /** Pop element from stack. */
     void popBack()
     {
         assert(!empty);
-        try
-        {
-            _store.popBack;
-        }
-        catch (Exception e)
-        {
-            assert(false, "Assertion was thrown");
-        }
+        _store.popBack;
     }
 
-    private Array!(T, Ordering.unsorted, false) _store;
-    alias _store this;
+    /** Pop back element and return it. */
+    T backPop()
+    {
+        assert(!empty);
+        T value = back;
+        _store.popBack;
+        return value;
+    }
+
+    /** Returns: `true` if stack is empty `false` otherwise. */
+    bool empty() const @safe pure nothrow @nogc { return _store.empty; }
+
+private:
+    Array!(T, Ordering.unsorted, false) _store;
 }
 
 @safe pure nothrow /* TODO @nogc */ unittest
@@ -58,5 +63,25 @@ struct Stack(T)
     assert(s.back == 13);
 
     s.popBack();
+    assert(s.empty);
+
+    // backPop
+
+    s.pushBack(13);
+    assert(!s.empty);
+    assert(s.back == 13);
+
+    s.pushBack(14);
+    assert(!s.empty);
+    assert(s.back == 14);
+
+    s.pushBack(15);
+    assert(!s.empty);
+    assert(s.back == 15);
+
+    assert(s.backPop() == 15);
+    assert(s.backPop() == 14);
+    assert(s.backPop() == 13);
+
     assert(s.empty);
 }

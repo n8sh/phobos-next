2cd64a3dcfaa7f8cb01d39fb108b6b521acc293b
Per Nordlöw <per.nordlow@gmail.com>
2016-08-23 13:44:22 +0200

    Fix

---
+++ src/test_trie.d
@@ -0,0 +1,21 @@
+#!/usr/bin/env rdmd
+
+import std.algorithm, std.stdio;
+import trie;
+
+void main(string[] args)
+{
+    alias Key = int;
+
+    auto set = radixTreeSet!(Key);
+
+    set.insert(42);
+    assert(set.contains(42));
+
+    foreach (e; set[])
+    {
+        writeln(e);
+    }
+
+    assert(set[].equal([42]));
+}

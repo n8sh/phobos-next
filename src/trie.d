ae90ef11e0e6bf49f40282266a83b91d41e72382
Per Nordlöw <per.nordlow@gmail.com>
2016-08-23 15:10:44 +0200

    Comment out stuff

--- src/trie.d
+++ src/trie.d
@@ -4293,6 +4293,75 @@ auto radixTreeMap(Key, Value)()
     return RadixTree!(MutableKey!Key, Value)(false);
 }
 
+auto testScalar(uint span, Keys...)()
+    if (Keys.length >= 1)
+{
+    import std.range : iota;
+    foreach (const it; 0.iota(1))
+    {
+        foreach (Key; Keys)
+        {
+            auto set = radixTreeSet!(Key);
+
+            static if (isIntegral!Key)
+            {
+                const low = max(Key.min, -100_000);
+                const high = min(Key.max, 100_000);
+            }
+            else static if (isFloatingPoint!Key)
+            {
+                const low = -100_000;
+                const high = 100_000;
+            }
+            else static if (is(Key == bool))
+            {
+                const low = false;
+                const high = true;
+            }
+
+            foreach (const uk; low.iota(high + 1))
+            {
+                const Key key = cast(Key)uk;
+                assert(set.insert(key));  // insert new value returns `true` (previously not in set)
+                assert(!set.insert(key)); // reinsert same value returns `false` (already in set)
+            }
+
+            // static if (!is(Key == float) &&
+            //            !is(Key == double) &&
+            //            !is(Key == long))
+            // {
+            //     dln("Key:", Key.stringof);
+            //     size_t i = 0;
+            //     foreach (const e; set[])
+            //     {
+            //         import std.conv : to;
+            //         const expected = i + low;
+            //         if (e != expected)
+            //         {
+            //             dln("Failed: value:", e, " expected:", expected, " i:", i, " low:", low);
+            //         }
+            //         // assert(e == low + i, "Failed for " ~ i.to!string);
+            //         ++i;
+            //     }
+
+            //     // import std.algorithm.comparison : equal;
+            //     // import std.algorithm : map;
+            //     // assert(set[].equal(low.iota(high + 1).map!(uk => cast(Key)uk)));
+            // }
+        }
+    }
+}
+
+///
+@safe pure nothrow /* TODO @nogc */ unittest
+{
+    testScalar!(8,
+                bool,
+                double, float,
+                long, int, short, byte,
+                ulong, uint, ushort, ubyte);
+}
+
 ///
 @safe pure nothrow /* TODO @nogc */ unittest
 {
@@ -4956,52 +5025,6 @@ void benchmark()()
                       long, int, short, byte,
                       ulong, uint, ushort, ubyte);
     }
-}
-
-auto testScalar(uint span, Keys...)()
-    if (Keys.length >= 1)
-{
-    import std.range : iota;
-    foreach (const it; 0.iota(1))
-    {
-        foreach (Key; Keys)
-        {
-            auto set = radixTreeSet!(Key);
-
-            static if (isIntegral!Key)
-            {
-                const low = max(Key.min, -100_000);
-                const high = min(Key.max, 100_000);
-            }
-            else static if (isFloatingPoint!Key)
-            {
-                const low = -100_000;
-                const high = 100_000;
-            }
-            else static if (is(Key == bool))
-            {
-                const low = false;
-                const high = true;
-            }
-
-            foreach (const uk; low.iota(high + 1))
-            {
-                const Key key = cast(Key)uk;
-                assert(set.insert(key));  // insert new value returns `true` (previously not in set)
-                assert(!set.insert(key)); // reinsert same value returns `false` (already in set)
-            }
-        }
-    }
-}
-
-///
-@safe pure nothrow /* TODO @nogc */ unittest
-{
-    testScalar!(8,
-                bool,
-                double, float,
-                long, int, short, byte,
-                ulong, uint, ushort, ubyte);
 }
 
 /** Static Iota.

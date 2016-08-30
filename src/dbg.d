e098cbd362b95c3dc6ffa93f86346e21298ae2ae
Per Nordlöw <per.nordlow@gmail.com>
2016-08-30 19:46:53 +0200

    Fix

--- src/dbg.d
+++ src/dbg.d
@@ -70,41 +70,41 @@ version(show) @safe pure nothrow @nogc unittest
     dln("x:", " ", 12);
 }
 
-// /** Show the symbol name and variable of $(D Args).
-//     See also: http://forum.dlang.org/thread/yczwqrbkxdiqijtiynrh@forum.dlang.org?page=1
-//  */
-// template show(Args...)
-//     if (Args.length >= 1)
-// {
-//     void show(string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__)
-//     {
-//         import std.stdio: write, writeln;
-//         try
-//         {
-//             debug write(file, ":",line, ":" /* , ": in ",fun */, " debug: ");
-//             foreach (const i, Args; Args)
-//             {
-//                 if (i) debug write(", "); // separator
-//                 debug write(Args[i].stringof, ":", Args);
-//             }
-//             debug writeln();
-//         }
-//         catch (Exception) { }
-//     }
-// }
+/** Show the symbol name and variable of $(D Args).
+    See also: http://forum.dlang.org/thread/yczwqrbkxdiqijtiynrh@forum.dlang.org?page=1
+ */
+template show(Args...)
+    if (Args.length >= 1)
+{
+    void show(string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__)
+    {
+        import std.stdio: write, writeln;
+        try
+        {
+            debug write(file, ":",line, ":" /* , ": in ",fun */, " debug: ");
+            foreach (const i, Arg; Args)
+            {
+                if (i) debug write(", "); // separator
+                debug write(Args[i].stringof, ":", Arg);
+            }
+            debug writeln();
+        }
+        catch (Exception) { }
+    }
+}
 
-// version(show) unittest
-// {
-//     int x = 11;
-//     int y = 12;
-//     int z = 13;
-//     show!x;
-//     show!y;
-//     show!z;
-// }
+version(show) unittest
+{
+    int x = 11;
+    int y = 12;
+    int z = 13;
+    show!x;
+    show!y;
+    show!z;
+}
 
-// version(show) unittest
-// {
-//     int x = 11, y = 12, z = 13;
-//     show!(x, y, z);
-// }
+version(show) unittest
+{
+    int x = 11, y = 12, z = 13;
+    show!(x, y, z);
+}

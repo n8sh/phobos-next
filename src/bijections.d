ac7c4d192c6eefd381bc95aa43bec720f38eb5c9
Per Nordlöw <per.nordlow@gmail.com>
2016-08-31 14:01:05 +0200

    Fix

--- src/bijections.d
+++ src/bijections.d
@@ -23,8 +23,10 @@ alias IntegralBijectableTypes = AliasSeq!(bool, char, wchar, dchar,
 
 enum isIntegralBijectableType(T) = staticIndexOf!(Unqual!T, IntegralBijectableTypes) >= 0;
 
+pragma(inline) @safe pure nothrow @nogc:
+
 /** Biject (Shift) Signed $(D a) "up" to Unsigned (before radix sorting). */
-auto bijectToUnsigned(T)(T a) @trusted pure nothrow
+auto bijectToUnsigned(T)(T a) @trusted
     if (isIntegralBijectableType!T)
 {
     alias UT = Unqual!T;
@@ -48,60 +50,57 @@ auto bijectToUnsigned(T)(T a) @trusted pure nothrow
             static assert(false, "Unsupported integral input type " ~ UT.stringof);
         }
     }
-    else static if (is(UT == float))  { return ff(*cast(uint*)(&a)); }
-    else static if (is(UT == double)) { return ff(*cast(ulong*)(&a)); }
+    else static if (is(UT == float))  { return *cast(uint*)(&a); }
+    else static if (is(UT == double)) { return *cast(ulong*)(&a); }
     else static assert(false, "Unsupported input type " ~ UT.stringof);
 }
 
-@safe @nogc pure nothrow
+auto bijectToUnsigned(T)(T a, bool descending)
 {
-    auto bijectToUnsigned(T)(T a, bool descending)
-    {
-        immutable ua = a.bijectToUnsigned;
-        return descending ? ua.max-ua : ua;
-    }
-
-    void bijectFromUnsigned(U)(U a, ref U b)
-        if (isUnsigned!U)
-    {
-        b = a;                  ///< Identity.
-    }
-
-    /** Biject (Shift) Unsigned  $(D a) "back down" to Signed (after radix sorting). */
-    void bijectFromUnsigned(U, V)(U a, ref V b)
-        if (isUnsigned!U &&
-            isIntegral!V && isSigned!V &&
-            is(U == Unsigned!V))
-    {
-        b = a - (cast(Unsigned!U)1 << (8*U.sizeof - 1)); // "add down""
-    }
-
-    /** Map a Floating Point Number \p a Back from Radix Sorting
-     * (Inverse of \c radix_flip_float()).
-     * - if sign is 1 (negative), it flips the sign bit back
-     * - if sign is 0 (positive), it flips all bits back
-     */
-
-    /** Map Bits of Floating Point Number \p a to Unsigned Integer that can be Radix Sorted.
-     * Also finds \em sign of \p a.
-     * - if it's 1 (negative float), it flips all bits.
-     * - if it's 0 (positive float), it flips the sign only.
-     */
-    uint    ff(uint f) { return f ^ (-cast(uint)  (f >> (32-1))      | 0x80000000); }
-    uint   iff(uint f) { return f ^             (((f >> (32-1)) - 1) | 0x80000000); }
-    ulong  ff(ulong f) { return f ^ (-cast(ulong) (f >> (64-1))      | 0x8000000000000000); }
-    ulong iff(ulong f) { return f ^             (((f >> (64-1)) - 1) | 0x8000000000000000); }
-
-    @trusted void bijectFromUnsigned(ubyte  a, ref  bool b) { b = *cast(typeof(b)*)(&a); }
-    @trusted void bijectFromUnsigned(ubyte  a, ref  char b) { b = *cast(typeof(b)*)(&a); }
-    @trusted void bijectFromUnsigned(ushort a, ref wchar b) { b = *cast(typeof(b)*)(&a); }
-    @trusted void bijectFromUnsigned(ulong  a, ref dchar b) { b = *cast(typeof(b)*)(&a); }
-
-    @trusted void bijectFromUnsigned(uint  a, ref float  b) { uint  t = iff(a); b = *cast(float*)(&t); }
-    @trusted void bijectFromUnsigned(ulong a, ref double b) { ulong t = iff(a); b = *cast(double*)(&t); }
+    immutable ua = a.bijectToUnsigned;
+    return descending ? ua.max-ua : ua;
 }
 
-@safe @nogc pure nothrow unittest
+void bijectFromUnsigned(U)(U a, ref U b)
+    if (isUnsigned!U)
+{
+    b = a;                  ///< Identity.
+}
+
+/** Biject (Shift) Unsigned  $(D a) "back down" to Signed (after radix sorting). */
+void bijectFromUnsigned(U, V)(U a, ref V b)
+    if (isUnsigned!U &&
+        isIntegral!V && isSigned!V &&
+        is(U == Unsigned!V))
+{
+    b = a - (cast(Unsigned!U)1 << (8*U.sizeof - 1)); // "add down""
+}
+
+/** Map a Floating Point Number \p a Back from Radix Sorting
+ * (Inverse of \c radix_flip_float()).
+ * - if sign is 1 (negative), it flips the sign bit back
+ * - if sign is 0 (positive), it flips all bits back
+ */
+
+/** Map Bits of Floating Point Number \p a to Unsigned Integer that can be Radix Sorted.
+ * Also finds \em sign of \p a.
+ * - if it's 1 (negative float), it flips all bits.
+ * - if it's 0 (positive float), it flips the sign only.
+ */
+uint    ff(uint f) { return f ^ (-cast(uint)  (f >> (32-1))      | 0x80000000); }
+uint   iff(uint f) { return f ^             (((f >> (32-1)) - 1) | 0x80000000); }
+ulong  ff(ulong f) { return f ^ (-cast(ulong) (f >> (64-1))      | 0x8000000000000000); }
+ulong iff(ulong f) { return f ^             (((f >> (64-1)) - 1) | 0x8000000000000000); }
+
+@trusted void bijectFromUnsigned(ubyte  a, ref  bool b) { b = *cast(typeof(b)*)(&a); }
+@trusted void bijectFromUnsigned(ubyte  a, ref  char b) { b = *cast(typeof(b)*)(&a); }
+@trusted void bijectFromUnsigned(ushort a, ref wchar b) { b = *cast(typeof(b)*)(&a); }
+@trusted void bijectFromUnsigned(ulong  a, ref dchar b) { b = *cast(typeof(b)*)(&a); }
+
+@trusted void bijectFromUnsigned(uint  a, ref float  b) { /* uint  t = iff(a); */ b = *cast(float*)(&a); }
+@trusted void bijectFromUnsigned(ulong a, ref double b) { /* ulong t = iff(a); */ b = *cast(double*)(&a); }
+
+@safe pure nothrow @nogc unittest
 {
     foreach (T; AliasSeq!(ubyte, ushort, uint, ulong))
     {

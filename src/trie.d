3d9ed9a01d5c1dc55b496e78aaba49017b1d3b01
Per Nordlöw <per.nordlow@gmail.com>
2016-08-31 12:33:22 +0200

    Fix

--- src/trie.d
+++ src/trie.d
@@ -87,6 +87,8 @@
     TODO opBinaryRight shall return `_rawTree.ElementRef` instead of `bool`
 
     TODO Use scope on members that return key and value reference when DIP-1000 has been implemented
+
+    TODO Make all Node-members insert.*At.*(Node|Branch) free functions to
 */
 module trie;
 

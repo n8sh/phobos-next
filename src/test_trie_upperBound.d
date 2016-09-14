3d9887a1deb6544697ce3391303c105ed7d91229
Per Nordlöw <per.nordlow@gmail.com>
2016-09-15 00:36:59 +0200

    Fix

--- src/test_trie_upperBound.d
+++ src/test_trie_upperBound.d
@@ -10,12 +10,16 @@ void main(string[] args)
     auto set = radixTreeSet!(Key);
 
     set.clear();
+
     set.insert(1);
     set.insert(2);
-    set.insert(3);
+    set.insert(3);              // limit
     set.insert(4);
     set.insert(5);
-    const Key[2] expected = [4, 5];
+    set.insert(6);
+    set.insert(7);
+
+    const Key[4] expected = [4, 5, 6, 7];
     assert(set.upperBound(3)
               .equal(expected[]));
 }

75c65161b729fd61685b1807a2e9986888301275
Per Nordlöw <per.nordlow@gmail.com>
2016-09-20 12:04:42 +0200

    Fix

---
+++ src/test_trie_refs.d
@@ -0,0 +1,20 @@
+#!/usr/bin/env rdmd
+
+import std.algorithm.comparison : equal;
+import dbgio : dln;
+import std.stdio : writeln;
+import trie : RadixTreeSetGrowOnly;
+
+alias Tree = RadixTreeSetGrowOnly!string;
+
+// void recursiveTest(Tree visits = Tree.init, size_t maxCount = 0)
+// {
+//     recursiveTest(visits, ++maxCount);
+// }
+
+void main(string[] args)
+{
+    Tree t;
+    auto t2 = t.dup;
+    // recursiveTest();
+}

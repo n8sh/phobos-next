650e9dbc4310404c347bcc490398e3fec2213af1
Per Nordlöw <per.nordlow@gmail.com>
2016-10-02 23:22:28 +0200

    Fix

---
+++ src/hacks.d
@@ -0,0 +1,24 @@
+module hacks;
+
+import std.traits : isFunctionPointer, isDelegate, functionAttributes, FunctionAttribute, SetFunctionAttributes, functionLinkage;
+
+/// Return `T` assumed to be `pure`.
+auto assumePure(T)(T t)
+    if (isFunctionPointer!T || isDelegate!T)
+{
+    enum attrs = functionAttributes!T | FunctionAttribute.pure_;
+    return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs)) t;
+}
+
+int f(int x)
+{
+    return x + 1;
+}
+
+void g() pure
+{
+    // static assert(!__traits(compiles, { auto x = f(42); }));
+    // alias puref = assumePure!(typeof(&f));
+    // pragma(msg, typeof(puref));
+    // auto x = (*puref)(42);
+}

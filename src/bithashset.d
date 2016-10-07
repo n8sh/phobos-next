6cc7f20b0f1200ce427b4e370a4ced61f1ef2f54
Per Nordlöw <per.nordlow@gmail.com>
2016-10-07 10:53:05 +0200

    Better API

--- src/bithashset.d
+++ src/bithashset.d
@@ -26,15 +26,16 @@ struct BitHashSet(E, Growable growable = Growable.no)
     /// Construct set to store at most `length` number of bits.
     this(size_t length) @trusted
     {
-        _length = length;
         _blocksPtr = null;
         static if (growable == Growable.yes)
         {
+            _length = length;
             _capacity = 0;
             assureCapacity(length);
         }
         else
         {
+            _capacity = length;
             _blocksPtr = cast(Block*)calloc(blockCount, Block.sizeof);
         }
     }
@@ -50,7 +51,11 @@ struct BitHashSet(E, Growable growable = Growable.no)
     typeof(this) dup() @trusted
     {
         typeof(this) copy;
-        copy._length = _length;
+        static if (growable == Growable.yes)
+        {
+            copy._length = this._length;
+        }
+        copy._capacity = this._capacity;
         copy._blocksPtr = cast(Block*)malloc(blockCount * Block.sizeof);
         copy._blocksPtr[0 .. blockCount] = this._blocksPtr[0 .. blockCount];
         return copy;
@@ -81,7 +86,7 @@ struct BitHashSet(E, Growable growable = Growable.no)
     bool insert(E e) @trusted
     {
         const ix = cast(size_t)e;
-        static if (growable == Growable.yes) { assureCapacity(ix + 1); _length = ix + 1; } else { assert(ix < _length); }
+        static if (growable == Growable.yes) { assureCapacity(ix + 1); _length = ix + 1; } else { assert(ix < _capacity); }
         return bts(_blocksPtr, ix) != 0;
     }
     alias put = insert;         // OutputRange compatibility
@@ -92,7 +97,7 @@ struct BitHashSet(E, Growable growable = Growable.no)
     bool remove(E e) @trusted
     {
         const ix = cast(size_t)e;
-        static if (growable == Growable.yes) { assureCapacity(ix + 1); _length = ix + 1; } else { assert(ix < _length); }
+        static if (growable == Growable.yes) { assureCapacity(ix + 1); _length = ix + 1; } else { assert(ix < _capacity); }
         return btr(_blocksPtr, ix) != 0;
     }
 
@@ -102,7 +107,7 @@ struct BitHashSet(E, Growable growable = Growable.no)
     bool complement(E e) @trusted
     {
         const ix = cast(size_t)e;
-        static if (growable == Growable.yes) { assureCapacity(ix + 1); _length = ix + 1; } else { assert(ix < _length); }
+        static if (growable == Growable.yes) { assureCapacity(ix + 1); _length = ix + 1; } else { assert(ix < _capacity); }
         return btc(_blocksPtr, ix) != 0;
     }
 
@@ -110,7 +115,14 @@ struct BitHashSet(E, Growable growable = Growable.no)
     bool contains(E e) @trusted const
     {
         const ix = cast(size_t)e;
-        return ix < _length && bt(_blocksPtr, ix) != 0;
+        static if (growable == Growable.yes)
+        {
+            return ix < _length && bt(_blocksPtr, ix) != 0;
+        }
+        else
+        {
+            return ix < _capacity && bt(_blocksPtr, ix) != 0;
+        }
     }
 
     /// ditto
@@ -120,40 +132,28 @@ struct BitHashSet(E, Growable growable = Growable.no)
         return contains(e);
     }
 
-    /// Get length.
-    @property size_t length() const // TODO should we count number of elements here instead?
+    /// Get capacity.
+    @property size_t capacity() const
     {
-        return _length;
+        return _capacity;
     }
 
 private:
-    static if (growable == Growable.yes)
-    {
-        @property size_t capacity() const { return _capacity; }
-    }
-
     @property size_t blockCount() const
     {
-        static if (growable == Growable.yes)
-        {
-            return _capacity / Block.sizeof + (_capacity % Block.sizeof ? 1 : 0);
-        }
-        else
-        {
-            return _length / Block.sizeof + (_length % Block.sizeof ? 1 : 0);
-        }
+        return _capacity / Block.sizeof + (_capacity % Block.sizeof ? 1 : 0);
     }
 
     alias Block = size_t;       /// Allocated block type.
     Block* _blocksPtr;          /// Pointer to blocks of bits.
     static if (growable == Growable.yes)
     {
-        size_t _length;             /// Offset + 1 of highest set bit.
-        size_t _capacity;           /// Number of bits allocated.
+        size_t _length;         /// Offset + 1 of highest set bit.
+        size_t _capacity;       /// Number of bits allocated.
     }
     else
     {
-        size_t _length;             /// Number of bits stored.
+        size_t _capacity;       /// Number of bits allocated.
     }
 }
 
@@ -167,12 +167,12 @@ private:
     static assert(isOutputRange!(Set, E));
 
     const set0 = Set();
-    assert(set0.length == 0);
+    assert(set0.capacity == 0);
 
     const length = 2^^6;
     auto set = BitHashSet!E(2*length);
     const y = set.dup;
-    assert(y.length == 2*length);
+    assert(y.capacity == 2*length);
 
     foreach (ix; 0 .. length)
     {
@@ -221,7 +221,7 @@ private:
     alias E = uint;
 
     auto set = BitHashSet!(E, Growable.yes)();
-    assert(set.length == 0);
+    assert(set._length == 0);
 
     const length = 2^^16;
     foreach (ix; 0 .. length)
@@ -253,11 +253,11 @@ nothrow @nogc unittest          // TODO @safe pure when https://github.com/dlang
 
     RefCounted!(BitHashSet!(E, Growable.yes)) set;
 
-    assert(set.length == 0);
+    assert(set._length == 0);
     assert(set.capacity == 0);
 
     assert(!set.insert(0));
-    assert(set.length == 1);
+    assert(set._length == 1);
     assert(set.capacity == 2);
 
     const y = set;
@@ -265,11 +265,11 @@ nothrow @nogc unittest          // TODO @safe pure when https://github.com/dlang
     foreach (const e; 1 .. 1000)
     {
         assert(!set.insert(e));
-        assert(set.length == e + 1);
-        assert(y.length == e + 1);
+        assert(set._length == e + 1);
+        assert(y._length == e + 1);
     }
 
     const set1 = RefCounted!(BitHashSet!(E, Growable.yes))(42);
-    assert(set1.length == 42);
+    assert(set1._length == 42);
     assert(set1.capacity == 64);
 }

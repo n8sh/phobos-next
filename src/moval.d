2d171cc430217e3c446f70a61705ab3cb3ef85a0
Per Nordlöw <per.nordlow@gmail.com>
2016-10-10 13:55:58 +0200

    Add isRvalue

--- src/moval.d
+++ src/moval.d
@@ -1,5 +1,28 @@
 module moval;
 
+/** Is `true` if `A` is an l-value, `false` otherwise.
+    See also: https://forum.dlang.org/post/mailman.4192.1454351296.22025.digitalmars-d-learn@puremagic.com
+    TODO Add to Phobos
+*/
+enum isLvalue(alias A) = is(typeof((ref _){}(A)));
+
+/** Is `true` if `A` is an l-value, `false` otherwise.
+ */
+enum isRvalue(alias A) = !isLvalue!A;
+
+unittest
+{
+    int i;
+    string s;
+    static assert(isLvalue!i);
+    static assert(isLvalue!s);
+    static assert(!isLvalue!13);
+    static assert(!isLvalue!"a");
+}
+
+/** Move contents of `e` into an r-value and return it.
+    See also http://forum.dlang.org/post/lxfwyxtgfosdtsdjwgwz@forum.dlang.org
+*/
 E movedToRvalue(E)(ref E e)
 {
     import std.algorithm.mutation : move;

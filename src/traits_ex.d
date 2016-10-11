41605329365cd0b5beb461cc8d2aa120feba27be
Per Nordlow <per.nordlow@autoliv.com>
2016-10-11 13:47:20 +0200

    Add isLvalue and isRvalue

--- src/traits_ex.d
+++ src/traits_ex.d
@@ -946,3 +946,23 @@ template EntropyBitsOf(T)
 {
     static assert(EntropyBitsOf!int == 8*int.sizeof);
 }
+
+/** Is `true` if `A` is an l-value, `false` otherwise.
+    See also: https://forum.dlang.org/post/mailman.4192.1454351296.22025.digitalmars-d-learn@puremagic.com
+    TODO Add to Phobos
+*/
+enum isLvalue(alias A) = is(typeof((ref _){}(A)));
+
+/** Is `true` if `A` is an l-value, `false` otherwise.
+ */
+enum isRvalue(alias A) = !isLvalue!A;
+
+unittest
+{
+    int i;
+    string s;
+    static assert(isLvalue!i);
+    static assert(isLvalue!s);
+    static assert(!isLvalue!13);
+    static assert(!isLvalue!"a");
+}

f28b50681aced07e0433ab482f1b8c4be5478629
Per Nordlow <per.nordlow@autoliv.com>
2016-10-11 13:46:14 +0200

    Remove movedToRvalue

--- src/moval.d
+++ src/moval.d
@@ -19,34 +19,3 @@ unittest
     static assert(!isLvalue!13);
     static assert(!isLvalue!"a");
 }
-
-/** Move contents of `e` into an r-value and return it.
-    See also: http://forum.dlang.org/post/lxfwyxtgfosdtsdjwgwz@forum.dlang.org
-*/
-E movedToRvalue(E)(ref E e)
-{
-    import std.algorithm.mutation : move;
-    E value;
-    move(e, value);             // this can be optimized
-    return value;
-}
-
-@safe pure nothrow @nogc:
-
-struct S
-{
-    @disable this(this);
-    int x;
-}
-
-void consume(S)
-{
-}
-
-unittest
-{
-    auto s = S(13);
-    static assert(!__traits(compiles, { consume(s); }));
-    static assert(__traits(compiles, { consume(S(14)); })); // TODO optimize
-    consume(s.movedToRvalue());
-}

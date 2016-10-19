2eef1425c26536479b4a2b4a0443792b448f9fc3
Per Nordlöw <per.nordlow@gmail.com>
2016-10-19 08:40:28 +0200

    Fix

--- src/trie.d
+++ src/trie.d
@@ -31,6 +31,8 @@
     See_Also: $(HTTP en.wikipedia.org/wiki/Radix_tree)
     See_Also: $(HTTP github.com/nordlow/phobos-next/blob/master/src/test_trie_prefix.d) for a descriptive usage of prefixed access.
 
+    TODO Make member functions such as lowerBound free-functions to reduce compilation times.
+
     TODO Use scope on `Range`, `RawRange` and members that return key and value reference when DIP-1000 has been implemented
 
     TODO Encode and `string` with zero-terminating 0 byte

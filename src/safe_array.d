fd4b3f25e9039e31fa20457402ee193ec3ac1cf9
Per Nordlöw <per.nordlow@gmail.com>
2016-11-03 23:49:18 +0100

    Fix

--- src/safe_array.d
+++
@@ -1,7 +0,0 @@
-module safe_array;
-
-import array_ex : UncopyableArray;
-import borrown : Owned;
-
-alias SafeUncopyableArray(E, bool useGCAllocation = false) = Owned!(UncopyableArray!(E, useGCAllocation));
-alias SafeCopyableArray(E, bool useGCAllocation = false) = Owned!(CopyableArray!(E, useGCAllocation));

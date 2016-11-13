5f40deecd3d28cbc7641a1d46f0af6a8741d5e99
Per Nordlöw <per.nordlow@gmail.com>
2016-11-13 21:39:20 +0100

    New

---
+++ src/permutations.d
@@ -0,0 +1,107 @@
+#!/usr/bin/env rdmd-dev-module
+
+module permutations;
+
+import std.range.primitives: isInputRange;
+
+/**
+   See also: http://forum.dlang.org/thread/mailman.1434.1339436657.24740.digitalmars-d-learn@puremagic.com#post-uftixibnvfffugjwsdbl:40forum.dlang.org
+ */
+struct CartesianPower(bool doCopy = true, T)
+{
+    T[] items;
+    ulong repeat;
+    T[] row;
+    ulong i, maxN;
+
+    this(T[] items_, in uint repeat_, T[] buffer) pure nothrow @safe @nogc
+    {
+        this.items = items_;
+        this.repeat = repeat_;
+        row = buffer[0 .. repeat];
+        row[] = items[0];
+        maxN = items.length ^^ repeat;
+    }
+
+    static if (doCopy)
+    {
+        @property T[] front() pure nothrow @safe @nogc
+        {
+            return row.dup;
+        }
+    }
+    else
+    {
+        @property T[] front() pure nothrow @safe @nogc
+        {
+            return row;
+        }
+    }
+
+    @property bool empty() pure nothrow @safe @nogc
+    {
+        return i >= maxN;
+    }
+
+    void popFront() pure nothrow @safe @nogc
+    {
+        i++;
+        if (empty)
+            return;
+        ulong n = i;
+        size_t count = repeat - 1;
+        while (n)
+        {
+            row[count] = items[n % items.length];
+            count--;
+            n /= items.length;
+        }
+    }
+}
+
+static assert(isInputRange!(typeof([1, 2].cartesianPower!false(4))));
+
+auto cartesianPower(bool doCopy = true, T)(T[] items, in uint repeat)
+    pure nothrow @safe {
+    return CartesianPower!(doCopy, T)(items, repeat, new T[repeat]);
+}
+
+auto cartesianPower(bool doCopy = true, T)(T[] items, in uint repeat, T[] buffer)
+    pure nothrow @safe @nogc
+{
+    if (buffer.length >= repeat)
+    {
+        return CartesianPower!(doCopy, T)(items, repeat, buffer);
+    }
+    else
+    {
+        // Is this correct in presence of chaining?
+        static immutable err = new Error("buffer.length < repeat");
+        throw err;
+    }
+}
+
+@safe pure nothrow unittest
+{
+    int[2] items = [1, 2];
+    const n = 4;
+    int[n] buf;
+    import std.algorithm.comparison : equal;
+    assert(items.cartesianPower!false(n, buf)
+                .equal([[1, 1, 1, 1],
+                        [1, 1, 1, 2],
+                        [1, 1, 2, 1],
+                        [1, 1, 2, 2],
+                        [1, 2, 1, 1],
+                        [1, 2, 1, 2],
+                        [1, 2, 2, 1],
+                        [1, 2, 2, 2],
+                        [2, 1, 1, 1],
+                        [2, 1, 1, 2],
+                        [2, 1, 2, 1],
+                        [2, 1, 2, 2],
+                        [2, 2, 1, 1],
+                        [2, 2, 1, 2],
+                        [2, 2, 2, 1],
+                        [2, 2, 2, 2]]));
+}

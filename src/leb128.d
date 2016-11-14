6a0e52096cceddfa355c097240ccdfa7031dc495
Per Nordlöw <per.nordlow@gmail.com>
2016-11-14 14:02:40 +0100

    Better

--- src/leb128.d
+++ src/leb128.d
@@ -5,9 +5,10 @@ module leb128;
 
 import std.range : isOutputRange;
 import std.traits : isUnsigned, isSigned;
+import std.meta : Unqual;
 
-/// Encode a SLEB128 value to `os`.
-void encodeSLEB128(Output, SInt)(ref Output os, SInt value)
+/// Encode a LEB128-encoded value of signed integer type `SInt` to `os`.
+void encodeLEB128(SInt, Output)(ref Output os, Unqual!SInt value)
     if (isOutputRange!(Output, ubyte) &&
         isSigned!SInt)
 {
@@ -26,11 +27,11 @@ void encodeSLEB128(Output, SInt)(ref Output os, SInt value)
     while (more);
 }
 
-/// Decode a SLEB128 value.
-long decodeSLEB128(ubyte *p, uint *n = null)
+/// Decode a LEB128-encoded value of signed integer type `SInt`.
+SInt decodeLEB128(SInt)(ubyte *p, uint *n = null)
 {
     const ubyte *orig_p = p;
-    long value = 0;
+    SInt value = 0;
     uint shift = 0;
     ubyte byte_;
     do
@@ -41,6 +42,7 @@ long decodeSLEB128(ubyte *p, uint *n = null)
     } while (byte_ >= 128);
     // sign extend negative numbers
     if (byte_ & 0x40)
+        // TODO Unsigned!SInt
         value |= (cast(ulong)-1) << shift; // value |= (-1ULL) << shift;
     if (n)
         *n = cast(uint)(p - orig_p);
@@ -60,22 +62,24 @@ version(unittest)
 
 @safe pure nothrow unittest
 {
+    alias SInt = long;
     foreach (immutable i; 0 .. 64)
     {
         Raw os;
-        os.encodeSLEB128(i);
+        os.encodeLEB128!SInt(i);
         assert(os.data.equal([i]));
+        // const value = os.data.decodeLEB128!SInt();
     }
     foreach (immutable i; 64 .. 128)
     {
         Raw os;
-        os.encodeSLEB128(i);
+        os.encodeLEB128!SInt(i);
         assert(os.data.equal([128 + i, 0]));
     }
 }
 
 /// Encode a ULEB128 value to `os`.
-void encodeULEB128(Output, UInt)(ref Output os, UInt value)
+void encodeULEB128(UInt, Output)(ref Output os, Unqual!UInt value)
     if (isOutputRange!(Output, ubyte) &&
         isUnsigned!UInt)
 {
@@ -109,28 +113,29 @@ ulong decodeULEB128(ubyte *p, uint *n = null)
 
 @safe pure nothrow unittest
 {
+    alias UInt = ulong;
     foreach (immutable i; 0 .. 128)
     {
         Raw os;
-        os.encodeULEB128(i);
+        os.encodeULEB128!UInt(i);
         assert(os.data.equal([i]));
     }
     foreach (immutable i; 128 .. 256)
     {
         Raw os;
-        os.encodeULEB128(i);
+        os.encodeULEB128!UInt(i);
         assert(os.data.equal([i, 1]));
     }
     foreach (immutable i; 256 .. 256 + 128)
     {
         Raw os;
-        os.encodeULEB128(i);
+        os.encodeULEB128!UInt(i);
         assert(os.data.equal([i - 128, 2]));
     }
     foreach (immutable i; 256 + 128 .. 512)
     {
         Raw os;
-        os.encodeULEB128(i);
+        os.encodeULEB128!UInt(i);
         assert(os.data.equal([i - 256, 3]));
     }
 }

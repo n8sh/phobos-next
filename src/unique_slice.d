e61336894dc37dd99ceb5208a4f36a41b6ff6703
Per Nordlöw <per.nordlow@gmail.com>
2016-12-19 12:40:49 +0100

    First working

--- src/unique_slice.d
+++ src/unique_slice.d
@@ -11,6 +11,16 @@ struct UniqueSlice(Source)
 
     @disable this(this);
 
+    this(Source source) @trusted
+    {
+        import std.algorithm.mutation : move;
+        _source = move(source); // TODO remove `move` when compiler does it for us
+        _slice = source[];
+    }
+
+    alias _slice this;
+
+    Slice _slice;
     Source _source; // typically a non-reference count container type with disable copy construction
 }
 
@@ -23,9 +33,13 @@ pragma(inline) UniqueSlice!Source intoSlice(Source)(Source source)
     return typeof(return)(move(source)); // TODO remove `move` when compiler does it for us
 }
 
+///
 nothrow @nogc unittest
 {
     import array_ex : SA = UncopyableArray;
     alias C = SA!int;
     auto cs = C.withElements(1, 3, 5, 7).intoSlice;
+    foreach (e; cs)
+    {
+    }
 }

c132ef547d7f78e543544ba5154517f4ace2665b
Per Nordlöw <per.nordlow@gmail.com>
2016-12-21 13:09:49 +0100

    Add E

--- src/eager_algorithms.d
+++ src/eager_algorithms.d
@@ -57,23 +57,24 @@ pure nothrow @nogc unittest
     import unique_range : intoUniqueRange;
     import array_ex : UncopyableArray;
     alias A = UncopyableArray!int;
+    alias E = int;
 
     // empty case
-    immutable int[0] c0 = [];
+    immutable E[0] c0 = [];
     assert(A()
             .filteredInplace!(_ => _ & 1)
             .intoUniqueRange()
             .equal(c0[]));
 
     // odd elements
-    immutable int[6] c1 = [3, 11, 13, 15, 17, 19];
+    immutable E[6] c1 = [3, 11, 13, 15, 17, 19];
     assert(A.withElements(3, 11, 12, 13, 14, 15, 16, 17, 18, 19)
             .filteredInplace!(_ => _ & 1)
             .intoUniqueRange()
             .equal(c1[]));
 
     // elements less than or equal to limit
-    immutable int[6] c2 = [3, 11, 12, 13, 14, 15];
+    immutable E[6] c2 = [3, 11, 12, 13, 14, 15];
     assert(A.withElements(3, 11, 12, 13, 14, 15, 16, 17, 18, 19)
             .filteredInplace!(_ => _ <= 15)
             .intoUniqueRange()

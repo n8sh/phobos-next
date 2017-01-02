4e2524963932d21bb3a6651fcdbe19001321945b
Per Nordlöw <per.nordlow@gmail.com>
2017-01-02 16:49:31 +0100

    Fix

--- src/array_ex.d
+++ src/array_ex.d
@@ -86,7 +86,7 @@ import container_traits : ContainerElementType;
 
 import std.traits : isInstanceOf;
 
-/// Is `true` iff `C` is an instance of `Array`.
+/// Is `true` iff `C` is an instance of an `Array` container.
 enum isArrayContainer(C) = isInstanceOf!(Array, C);
 
 static if (__VERSION__ >= 2072)

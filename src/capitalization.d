5a6c8b8e07880303e4b3b0d982503fb6275785f4
Per Nordlöw <per.nordlow@gmail.com>
2017-04-14 18:17:41 +0200

    Move stuff to capitalization

---
+++ src/capitalization.d
@@ -0,0 +1,114 @@
+module capitalization;
+
+import std.traits : isSomeString;
+
+/** Check if $(D s) starts with a capital letter followed by a lower
+    letter. */
+bool isCapitalizedEasy(S)(S s)
+    if (isSomeString!S)
+{
+    import std.range.primitives : empty, front, popFront;
+    import std.uni : isUpper, isLower;
+
+    if (s.empty) { return false; }
+
+    const firstUpper = s.front.isUpper;
+    if (!firstUpper) return false;
+    s.popFront();
+
+    if (s.empty) { return false; }
+    return s.front.isLower;
+}
+
+@safe pure unittest
+{
+    assert(!`A`.isCapitalizedEasy);
+    assert(!`a`.isCapitalizedEasy);
+    assert(!`alpha`.isCapitalizedEasy);
+    assert(!`ALPHA`.isCapitalizedEasy);
+    assert(!`aThing`.isCapitalizedEasy);
+    assert(`Alpha`.isCapitalizedEasy);
+    assert(`Jack London`.isCapitalizedEasy);
+}
+
+/** Check if $(D s) starts with a capital letter followed by only lower
+    letters. */
+bool isCapitalized(S)(S s)
+    if (isSomeString!S)
+{
+    import std.range.primitives : empty, front, popFront;
+    import std.ascii : isDigit;
+    import std.uni : isUpper, isLower;
+
+    if (s.empty) { return false; }
+
+    const firstDigit = s.front.isDigit;
+    const firstUpper = s.front.isUpper;
+    if (!(firstDigit ||
+          firstUpper))
+        return false;
+
+    s.popFront();
+
+    import std.algorithm.searching : all;
+
+    if (s.empty)
+        return firstDigit;
+    else
+        return s.all!(x => (x.isDigit ||
+                            x.isLower));
+}
+
+@safe pure unittest
+{
+    assert(!``.isCapitalized);
+    assert(!`alpha`.isCapitalized);
+    assert(!`ALPHA`.isCapitalized);
+    assert(!`aThing`.isCapitalized);
+    assert(`Alpha`.isCapitalized);
+    assert(!`Jack London`.isCapitalized);
+}
+
+/** Return `true` if `s` has proper name-style capitalization, such as in `Alpha Centauri`.
+ */
+bool isNameCapitalized(S)(S s)
+    if (isSomeString!S)
+{
+    import std.algorithm.iteration : splitter;
+    import std.algorithm.searching : all;
+    import std.algorithm.comparison : among;
+    import std.ascii : isWhite;
+    import std.range : enumerate;
+    import std.uni : isUpper;
+    return s.splitter!(s => (s.isWhite || s.among!('-') != 0))
+            .enumerate
+            .all!(x => ((x.index >= 1 &&
+                         (x.value.all!(x => x.isUpper) || // Henry II
+                          x.value.among!(`of`, `upon`))) ||
+                        x.value.isCapitalized)); // TODO add enumerate and all middle word to be a preposition
+}
+
+@safe pure unittest
+{
+    assert(!`alpha`.isNameCapitalized);
+    assert(!`alpha centauri`.isNameCapitalized);
+    assert(!`ALPHA`.isNameCapitalized);
+    assert(!`ALPHA CENTAURI`.isNameCapitalized);
+    assert(!`aThing`.isNameCapitalized);
+    assert(`Alpha`.isNameCapitalized);
+    assert(`Alpha Centauri`.isNameCapitalized);
+    assert(`11104 Airion`.isNameCapitalized);
+    assert(`New York City`.isNameCapitalized);
+    assert(`1-Hexanol`.isNameCapitalized);
+    assert(`11-Hexanol`.isNameCapitalized);
+    assert(`22nd Army`.isNameCapitalized);
+    assert(!`22nd army`.isNameCapitalized);
+    assert(`2nd World War`.isNameCapitalized);
+    assert(`Second World War`.isNameCapitalized);
+    assert(`Värmland`.isNameCapitalized);
+    assert(!`The big sky`.isNameCapitalized);
+    assert(`Suur-London`.isNameCapitalized);
+    assert(`Kingdom of Sweden`.isNameCapitalized);
+    assert(`Stratford upon Avon`.isNameCapitalized);
+    assert(`Henry II`.isNameCapitalized);
+}

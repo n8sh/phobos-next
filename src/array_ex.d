1f7a9fef89efefb2207635834e83a21bcc264f18
Per Nordlöw <per.nordlow@gmail.com>
2017-04-14 18:59:22 +0200

    Fix

--- src/array_ex.d
+++ src/array_ex.d
@@ -63,7 +63,11 @@ alias s = asStatic;
     static assert(is(typeof(x) == int[x.length]));
     static assert(is(typeof([1, 2, 3].asStatic) == int[x.length]));
 
-    static assert(!__traits(compiles, { static int[] doNotDoThat() @nogc { return [1, 2, 3].s; } }));
+    static assert(!__traits(compiles,
+                            {
+                                static int[] doNotDoThat() @nogc { return [1, 2, 3].s; }
+                            }
+                      ));
 }
 
 /// Array element ordering.

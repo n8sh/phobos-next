3ee608d5eaa584e1fc7eedcac3675dba29a0ab67
Per Nordlöw <per.nordlow@gmail.com>
2017-05-09 16:32:33 +0200

    Fix

--- tests/test_scope.d
+++ tests/test_scope.d
@@ -3,12 +3,10 @@
 struct S
 {
     @safe pure nothrow @nogc
-
-    auto opSlice() return scope
+    int[] opSlice() return scope
     {
         return x[];
     }
-
     int[4] x;
 }
 
@@ -16,16 +14,4 @@ int[] f()
 {
     S s;
     return s[];                 // should error with -dip1000
-}
-
-int[] g()()
-{
-    S s;
-    return s.x[];               // errors by default
-}
-
-unittest
-{
-    f();
-    static assert(!__traits(compiles, { g(); }));
 }

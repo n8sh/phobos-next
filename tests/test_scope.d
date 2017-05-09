cf85f449d24981fbe6269f8096db23282e2fbb65
Per Nordlöw <per.nordlow@gmail.com>
2017-05-09 14:23:50 +0200

    Fix

--- tests/test_scope.d
+++ tests/test_scope.d
@@ -1,5 +1,31 @@
 @safe pure nothrow @nogc:
 
+struct S
+{
+    @safe pure nothrow @nogc
+
+    auto opSlice() return scope
+    {
+        return x[];
+    }
+
+    int[4] x;
+}
+
+int[] f()
+{
+    S s;
+    return s[];                 // should error with -dip1000
+}
+
+int[] g()()
+{
+    S s;
+    return s.x[];               // errors by default
+}
+
 unittest
 {
+    f();
+    static assert(!__traits(compiles, { g(); }));
 }

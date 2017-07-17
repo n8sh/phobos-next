2aeaed65fd6c123a42d579902d153e3beb5b1df2
Per Nordlöw <per.nordlow@gmail.com>
2017-07-17 19:33:16 +0200

    Fix

--- src/algorithm_ex.d
+++ src/algorithm_ex.d
@@ -1746,6 +1746,12 @@ ref R append(R, Args...)(ref R data,
             data[$ - args.length + i] = arg;
         }
     }
+    // else static if (isStaticArray!R &&
+    //                 allSatisfy!(isElementType, Args) &&
+    //                 allSatisfy!(isStaticArray, Args))
+    // {
+    //     // TODO
+    // }
     else
     {
         static size_t estimateLength(Args args)

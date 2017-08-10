ca470f11b574330536345f7cc968bc39788b7949
Per Nordlöw <per.nordlow@gmail.com>
2017-08-10 18:39:17 +0200

    Fix

--- src/suokif.d
+++ src/suokif.d
@@ -124,7 +124,7 @@ bool isSymbolChar(char x)
     return x.isAlphaNum || x.among!('_', '-'); // TODO merge to single call to among
 }
 
-/** Returns: true if `s` is null-terminated (`'\0'`).
+/** Returns: true if `s` is null-terminated (ending with `'\0'`).
 
     Used to verify input to parser make use of sentinel-based search.
 

b799d76863848166bc8eed6d75c8f6013854cebb
Per Nordlöw <per.nordlow@gmail.com>
2017-08-20 22:21:13 +0200

    Fix

--- src/suokif.d
+++ src/suokif.d
@@ -304,7 +304,7 @@ nullFound:
 
 @safe pure unittest
 {
-    const text = ";;a comment\n(instance AttrFn BinaryFunction)\0";
+    immutable text = ";;a comment\n(instance AttrFn BinaryFunction)\0";
     const exprs = parseSUOKIF(text);
 
     assert(exprs.length == 1);

d06313b5110316d34aa98316eeffcd93c0adf119
Per Nordlöw <per.nordlow@gmail.com>
2017-08-29 20:57:35 +0200

    Add rawio

---
+++ src/rawio.d
@@ -0,0 +1,23 @@
+module rawio;
+
+/** Read file $(D path) into raw array with one extra terminating zero (null)
+    byte.
+
+    TODO add or merge to Phobos?
+
+    See also: https://en.wikipedia.org/wiki/Sentinel_value
+    See also: http://forum.dlang.org/post/pdzxpkusvifelumkrtdb@forum.dlang.org
+*/
+void[] rawReadNullTerminated(string path)
+    @safe
+{
+    import std.stdio : File;
+    auto file = File(path, `rb`);
+
+    import std.array : uninitializedArray;
+    ubyte[] data = uninitializedArray!(ubyte[])(file.size + 1); // one extra for null terminator
+    file.rawRead(data);
+    data[file.size] = 0;     // null terminator for sentinel
+
+    return data;                // TODO can we cast this to immutable?
+}

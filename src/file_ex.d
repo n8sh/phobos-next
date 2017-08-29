2307fe15d8c8d99eb352ed39de4d40daf73c1c2a
Per Nordlöw <per.nordlow@gmail.com>
2017-08-29 18:00:02 +0200

    Add file_ex

---
+++ src/file_ex.d
@@ -0,0 +1,20 @@
+module file_ex;
+
+/** Read file $(D path) into raw array with one extra terminating zero (null)
+    byte.
+
+    See also: https://en.wikipedia.org/wiki/Sentinel_value
+*/
+void[] rawReadNullTerminated(string path)
+    @safe
+{
+    import std.stdio : File;
+    auto file = File(path, `rb`);
+
+    import std.array : uninitializedArray;
+    ubyte[] data = uninitializedArray!(ubyte[])(file.size + 1); // one extra for null terminator
+    file.rawRead(data);
+    data[file.size] = 0;     // null terminator for sentinel
+
+    return data;
+}

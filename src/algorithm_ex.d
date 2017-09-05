38ea33407e2ae6de45471c2d7ac5d33a0c0e4b51
Per Nordlöw <per.nordlow@gmail.com>
2017-09-05 23:05:31 +0200

    Fix

--- src/algorithm_ex.d
+++ src/algorithm_ex.d
@@ -2451,7 +2451,7 @@ auto use(alias F, T)(T t)
     }
 }
 
-/** Is `true` if `x` is an ASCII character, false otherwise.
+/** Is `true` if `x` is an ASCII character constant, false otherwise.
     See also: `std.ascii.isASCII`.
 */
 enum isASCIIConstant(alias x) = (is(typeof(x) : char) ||

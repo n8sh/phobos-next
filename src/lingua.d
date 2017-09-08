0fbede2dfceac266117db4d1983f35d4dcf081bf
Per Nordlöw <per.nordlow@gmail.com>
2017-09-08 11:16:14 +0200

    Fix

--- src/lingua.d
+++ src/lingua.d
@@ -304,7 +304,7 @@ enum VerbForm : ubyte
     presentIndicative,          // sv:presens. Swedish example: springer
 
     presentParticiple,          // sv:presens particip. Swedish example: springande
-    gerund = presentParticiple, // https://en.wikipedia.org/wiki/Gerund
+    gerund = presentParticiple, // Form that functions as a noun. Source: https://en.wikipedia.org/wiki/Gerund
 
     pastIndicative,             // sv:imperfekt. Swedish example: sprang
     preteritum = pastIndicative,

1287538c3135113764dafb04b8bc5db76a5fff1a
Per Nordlöw <per.nordlow@gmail.com>
2017-09-11 13:05:38 +0200

    Use array_help

--- src/array_ex.d
+++ src/array_ex.d
@@ -47,38 +47,6 @@ version(useMemoryErrorHandler)
 }
 // version = showCtors;
 
-/** Returns: statically (stack) allocated array with elements of type `T` of
-    length `n`.
-
-    For more convenient usage alias it as `s' together with UFCS for the
-    following convenient notation:
-
-    const x = [1, 2, 3].s;
-
-    TODO Add to Phobos `std.array`.
-*/
-T[n] asStatic(T, size_t n)(T[n] arr)
-//    @safe pure nothrow @nogc // TODO remove. needed for now in order for DIP-1000 to work correctly, See http://forum.dlang.org/post/otrsanpgmokzpzqmfyvx@forum.dlang.org
-{
-    return arr;
-}
-alias s = asStatic;
-
-///
-@safe pure nothrow @nogc unittest
-{
-    auto x = [1, 2, 3].asStatic;
-
-    static assert(is(typeof(x) == int[x.length]));
-    static assert(is(typeof([1, 2, 3].asStatic) == int[x.length]));
-
-    static assert(!__traits(compiles,
-                            {
-                                static int[] doNotDoThat() { return [1, 2, 3].s; }
-                            }
-                      ));
-}
-
 /// Array element ordering.
 enum Ordering
 {
@@ -98,6 +66,7 @@ version(unittest)
     import std.meta : AliasSeq;
     import std.traits : Unqual;
     import dbgio : dln;
+    import array_help : s;
 }
 
 import container_traits : ContainerElementType;

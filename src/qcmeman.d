8563ae90d9200c48b59badb280e84ad8f5b9dbd7
Per Nordlöw <per.nordlow@gmail.com>
2017-09-12 02:44:17 +0200

    Add some space

--- src/qcmeman.d
+++ src/qcmeman.d
@@ -5,9 +5,11 @@ extern(C)
 {
     // qualified C memory allocations
     @safe pure nothrow @nogc:
+
     void* malloc(size_t size);
     void* calloc(size_t nmemb, size_t size);
     void* realloc(void* ptr, size_t size);
+
     void free(void* ptr);
 
     void gc_addRange( in void* p, size_t sz, const TypeInfo ti = null );

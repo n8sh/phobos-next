3f952909384fe3014443d5fcf2b747e790110720
Per Nordlöw <per.nordlow@gmail.com>
2017-09-14 17:29:04 +0200

    Remove of

--- src/predicates.d
+++ src/predicates.d
@@ -7,72 +7,7 @@
    */
 module predicates;
 
-import std.traits : CommonType;
 import std.range : isIterable, ElementType;
-
-/** Return true if $(D x) is a equal to any of $(D ys).
-    TODO Make $(D ys) lazy if any of $(D ys) is a delegate.
-    TODO Reuse $(D among)
- */
-bool of(S, T...)(in S x, in T ys)
-    if (ys.length >= 1 &&
-        is(typeof({ return S.init ==
-                    CommonType!(T).init; })))
-{
-    foreach (y; ys)
-    {
-        if (x == y)
-            return true;
-    }
-    return false;
-}
-
-@safe pure nothrow @nogc unittest
-{
-    assert(1.of(1, 2, 3));
-    assert(!4.of(1, 2, 3));
-}
-
-/** Return true if $(D x) is a equal to any of $(D ys).
-    TODO Reuse $(D among)
- */
-bool of(E, R)(E x, R ys)
-    if (isIterable!R/*  && */
-        /* is(E.init == ElementType!R.init) */)
-{
-    import std.range : isNarrowString;
-    static if (isNarrowString!R)
-    {
-        import std.algorithm: canFind;
-        return ys.canFind(x);
-    }
-    else
-    {
-        foreach (y; ys)
-        {
-            if (x == y)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
-@safe pure nothrow unittest
-{
-    assert(1.of([1, 2, 3]));
-    assert(!4.of([1, 2, 3]));
-}
-
-@safe pure unittest
-{
-    assert('a'.of("abc"));
-    assert(!'z'.of("abc"));
-    assert('å'.of("åäö"));
-}
-
-alias isEither = of;
 
 // ==============================================================================================
 

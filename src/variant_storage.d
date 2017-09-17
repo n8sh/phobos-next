f9975d7c52235bb0c0eddf4c75caec0cdb86d1a4
Per Nordlöw <per.nordlow@gmail.com>
2017-09-18 01:04:50 +0200

    Better naming

--- src/variant_storage.d
+++ src/variant_storage.d
@@ -9,9 +9,9 @@ struct VariantIndex(Types...)
 
     private enum N = typeCount; // useful local shorthand
 
-    import std.bitmanip : bitfields;
-    mixin(bitfields!(Ix, "_type", 1,
-                     size_t, "_index", 7));
+    // import std.bitmanip : bitfields;
+    // mixin(bitfields!(Ix, "_type", 1,
+    //                  size_t, "_index", 7));
 }
 
 /** Stores set of variants.
@@ -24,26 +24,46 @@ struct VariantStorage(Types...)
 {
     alias IndexType = VariantIndex!Types;
 
-    // TODO this crashes
+    // TODO this crashes. Make this work when LDC is at 2.076
     // import std.meta : AliasSeq;
     // static foreach (Type; Types)
     // {
     // }
 
-    import basic_array : UniqueBasicArray;
+    import basic_array : Array = UniqueBasicArray;
     import std.ascii : toLower;
 
+private:
+    // storages
     mixin({
             string s = "";
             foreach (i, Type; Types)
             {
-                s ~= `UniqueBasicArray!` ~ Type.stringof ~ ` ` ~ `values` ~ Type.stringof ~ `;`;
+                s ~= `Array!` ~ Type.stringof ~ ` ` ~ `_values` ~ Type.stringof ~ `;`;
             }
             return s;
         }());
 }
 
+struct Node {}
+
+struct Fn1 {}
+struct Fn2 {}
+
+struct Rel1 {}
+struct Rel2 {}
+struct Rel3 {}
+
+struct Pred1 {}
+struct Pred2 {}
+struct Pred3 {}
+struct Pred4 {}
+struct Pred5 {}
+
 @safe pure nothrow @nogc unittest
 {
-    VariantStorage!(ubyte, ushort, uint, ulong, float, double, string) vs;
+    VariantStorage!(Node,
+                    Fn1, Fn2,
+                    Rel1, Rel2, Rel3,
+                    Pred1, Pred2, Pred3, Pred4, Pred5) vs;
 }

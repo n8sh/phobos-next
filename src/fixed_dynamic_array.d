02da3dadaaa5fefccda84056d5f31ac7896c0143
Per Nordlöw <per.nordlow@gmail.com>
2017-09-26 11:55:45 +0200

    Fix

--- src/fixed_dynamic_array.d
+++ src/fixed_dynamic_array.d
@@ -32,7 +32,7 @@ pragma(inline, true):
     // disable copying
     @disable this(this);
 
-    /// Get index.
+    /// Get element at index `i`.
     scope ref inout(E) opIndex(size_t i) inout @system return
     {
         return _storage[i];

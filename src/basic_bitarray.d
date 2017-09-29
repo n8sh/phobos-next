8d6ca49c6cee84bfee59d758ba9a70d9027ebb10
Per Nordlöw <per.nordlow@gmail.com>
2017-09-29 16:35:44 +0200

    Fix

--- src/basic_bitarray.d
+++ src/basic_bitarray.d
@@ -13,7 +13,7 @@ struct BitArray(alias Allocator = null)
     {
         _blockCount = ((length / blockBits) + // number of whole blocks
                        (length % blockBits ? 1 : 0)); // remained block
-        _ptr = cast(Block*)calloc(blockBits, _blockCount);
+        _ptr = cast(Block*)calloc(blockBits, _blockCount); // TODO use malloc and lazy call to `memset` later on
         _length = length;
     }
 

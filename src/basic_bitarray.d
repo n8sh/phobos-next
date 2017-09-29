47dadd4a9ffe755877d17481dc3eb9d102f261ae
Per Nordlöw <per.nordlow@gmail.com>
2017-09-29 16:08:16 +0200

    First try

---
+++ src/basic_bitarray.d
@@ -0,0 +1,40 @@
+module basic_bitarray;
+
+/** Array of bits. */
+struct BitArray
+{
+    @safe pure nothrow @nogc:
+
+    /** Construct with `length` number of bits. */
+    this(size_t length)
+    {
+        immutable blockCount = length / blockBits + (length % blockBits ? 1 : 0);
+        _store = Store.withLength(blockCount);
+    }
+
+private:
+    import basic_copyable_array : Array = CopyableArray;
+    alias Block = size_t;
+    enum blockBits = 8*Block.sizeof;
+    alias Store = Array!Block;
+    Store _store;
+}
+
+version = show;
+
+@safe pure nothrow @nogc unittest
+{
+    const bitCount = 100;
+    auto a = BitArray(bitCount);
+    assert(a._store.length == 2);
+}
+
+version(unittest)
+{
+    import array_help : s;
+}
+
+version(show)
+{
+    import dbgio : dln;
+}

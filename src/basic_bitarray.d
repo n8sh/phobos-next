68f33ed398ef95bf281998e719a0beb39e127f48
Per Nordlöw <per.nordlow@gmail.com>
2017-09-29 16:34:50 +0200

    Don't use array

--- src/basic_bitarray.d
+++ src/basic_bitarray.d
@@ -3,16 +3,23 @@ module basic_bitarray;
 /** Array of bits. */
 struct BitArray(alias Allocator = null)
 {
+    import qcmeman : malloc, calloc, realloc, free;
+
     pragma(inline, true)
     @safe pure nothrow @nogc:
 
     /** Construct with `length` number of bits. */
-    this(size_t length)
+    this(size_t length) @trusted
     {
-        immutable blockCount = ((length / blockBits) + // number of whole blocks
-                                (length % blockBits ? 1 : 0)); // remained block
-        _store = Store.withLength(blockCount);
+        _blockCount = ((length / blockBits) + // number of whole blocks
+                       (length % blockBits ? 1 : 0)); // remained block
+        _ptr = cast(Block*)malloc(blockBits*_blockCount);
         _length = length;
+    }
+
+    ~this() @trusted
+    {
+        free(_ptr);
     }
 
     /// Check if empty.
@@ -22,21 +29,19 @@ struct BitArray(alias Allocator = null)
     @property size_t length() const { return _length; }
     alias opDollar = length;    /// ditto
 
-    /// Get capacity.
+    /// Get capacity in number of bits.
     pragma(inline, true)
-    @property size_t capacity() const { return blockBits*_store.capacity; }
+    @property size_t capacity() const { return blockBits*_blockCount; }
 
     @disable this(this);
 
 private:
-    import basic_copyable_array : Array = CopyableArray;
-
     alias Block = size_t;
     enum blockBits = 8*Block.sizeof;
-    alias Store = Array!(Block, Allocator);
 
-    Store _store;
-    size_t _length;
+    Block* _ptr;
+    size_t _blockCount;
+    size_t _length;             // TODO remove this
 }
 
 version = show;
@@ -49,7 +54,6 @@ version = show;
 
     assert(a.length == bitCount);
     assert(a.capacity == 2*a.blockBits);
-    assert(a._store.length == 2);
 }
 
 version(unittest)

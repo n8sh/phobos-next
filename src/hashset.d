daffebc77700635aec2b98f9a0ae5fc76287986d
Per Nordlöw <per.nordlow@gmail.com>
2017-09-29 13:18:18 +0200

    Remove ldn

--- src/hashset.d
+++ src/hashset.d
@@ -48,6 +48,7 @@ struct HashSet(T,
     {
         import std.algorithm.searching : canFind;
         immutable bucketIndex = hashFunction(value) & hashMask;
+        // dln(bucketIndex);
         if (!_buckets[bucketIndex][].canFind(value))
         {
             _buckets[bucketIndex].insertBackMove(value);

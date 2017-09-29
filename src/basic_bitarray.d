cd34fb5f9106709b6d7314928b474b1836c9aaf0
Per Nordlöw <per.nordlow@gmail.com>
2017-09-29 16:21:54 +0200

    Add length

--- src/basic_bitarray.d
+++ src/basic_bitarray.d
@@ -11,16 +11,29 @@ struct BitArray(alias Allocator = null)
         immutable blockCount = ((length / blockBits) + // number of whole blocks
                                 (length % blockBits ? 1 : 0)); // remained block
         _store = Store.withLength(blockCount);
+        _length = length;
     }
+
+    /// Check if empty.
+    pragma(inline, true)
+    bool empty() const { return _length == 0; }
+
+    /// Get length.
+    pragma(inline, true)
+    @property size_t length() const { return _length; }
+    alias opDollar = length;    /// ditto
 
     @disable this(this);
 
 private:
     import basic_copyable_array : Array = CopyableArray;
+
     alias Block = size_t;
     enum blockBits = 8*Block.sizeof;
     alias Store = Array!(Block, Allocator);
+
     Store _store;
+    size_t _length;
 }
 
 version = show;
@@ -29,6 +42,7 @@ version = show;
 {
     const bitCount = 100;
     auto a = BitArray!(null)(bitCount);
+    assert(a.length == bitCount);
     assert(a._store.length == 2);
 }
 

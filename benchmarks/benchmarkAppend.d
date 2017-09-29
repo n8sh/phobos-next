0a8484b8ee31023c09cd297832c422bd690d4857
Per Nordlöw <per.nordlow@gmail.com>
2017-09-29 11:28:25 +0200

    Add VariantArrays

--- benchmarks/benchmarkAppend.d
+++ benchmarks/benchmarkAppend.d
@@ -3,6 +3,8 @@ void main()
     import std.array : Appender;
     import std.container.array : StdArray = Array;
     import basic_copyable_array : CopyableArray;
+    import variant_arrays : VariantArrays;
+    import hashset : HashSet;
 
     import std.stdio : writeln;
     import std.datetime : MonoTime;
@@ -12,23 +14,32 @@ void main()
     immutable n = 5_000_000;
 
     foreach (A; AliasSeq!(CopyableArray!E,
+                          VariantArrays!E,
                           StdArray!E,
                           Appender!(E[]),
                           E[]))
     {
         A a;
-
         // a.reserve(n);
-
         immutable before = MonoTime.currTime();
-
         foreach (const i; 0 .. n)
         {
-            a ~= i;
+            a ~= cast(E)i;      // need to cast away const here
         }
-
         immutable after = MonoTime.currTime();
+        writeln("Added ", n, " integers into ", A.stringof, " in ", after - before);
+    }
 
-        writeln("Added ", n, " integer nodes into ", A.stringof, " in ", after - before);
+    foreach (A; AliasSeq!(HashSet!E))
+    {
+        A a = A(n);
+        // a.reserve(n);
+        immutable before = MonoTime.currTime();
+        foreach (const i; 0 .. n)
+        {
+            a.insert(i);
+        }
+        immutable after = MonoTime.currTime();
+        writeln("Inserted ", n, " integers into ", A.stringof, " in ", after - before);
     }
 }

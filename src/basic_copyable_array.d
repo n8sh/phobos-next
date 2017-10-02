d032a4d55b5e3cf200c719d2974709da04f910de
Per Nordlöw <per.nordlow@gmail.com>
2017-10-02 18:36:41 +0200

    remove code

--- src/basic_copyable_array.d
+++ src/basic_copyable_array.d
@@ -902,16 +902,6 @@ version(unittest)
 /// convert to string
 unittest
 {
-    // import arrayn : ArrayN;
-    // alias Chars7 = ArrayN!(char, 7);
-    // alias A = CopyableArray!(Chars7);
-    // CopyableArray!char sink;
-    // TODO make this work: A([1, 2, 3]).toString(sink.put);
-}
-
-/// convert to string
-unittest
-{
     alias T = int;
     alias A = CopyableArray!(T);
 

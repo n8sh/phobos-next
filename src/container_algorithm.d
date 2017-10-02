f0f8ae51bffd9f65a0e5f08c16f45b9cc62ce034
Per Nordlöw <per.nordlow@gmail.com>
2017-10-03 01:37:30 +0200

    Fix

--- src/container_algorithm.d
+++ src/container_algorithm.d
@@ -1,16 +1,13 @@
 module container_algorithm;
 
-import std.range.primitives : hasSlicing;
-
 /** Try to pop first occurrence of `needle` in `haystack` (if any).
     Returns: `true` iff pop was made, `false` otherwise.
  */
 bool popFirst(C, E)(ref C haystack,
                     in E needle)
-    if (__traits(hasMember, C, `popAt`) &&
-        hasSlicing!C)
+    if (__traits(hasMember, C, `popAt`))
     // TODO activate this restriction
-    // if (isContainer!C &&
+    // if (hasSlicing!C &&
     //     is(ElementType!C == E.init))
 {
     import std.algorithm.searching : countUntil;

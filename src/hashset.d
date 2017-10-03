abcc23573691c6c33521448975ed4509d930d084
Per Nordlöw <per.nordlow@gmail.com>
2017-10-03 02:30:20 +0200

    Fix

--- src/hashset.d
+++ src/hashset.d
@@ -149,7 +149,7 @@ struct HashSet(T,
                 // .destroy(_buckets[bucketIndex].large);
                 // moveEmplace(small, _buckets[bucketIndex].small);
                 // _largeBucketFlags[bucketIndex] = false; // now small
-                // _length -= 1;
+                _length -= 1;
             }
             return hit;
         }

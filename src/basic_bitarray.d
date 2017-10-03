e8d589d1e4fd45dad47c484dd0116d6f422dd606
Per Nordlöw <per.nordlow@gmail.com>
2017-10-03 23:28:05 +0200

    Better countOnes

--- src/basic_bitarray.d
+++ src/basic_bitarray.d
@@ -65,33 +65,23 @@ struct BitArray(alias Allocator = null)
         typeof(return) n = 0;
         foreach (const ref block; blocks)
         {
-            if (block != 0)
+            import core.bitop : popcnt;
+            static if (block.sizeof == 1 ||
+                       block.sizeof == 2 ||
+                       block.sizeof == 4 ||
+                       block.sizeof == 4)
             {
-                import core.bitop : popcnt;
-                static      if (block.sizeof == 1)
-                {
-                    // TODO do we need to force `uint`-overload of `popcnt`?
-                    n += cast(uint)block.popcnt;
-                }
-                else static if (block.sizeof == 2)
-                {
-                    // TODO do we need to force `uint`-overload of `popcnt`?
-                    n += cast(uint)block.popcnt;
-                }
-                else static if (block.sizeof == 4)
-                {
-                    // TODO do we need to force `uint`-overload of `popcnt`?
-                    n += cast(uint)block.popcnt;
-                }
-                else static if (block.sizeof == 8)
-                {
-                    n += (cast(ulong)((cast(uint)(block)).popcnt) +
-                          cast(ulong)((cast(uint)(block >> 32)).popcnt));
-                }
-                else
-                {
-                    assert(false, "Unsupported Block size " ~ Block.sizeof.stringof);
-                }
+                // TODO do we need to force `uint`-overload of `popcnt`?
+                n += cast(uint)block.popcnt;
+            }
+            else static if (block.sizeof == 8)
+            {
+                n += (cast(ulong)((cast(uint)(block)).popcnt) +
+                      cast(ulong)((cast(uint)(block >> 32)).popcnt));
+            }
+            else
+            {
+                assert(false, "Unsupported Block size " ~ Block.sizeof.stringof);
             }
         }
         return typeof(return)(n);

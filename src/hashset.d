d049d8c6913f921b48ac65583513c6edf562c7ef
Per Nordlöw <per.nordlow@gmail.com>
2017-10-04 01:35:33 +0200

    Better name

--- src/hashset.d
+++ src/hashset.d
@@ -105,7 +105,7 @@ struct HashSet(K, V = void,
     bool insert(K value) @trusted
     {
         import std.conv : emplace;
-        immutable bucketIndex = bucketHashIndex(value);
+        immutable bucketIndex = bucketHash!(hasher)(value) & _hashMask;
         if (_largeBucketFlags[bucketIndex])
         {
             if (!_buckets[bucketIndex].large[].canFind(value))
@@ -140,7 +140,7 @@ struct HashSet(K, V = void,
      */
     bool contains(in K value) const @trusted
     {
-        immutable bucketIndex = bucketHashIndex(value);
+        immutable bucketIndex = bucketHash!(hasher)(value) & _hashMask;
         if (_largeBucketFlags[bucketIndex])
         {
             return _buckets[bucketIndex].large[].canFind(value);
@@ -158,7 +158,7 @@ struct HashSet(K, V = void,
         @trusted
     {
         // dln("value:", value);
-        immutable bucketIndex = bucketHashIndex(value);
+        immutable bucketIndex = bucketHash!(hasher)(value) & _hashMask;
         import container_algorithm : popFirst;
         if (_largeBucketFlags[bucketIndex])
         {
@@ -190,87 +190,6 @@ struct HashSet(K, V = void,
                 _length -= 1;
             }
             return hit;
-        }
-    }
-
-    /** Get index into `bucket` for `value`.
-     */
-    pragma(inline)              // LDC can inline, DMD cannot
-    size_t bucketHashIndex(in K value) const
-    {
-        import std.digest.digest : isDigest;
-        import std.traits : hasMember;
-
-        static if (__traits(compiles, { size_t _ = hasher(value); }))
-        {
-            return hasher(value) & _hashMask;
-        }
-        else static if (__traits(compiles, { enum _ = isDigest!hasher; }) &&
-                        isDigest!hasher &&
-                        hasMember!(hasher, "get"))
-        {
-            import std.digest.digest : makeDigest;
-
-            auto dig = makeDigest!(hasher);
-            dig.put((cast(ubyte*)&value)[0 .. value.sizeof]);
-            dig.finish();
-
-            static if (is(typeof(dig.get()) == typeof(return)))
-            {
-                return dig.get() & _hashMask;
-            }
-            else static if (is(typeof(dig.get()) == typeof(return)[2]))
-            {
-                return (dig.get()[0] ^ dig.get()[1]) & _hashMask;
-            }
-            else
-            {
-                static assert(0, "Handle get() with return type " ~ typeof(dig.get()).stringof ~
-                              " on " ~ size_t.sizeof.stringof ~ "-bit platform");
-            }
-        }
-        else static if (__traits(compiles, { auto _ = hasher((ubyte[]).init); }))
-        {
-            // cast input `value` to `ubyte[]` and use std.digest API
-            immutable digest = hasher((cast(ubyte*)&value)[0 .. value.sizeof]); // TODO ask forums when this isn't correct
-
-            static assert(digest.sizeof >=
-                          typeof(return).sizeof,
-                          `Size of digest is ` ~ digest.sizeof
-                          ~ ` but needs to be at least ` ~ typeof(return).sizeof.stringof);
-
-            import std.traits : isUnsigned, isStaticArray;
-
-            static if (isUnsigned!(typeof(digest)))
-            {
-                return cast(typeof(return))digest & _hashMask; // fast modulo calculation
-            }
-            else static if (isStaticArray!(typeof(digest)))
-            {
-                typeof(return) hashIndex;
-
-                static if (2*size_t.sizeof == digest.sizeof)
-                {
-                    // for instance, use all 128-bits when size_t is 64-bit
-                    (cast(ubyte*)&hashIndex)[0 .. hashIndex.sizeof] = (digest[0 .. hashIndex.sizeof] ^
-                                                                       digest[hashIndex.sizeof .. 2*hashIndex.sizeof]);
-                }
-                else
-                {
-                    (cast(ubyte*)&hashIndex)[0 .. hashIndex.sizeof] = digest[0 .. hashIndex.sizeof];
-                }
-
-                return hashIndex & _hashMask;
-            }
-            else
-            {
-                static assert(0, "Unsupported digest type " ~ typeof(digest).stringof);
-            }
-        }
-        else
-        {
-            static assert(0, "Cannot combine hasher " ~ hasher.stringof ~
-                          " with element type " ~ K.stringof);
         }
     }
 
@@ -334,6 +253,87 @@ private:
     size_t _length;
 
     size_t _hashMask;
+}
+
+/** Get index into `bucket` for `value`.
+ */
+pragma(inline)              // LDC can inline, DMD cannot
+size_t bucketHash(alias hasher, K)(in K value)
+{
+    import std.digest.digest : isDigest;
+    import std.traits : hasMember;
+
+    static if (__traits(compiles, { size_t _ = hasher(value); }))
+    {
+        return hasher(value);
+    }
+    else static if (__traits(compiles, { enum _ = isDigest!hasher; }) &&
+                    isDigest!hasher &&
+                    hasMember!(hasher, "get"))
+    {
+        import std.digest.digest : makeDigest;
+
+        auto dig = makeDigest!(hasher);
+        dig.put((cast(ubyte*)&value)[0 .. value.sizeof]);
+        dig.finish();
+
+        static if (is(typeof(dig.get()) == typeof(return)))
+        {
+            return dig.get();
+        }
+        else static if (is(typeof(dig.get()) == typeof(return)[2]))
+        {
+            return (dig.get()[0] ^ dig.get()[1]);
+        }
+        else
+        {
+            static assert(0, "Handle get() with return type " ~ typeof(dig.get()).stringof ~
+                          " on " ~ size_t.sizeof.stringof ~ "-bit platform");
+        }
+    }
+    else static if (__traits(compiles, { auto _ = hasher((ubyte[]).init); }))
+    {
+        // cast input `value` to `ubyte[]` and use std.digest API
+        immutable digest = hasher((cast(ubyte*)&value)[0 .. value.sizeof]); // TODO ask forums when this isn't correct
+
+        static assert(digest.sizeof >=
+                      typeof(return).sizeof,
+                      `Size of digest is ` ~ digest.sizeof
+                      ~ ` but needs to be at least ` ~ typeof(return).sizeof.stringof);
+
+        import std.traits : isUnsigned, isStaticArray;
+
+        static if (isUnsigned!(typeof(digest)))
+        {
+            return cast(typeof(return))digest; // fast modulo calculation
+        }
+        else static if (isStaticArray!(typeof(digest)))
+        {
+            typeof(return) hashIndex;
+
+            static if (2*size_t.sizeof == digest.sizeof)
+            {
+                // for instance, use all 128-bits when size_t is 64-bit
+                (cast(ubyte*)&hashIndex)[0 .. hashIndex.sizeof] = (digest[0 .. hashIndex.sizeof] ^
+                                                                   digest[hashIndex.sizeof .. 2*hashIndex.sizeof]);
+            }
+            else
+            {
+                (cast(ubyte*)&hashIndex)[0 .. hashIndex.sizeof] = digest[0 .. hashIndex.sizeof];
+            }
+
+            return hashIndex;
+        }
+        else
+        {
+            static assert(0, "Unsupported digest type " ~ typeof(digest).stringof);
+        }
+    }
+    else
+    {
+        static assert(0, "Cannot combine hasher " ~ hasher.stringof ~
+                      " with element type " ~ K.stringof);
+    }
 }
 
 @safe pure nothrow unittest

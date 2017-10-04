6d10905999fbbe7b8e6ce1ceceb836941900501f
Per Nordlöw <per.nordlow@gmail.com>
2017-10-04 19:46:43 +0200

    Add TODO

--- src/algorithm_ex.d
+++ src/algorithm_ex.d
@@ -4,6 +4,8 @@
     Copyright: Per Nordlöw 2017-.
     License: $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
     Authors: $(WEB Per Nordlöw)
+
+    TODO merge stuff from algorithm_ex_2.d
 */
 module algorithm_ex;
 

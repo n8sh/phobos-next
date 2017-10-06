e6dcdab3648acb4e35a6eb46e5cb0b761a6e5912
Per Nordlöw <per.nordlow@gmail.com>
2017-10-06 11:46:39 +0200

    Remove test

--- src/modulo.d
+++ src/modulo.d
@@ -292,12 +292,6 @@ auto mod(size_t m, T = UnsignedOfModulo!m)(T value)
 
     x -= 4;
     assert(x == 252);
-
-    const Mod!(255, ubyte) y = 55;
-
-    import std.traits : isAssignable;
-    static assert(isAssignable!(typeof(x), typeof(y)));
-    static assert(!isAssignable!(typeof(y), typeof(x)));
 }
 
 /// construct from other precision

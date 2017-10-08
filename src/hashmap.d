e9bf3ff910e2e7014401d1de31814fa7bba66b76
Per Nordlöw <per.nordlow@gmail.com>
2017-10-08 15:37:45 +0200

    Fix

--- src/hashmap.d
+++ src/hashmap.d
@@ -20,8 +20,6 @@ enum InsertionStatus { added, modified, unchanged }
  * TODO rehash: if allocator has realloc we can do rehashing in-place similar to
  * reordering in in-place radix (integer_sorting.d)
  *
- * TODO merge with EMSI containers.hashmap
- *
  * TODO forward-ranges `byValue`, `byKeyValue`
  *
  * TODO support uncopyable value type for map-case

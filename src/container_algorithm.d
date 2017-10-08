4f9bb26dbf456dae384e581f249ef791bdeb02da
Per Nordlöw <per.nordlow@gmail.com>
2017-10-08 02:28:37 +0200

    Fix

--- src/container_algorithm.d
+++ src/container_algorithm.d
@@ -5,15 +5,15 @@ import std.traits : hasMember;
 /** Try to pop first occurrence of `needle` in `haystack` (if any).
     Returns: `true` iff pop was made, `false` otherwise.
  */
-bool popFirst(C, E)(ref C haystack,
-                    in E needle)
+bool popFirst(alias pred = "a == b", C, E)(ref C haystack,
+                                           in E needle)
     if (hasMember!(C, "popAt"))
     // TODO activate this restriction
     // if (hasSlicing!C &&
     //     is(ElementType!C == E.init))
 {
     import std.algorithm.searching : countUntil;
-    immutable offset = haystack[].countUntil(needle);
+    immutable offset = haystack[].countUntil!pred(needle);
     if (offset != -1)
     {
         haystack.popAt(offset);

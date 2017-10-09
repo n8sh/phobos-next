5c328a0253731ef9b96964b88363922d3497818a
Per Nordlöw <per.nordlow@gmail.com>
2017-10-09 15:21:58 +0200

    Add ref

--- tests/test_scope.d
+++ tests/test_scope.d
@@ -1,27 +1,30 @@
-@safe pure nothrow @nogc:
-
 /** See also: http://forum.dlang.org/post/hwfpmabyunqhlkaqogdt@forum.dlang.org
     See also: https://issues.dlang.org/show_bug.cgi?id=17388
  */
-struct S
+struct S(T)
 {
-    @safe pure nothrow @nogc
-    inout(int)[] opSlice() inout return scope
+    scope inout(T)[] opSlice() inout return
     {
         return x[];
     }
-    int[4] x;
+
+    scope ref inout(T) first() inout return
+    {
+        return x[0];
+    }
+
+    T[4] x;
 }
 
-int[] f()
+int[] testSlice() @safe pure nothrow @nogc
 {
-    S s;
+    S!int s;
+    // TODO static assert(__traits(compiles, { return s[]; }));
     return s[];                 // should error with -dip1000
 }
 
-// void g()
-// {
-//     int[] r;
-//     S s;
-//     static assert(!__traits(compiles, { r = s[]; }));
-// }
+ref int testFirst() @safe pure nothrow @nogc
+{
+    S!int s;
+    return s.first;             // should errir with -dip1000
+}

9e2d035aff89af728d5b242456ddc7a014c19124
Per Nordlöw <per.nordlow@gmail.com>
2017-10-11 18:19:18 +0200

    Fix TODO

--- src/hashmap.d
+++ src/hashmap.d
@@ -18,8 +18,7 @@ enum InsertionStatus { added, modified, unchanged }
  *
  * See also: https://probablydance.com/2017/02/26/i-wrote-the-fastest-hashtable/
  *
- * TODO add flag for use growth factor smaller than powers of two. Using prime
- * table. Details are here: https://github.com/greg7mdp/sparsepp
+ * TODO add flag for use growth factor smaller than powers of two. use prime_modulo.d
  *
  * TODO add withElements() with fast pre-allocation `withCapacity`
  *

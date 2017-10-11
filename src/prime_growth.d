22c64f82263fdcdee088df1cef41558e12555758
Per Nordlöw <per.nordlow@gmail.com>
2017-10-11 17:22:03 +0200

    better

--- src/prime_growth.d
+++ src/prime_growth.d
@@ -1,4 +1,4 @@
-/** Grow size to next prime number stored in table.
+/** Grow size to next prime number stored in a table.
  * See also: https://probablydance.com/2017/02/26/i-wrote-the-fastest-hashtable/
  */
 module prime_growth;

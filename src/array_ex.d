5cdaff3779d5053d211827c57df2fa7ab85772a0
Per Nordlöw <per.nordlow@gmail.com>
2017-10-14 17:14:42 +0200

    move concatenate to concatenation

--- src/array_ex.d
+++ src/array_ex.d
@@ -2773,52 +2773,6 @@ version(unittest)
 //     // assert(x[] == `abc`);
 // }
 
-/// Sum of the lengths of the static arrays 'A'.
-template sumOfLengths(A...)
-    if (A.length)
-{
-    static if (A.length == 1)
-        enum sumOfLengths = A[0].length;
-    else
-        enum sumOfLengths = A[0].length + sumOfLengths!(A[1 .. $]);
-}
-
-@safe pure nothrow @nogc unittest
-{
-    int[2] x, y, z;
-    static assert(sumOfLengths!(x, y, z) == 6);
-}
-
-alias Elem(A : E[n], E, size_t n) = E;
-
-import std.traits : isStaticArray;
-import std.meta : allSatisfy;
-
-/// Returns: concatenation of the static arrays `Args` as a static array.
-Elem!(Args[0])[sumOfLengths!Args] concatenate(Args...)(Args arrays)
-    if (allSatisfy!(isStaticArray, Args))
-{
-    typeof(return) result = void;
-    foreach (const i, a; arrays)
-    {
-        static if (i == 0)
-            enum offset = 0;
-        else
-            enum offset = sumOfLengths!(arrays[0 .. i]);
-        result[offset .. offset + a.length] = a[];
-    }
-    return result;
-}
-
-@safe pure nothrow @nogc unittest
-{
-    int[2] x = [1, 2];
-    const int[2] y = [3, 4];
-    auto z = concatenate(x, y);
-    static assert(is(typeof(z) == int[4]));
-    assert(z == [1, 2, 3, 4]);
-}
-
 /// See also: http://forum.dlang.org/post/omfm56$28nu$1@digitalmars.com
 @safe pure nothrow unittest
 {
---
+++ src/concatenation.d
@@ -0,0 +1,51 @@
+module concatenation;
+
+import std.traits : isStaticArray;
+import std.meta : allSatisfy;
+
+/// Sum of the lengths of the static arrays 'A'.
+template sumOfLengths(A...)
+    if (A.length)
+{
+    static if (A.length == 1)
+        enum sumOfLengths = A[0].length;
+    else
+        enum sumOfLengths = A[0].length + sumOfLengths!(A[1 .. $]);
+}
+
+@safe pure nothrow @nogc unittest
+{
+    int[2] x, y, z;
+    static assert(sumOfLengths!(x, y, z) == 6);
+}
+
+alias Elem(A : E[n], E, size_t n) = E;
+
+/// Returns: concatenation of the static arrays `Args` as a static array.
+Elem!(Args[0])[sumOfLengths!Args] concatenate(Args...)(Args arrays)
+    if (allSatisfy!(isStaticArray, Args))
+{
+    typeof(return) result = void;
+    foreach (const i, a; arrays)
+    {
+        static if (i == 0)
+        {
+            enum offset = 0;
+        }
+        else
+        {
+            enum offset = sumOfLengths!(arrays[0 .. i]);
+        }
+        result[offset .. offset + a.length] = a[];
+    }
+    return result;
+}
+
+@safe pure nothrow @nogc unittest
+{
+    int[2] x = [1, 2];
+    const int[2] y = [3, 4];
+    auto z = concatenate(x, y);
+    static assert(is(typeof(z) == int[4]));
+    assert(z == [1, 2, 3, 4]);
+}

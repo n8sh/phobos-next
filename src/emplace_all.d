f00eae2a4f396f866340ceb3666348b7980d9805
Per Nordlöw <per.nordlow@gmail.com>
2017-10-16 20:22:23 +0200

    Fix

--- src/emplace_all.d
+++ src/emplace_all.d
@@ -1,1 +1,11 @@
 module emplace_all;
+
+void moveEmplaceAllGeneric(T)(T[] src, T[] tgt)
+{
+    const n = src.length;
+    assert(n == tgt.length);
+    foreach (i; 0 .. n)
+    {
+        moveEmplace(src[i], tgt[i]);
+    }
+}

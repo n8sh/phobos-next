4714576090b4b9a78921ba2cc5de8be9a64f92d3
Per Nordlöw <per.nordlow@gmail.com>
2017-10-16 19:42:19 +0200

    use uncopyable_sample

--- src/hashmap.d
+++ src/hashmap.d
@@ -1224,31 +1224,8 @@ pure unittest
 
     immutable n = 11;
 
-    struct V
-    {
-        import qcmeman : malloc, free;
-
-        this(uint i)
-        {
-            _i = cast(typeof(_i))malloc(1 * (*_i).sizeof);
-            dln("allocated: ", _i, " being ", *_i);
-        }
-
-        @disable this(this);
-
-        ~this()
-        {
-            if (_i)
-            {
-                dln("freeing: ", _i, " being ", *_i);
-            }
-            free(_i);
-        }
-
-        uint *_i;
-    }
-
     alias K = uint;
+    import uncopyable_sample : V = SomeUncopyable;
 
     import std.exception : assertThrown, assertNotThrown;
     import core.exception : RangeError;

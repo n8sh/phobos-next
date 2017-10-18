17fbbf51736b181c5767bcb194df3f6800d94b12
Per Nordlöw <per.nordlow@gmail.com>
2017-10-18 13:46:53 +0200

    Fix

--- benchmarks/fibers.d
+++ benchmarks/fibers.d
@@ -16,6 +16,9 @@ import core.thread : Fiber;
 import std.stdio;
 
 static immutable maxFiberCount = 100;
+static immutable chunkFiberCount = 10;
+
+size_t fiberCounter = 0;
 
 /** Function-like fiber.
  *
@@ -37,24 +40,53 @@ private:
     Args _args;
 }
 
-alias TestFiber = FunFiber!size_t;
+/** Function-like fiber.
+ *
+ * Arguments must all fulfill `isFiberParameter`.
+ */
+class TestFiber : Fiber
+{
+    this(size_t counter)
+    {
+        writeln("here");
+        _counter = counter;
+        super(&run);
+    }
+private:
+    void run()
+    {
+        writeln("running");
+        while (fiberCounter + chunkFiberCount < maxFiberCount)
+        {
+            foreach (immutable i; 0 .. chunkFiberCount)
+            {
+                fiberCounter += chunkFiberCount;
+                writeln(fiberCounter);
+            }
+        }
+    }
+    size_t _counter;
+}
 
 unittest
 {
-    foreach (immutable i; 0 .. maxFiberCount)
-    {
-        // create instances of each type
-        auto derived = new TestFiber(i);
-        // Fiber composed = new Fiber(&fiberFunc, i);
+    auto rootFiber = new TestFiber(0);
+    rootFiber.call();
 
-        // call both fibers once
-        derived.call();
-        // composed.call();
-        // printf("Execution returned to calling context.\n");
-        // composed.call();
+    // foreach (immutable i; 0 .. maxFiberCount)
+    // {
+    //     // create instances of each type
+    //     auto derived = new TestFiber(i);
+    //     // Fiber composed = new Fiber(&fiberFunc, i);
 
-        // since each fiber has run to completion, each should have state TERM
-        assert(derived.state == Fiber.State.TERM);
-        // assert(composed.state == Fiber.State.TERM);
-    }
+    //     // call both fibers once
+    //     derived.call();
+    //     // composed.call();
+    //     // printf("Execution returned to calling context.\n");
+    //     // composed.call();
+
+    //     // since each fiber has run to completion, each should have state TERM
+    //     assert(derived.state == Fiber.State.TERM);
+    //     // assert(composed.state == Fiber.State.TERM);
+    // }
 }

eeba5892c3b87aac919ab4d781258d606d6f7dc7
Per Nordlöw <per.nordlow@gmail.com>
2017-10-18 10:22:28 +0200

    Fix

--- src/chunking.d
+++ src/chunking.d
@@ -1,1 +1,14 @@
 module chunking;
+
+@system unittest
+{
+    import std.algorithm;
+    import std.algorithm.comparison : equal;
+    auto r = [1, 2, 3, 4, 5, 6, 7, 8, 9].chunkBy!((x, y) => ((x*y) % 3) == 0);
+    assert(r.equal!equal([
+        [1],
+        [2, 3, 4],
+        [5, 6, 7],
+        [8, 9]
+    ]));
+}

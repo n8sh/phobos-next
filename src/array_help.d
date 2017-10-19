6e5dc54300514be19d4cd52bf99604736f7a3858
Per Nordlöw <per.nordlow@gmail.com>
2017-10-19 16:51:54 +0200

    better qualify

--- src/array_help.d
+++ src/array_help.d
@@ -62,10 +62,8 @@ version(unittest)
     }
 }
 
-@safe pure nothrow @nogc:
-
 ///
-unittest
+@safe pure nothrow @nogc unittest
 {
     auto a = [1, 2, 3].asStaticArray;
     static assert(is(typeof(a) == int[a.length]));
@@ -79,13 +77,13 @@ unittest
 }
 
 /// non-copyable element type in static array
-unittest
+@safe pure nothrow @nogc unittest
 {
     auto b = [US(42)].s;
 }
 
 ///
-unittest
+@safe pure nothrow @nogc unittest
 {
     auto x = [1, 2, 3].asStaticArray;
 
@@ -97,4 +95,43 @@ unittest
                                 static int[] doNotDoThat() { return [1, 2, 3].s; }
                             }
                       ));
+}
+
+import std.range : ElementType;
+
+/** Overload of `std.array.array` that creates a static array of length `n`.
+    TODO Better name: {make,array}{N,Exactly}
+    TODO could we find a way to propagate length at compile-time?
+ */
+ElementType!R[n] toStaticArray(size_t n, R)(R r)
+{
+    assert(r.length == n);
+    typeof(return) dst;
+    import std.algorithm.mutation : copy;
+    r.copy(dst[]);
+    return dst;
+}
+
+/** Static array overload for `std.algorithm.iteration.map`.
+    See also: http://forum.dlang.org/thread/rqlittlysttwxwphlnmh@forum.dlang.org
+    TODO Add to Phobos
+ */
+typeof(fun(E.init))[n] map(alias fun, E, size_t n)(const E[n] src)
+{
+    import std.algorithm.iteration : map;
+    return src[].map!fun.toStaticArray!n;
+}
+
+///
+@safe pure nothrow unittest
+{
+    import std.meta : AliasSeq;
+    foreach (E; AliasSeq!(int, double))
+    {
+        enum n = 42;
+        E[n] c;
+        const result = c.map!(_ => _^^2);
+        static assert(c.length == result.length);
+        static assert(is(typeof(result) == const(E)[n]));
+    }
 }

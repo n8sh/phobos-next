af6f7fb37a8bb4d205291effe4da3029e33a719e
Per Nordlöw <per.nordlow@gmail.com>
2017-10-24 10:16:19 +0200

    add ddox

--- src/suppressing.d
+++ src/suppressing.d
@@ -2,17 +2,20 @@
  */
 module suppressing;
 
+/** Wrapper type for `T` with postblit disabled.
+ */
 struct SuppressPostblit(T)
 {
-    // Disguise T as a humble array.
+    /// Disguise `T` as a humble array.
     private ubyte[T.sizeof] _payload;
 
-    // Create from instance of T.
-    this(T arg) {
+    /// Create from instance of `T`.
+    this(T arg)
+    {
         _payload = *cast(ubyte[T.sizeof]*)&arg;
     }
 
-    // Or forward constructor arguments to T's constructor.
+    /// Or forward constructor arguments to `T`'s constructor.
     static if (__traits(hasMember, T, "__ctor"))
     {
         this(Args...)(Args args)
@@ -22,17 +25,19 @@ struct SuppressPostblit(T)
         }
     }
 
-    // Pretend to be a T.
+    /// Pretend to be a `T`.
     @property
-    ref T get()
+    ref inout(T) get() inout
     {
-        return *cast(T*)_payload.ptr;
+        return *cast(typeof(return)*)_payload.ptr;
     }
 
     alias get this;
 
-    static if (__traits(hasMember, T, "__dtor")) {
-        ~this() {
+    static if (__traits(hasMember, T, "__dtor"))
+    {
+        ~this()
+        {
             __traits(getMember, get, "__dtor")();
         }
     }

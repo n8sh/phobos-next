497b0b75499e69c28aed952f1abf278475d41359
Per Nordlöw <per.nordlow@gmail.com>
2017-10-24 10:09:51 +0200

    add suppressing

---
+++ src/suppressing.d
@@ -0,0 +1,39 @@
+/** Various suppressing hacks.
+ */
+module suppressing;
+
+struct SuppressPostblit(T)
+{
+    // Disguise T as a humble array.
+    private ubyte[T.sizeof] _payload;
+
+    // Create from instance of T.
+    this(T arg) {
+        _payload = *cast(ubyte[T.sizeof]*)&arg;
+    }
+
+    // Or forward constructor arguments to T's constructor.
+    static if (__traits(hasMember, T, "__ctor"))
+    {
+        this(Args...)(Args args)
+        if (__traits(compiles, (Args e){__traits(getMember, T.init, "__ctor")(e);}))
+        {
+            __traits(getMember, get, "__ctor")(args);
+        }
+    }
+
+    // Pretend to be a T.
+    @property
+    ref T get()
+    {
+        return *cast(T*)_payload.ptr;
+    }
+
+    alias get this;
+
+    static if (__traits(hasMember, T, "__dtor")) {
+        ~this() {
+            __traits(getMember, get, "__dtor")();
+        }
+    }
+}

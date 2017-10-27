be8753acadf1195a686e352325b86630c82a7fed
Per Nordlöw <per.nordlow@gmail.com>
2017-10-27 17:25:21 +0200

    add test

---
+++ src/vary_hashmap_test.d
@@ -0,0 +1,28 @@
+module vary_hashmap_test;
+
+///
+pure unittest
+{
+    import digestx.fnv : FNV;
+    import hashmap : HashMap;
+    import vary : FastVariant;
+
+    alias K = FastVariant!(size_t, string);
+    alias V = K;
+    alias X = HashMap!(K, V, null, FNV!(64, true));
+
+    X x;
+    x[K.init] = V.init;
+
+    const n = 100;
+    foreach (immutable size_t i; 0 .. n)
+    {
+        immutable k = K(i);
+        immutable v = V(i);
+
+        assert(k !in x);
+
+        x[k] = v;
+        assert(k in x);
+    }
+}

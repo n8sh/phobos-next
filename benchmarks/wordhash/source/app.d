7eb951c361ed03e44227bb1061ab70676c143147
Per Nordlöw <per.nordlow@gmail.com>
2017-10-28 16:40:24 +0200

    add some aliases

--- benchmarks/wordhash/source/app.d
+++ benchmarks/wordhash/source/app.d
@@ -1,6 +1,16 @@
 void main()
 {
     import std.stdio;
+
+    import hashset : HashSet;
+    import hashmap : HashMap;
+    import basic_array : BasicArray;
+
+    alias Ix = size_t;
+    alias Str = BasicArray!char;
+    alias Strs = HashSet!(Str);
+    alias IxStr = HashMap!(Ix, Str);
+
     foreach (const line; File("/usr/share/dict/words").byLine)
     {
         if (line.length >= 3 &&

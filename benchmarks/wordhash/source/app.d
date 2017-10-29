c663353631360e8b21cd51d89b27c781361479c7
Per Nordlöw <per.nordlow@gmail.com>
2017-10-29 10:30:55 +0100

    add maxLength

--- benchmarks/wordhash/source/app.d
+++ benchmarks/wordhash/source/app.d
@@ -2,6 +2,7 @@ void main()
 {
     import std.stdio;
     import std.datetime : MonoTime;
+    import std.algorithm : max;
     import digestx.fnv : FNV;
 
     import hashset : HashSet;
@@ -11,10 +12,11 @@ void main()
     alias Ix = size_t;
     alias Str = BasicArray!(char); // TODO use uint as length
     alias Strs = HashSet!(Str, null, FNV!(64, true));
-    alias IxStr = HashMap!(Ix, Str, null, FNV!(64, true));
+    // alias IxStr = HashMap!(Ix, Str, null, FNV!(64, true));
 
-    Strs strs;
+    auto strs = Strs.withCapacity(72800);
 
+    size_t maxLength = 0;
     immutable before = MonoTime.currTime();
     foreach (line; File("/usr/share/dict/words").byLine) // TODO make const and fix HashSet.insert
     {
@@ -22,6 +24,7 @@ void main()
             line[$ - 2 .. $] != `'s`)
         {
             strs.insert(Str(line));
+            maxLength = max(maxLength, line.length);
         }
     }
     immutable after = MonoTime.currTime();
@@ -32,8 +35,9 @@ void main()
     immutable n = strs.length;
     immutable binCounts = strs.binCounts;
 
-    writef("Insertion: n:%s %1.2s ms, %3.1f ns/op binCounts:S:%s,L:%s",
+    writef("Insertion: n:%s maxLength:%s %1.2smsecs, %3.1fnsecs/op binCounts:S:%s,L:%s",
            n,
+           maxLength,
            secs,
            cast(double)nsecs / n,
            binCounts.smallCount,

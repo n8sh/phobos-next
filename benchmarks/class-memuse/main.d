37b0150291df1af77624ca26ab41a26f4c5e9065
Per Nordlöw <per.nordlow@gmail.com>
2017-11-08 16:40:46 +0100

    add test

---
+++ benchmarks/class-memuse/main.d
@@ -0,0 +1,29 @@
+extern(C++)
+class Node
+{
+    this(ubyte type)
+    {
+        this.type = type;
+    }
+    ulong type;
+    ulong dype;
+    ulong jype;
+}
+
+void main(string[] args)
+{
+    import std.traits, std.meta, std.range, std.algorithm, std.stdio, std.array;
+    Appender!(Node[]) as;
+
+    const n = 10_000_000;
+    as.reserve(n);
+
+    foreach (i; 0 .. n)
+    {
+        as.put(new Node(42));
+    }
+
+    while (true)
+    {
+    }
+}

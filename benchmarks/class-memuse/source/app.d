7b6596a625465738e76612f76854450233f87771
Per Nordlöw <per.nordlow@gmail.com>
2017-11-12 11:29:20 +0100

    add test for struct

--- benchmarks/class-memuse/source/app.d
+++ benchmarks/class-memuse/source/app.d
@@ -6,25 +6,35 @@ import std.stdio : write, writeln, writef, writefln;
 import std.datetime : MonoTime;
 
 extern(C++)
-class NodeCxx
+class NodeCxxClass
 {
     this(ubyte type)
     {
         this.type = type;
     }
     ulong type;
-    // EdgeCxx[] edges;
+    // EdgeCxxClass[] edges;
 }
 
 extern(C++)
-class EdgeCxx
+class EdgeCxxClass
 {
     this(ubyte type)
     {
         this.type = type;
     }
     ulong type;
-    // NodeCxx[] actors;
+    // NodeCxxClass[] actors;
+}
+
+struct NodeCxxStruct
+{
+    this(ubyte type)
+    {
+        this.type = type;
+    }
+    ulong type;
+    // EdgeCxxClass[] edges;
 }
 
 void main(string[] args)
@@ -33,14 +43,32 @@ void main(string[] args)
 
     {
         import std.array : Appender;
-        Appender!(NodeCxx[]) x;
+        Appender!(NodeCxxStruct[]) x;
 
         x.reserve(n);
 
         immutable before = MonoTime.currTime();
         foreach (i; 0 .. n)
         {
-            x.put(new NodeCxx(42));
+            x.put(NodeCxxStruct(42));
+        }
+        immutable after = MonoTime.currTime();
+
+        writefln("Appender: %3.1f msecs (%3.1f ns/op)",
+                 cast(double)(after - before).total!"msecs",
+                 cast(double)(after - before).total!"nsecs" / n);
+    }
+
+    {
+        import std.array : Appender;
+        Appender!(NodeCxxClass[]) x;
+
+        x.reserve(n);
+
+        immutable before = MonoTime.currTime();
+        foreach (i; 0 .. n)
+        {
+            x.put(new NodeCxxClass(42));
         }
         immutable after = MonoTime.currTime();
 
@@ -51,14 +79,14 @@ void main(string[] args)
 
     {
         import basic_array : BasicArray;
-        BasicArray!(NodeCxx) y;
+        BasicArray!(NodeCxxClass) y;
 
         y.reserve(n);
 
         immutable before = MonoTime.currTime();
         foreach (i; 0 .. n)
         {
-            y.insertBack(new NodeCxx(42));
+            y.insertBack(new NodeCxxClass(42));
         }
         immutable after = MonoTime.currTime();
 

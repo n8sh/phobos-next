ad22563c7f7c07870f8edfd20bf574b1c8227ebd
Per Nordlöw <per.nordlow@gmail.com>
2018-01-11 22:00:35 +0100

    fix

--- src/container_traits.d
+++ src/container_traits.d
@@ -283,6 +283,8 @@ template isHashable(T)
     static assert(!isHashable!(const(char)[]));
 }
 
-/// Is `true` iff `T` is a set-like container.
+/** Is `true` iff `T` is a set-like container.
+    TODO make more exact
+ */
 enum isSetLike(T) = (__traits(hasMember, T, `insert`) &&
                      __traits(hasMember, T, `remove`));

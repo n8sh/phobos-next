33cd89562e8d981f61db5c4a10bc0fd5d7aecf23
Per Nordlöw <per.nordlow@gmail.com>
2018-02-14 21:40:00 +0100

    better naming

--- src/probing.d
+++ src/probing.d
@@ -6,34 +6,33 @@ module probing;
  *
  * See also: https://fgiesen.wordpress.com/2015/02/22/triangular-numbers-mod-2n/
  */
-size_t triangularFindIndex(alias hasher = hashOf, T, K)(const scope T[] haystack,
-                                                        const scope auto ref K key)
+size_t triangularProbeIndexFrom(alias hasher = hashOf, T, K)(const scope T[] haystack,
+                                                             const scope auto ref K key,
+                                                             size_t index)
     @safe pure nothrow @nogc
 {
-    size_t isKeyForIx(const scope K key,
-                      const scope size_t ix) const
+    size_t isKeyForIndex(const scope K key,
+                         const scope size_t index)
     {
-        return (haystack[ix] is key || // hit slot
-                haystack[ix].isNull); // free slot
+        return (haystack[index] is key || // hit slot
+                haystack[index].isNull); // free slot
     }
 
     immutable typeof(return) mask = haystack.length - 1;
     assert((~mask ^ mask) == typeof(return).max); // std.math.isPowerOf2(haystack.length)
 
-    import digestion : hashOf2;
-    size_t ix = hashOf2!(hasher)(key) & mask;
-
-    size_t inc = 1;
-    while (!isKeyForIx(key, ix) &&
-           inc != haystack.length)
+    // search using triangular numbers as increments
+    size_t indexIncrement = 1;
+    while (indexIncrement != haystack.length &&
+           !isKeyForIndex(key, index))
     {
-        ix = (ix + inc) & mask;
-        inc += 1;
+        index = (index + indexIncrement) & mask;
+        indexIncrement += 1;
     }
 
-    if (isKeyForIx(key, ix))
+    if (isKeyForIndex(key, index))
     {
-        return ix;
+        return index;
     }
     else
     {
@@ -61,7 +60,10 @@ size_t triangularFindIndex(alias hasher = hashOf, T, K)(const scope T[] haystack
         haystack[] = T(17);     // other values are 17
         haystack[$-1] = key;    // set key
 
-        assert(haystack.triangularFindIndex!(FNV!(64, true))(key) != haystack.length);
+        import digestion : hashOf2;
+        size_t startIx = 0;
+
+        assert(haystack.triangularProbeIndexFrom!(FNV!(64, true))(key, startIx) != haystack.length);
     }
 }
 

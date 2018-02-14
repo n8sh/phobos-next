d31cdd79c424be1d58dae03cb37cc39e92783567
Per Nordlöw <per.nordlow@gmail.com>
2018-02-14 22:55:17 +0100

    shorter

--- src/probing.d
+++ src/probing.d
@@ -2,9 +2,9 @@
  */
 module probing;
 
-/** Quadratic probing of `key` in `haystack` using triangular numbers.
+/** Search for `key` in `haystack` starting at `index` in steps of triangular numbers.
  *
- * haystack.length must be an even power of two.
+ * `haystack.length` must be a power of two.
  *
  * See also: https://fgiesen.wordpress.com/2015/02/22/triangular-numbers-mod-2n/
  */
@@ -13,33 +13,23 @@ size_t triangularProbeIndexFrom(alias hasher = hashOf, T, K)(const scope T[] hay
                                                              size_t index)
     @safe pure nothrow @nogc
 {
-    size_t isKeyForIndex(const scope K key,
-                         const scope size_t index)
-    {
-        return (haystack[index] is key || // hit slot
-                haystack[index].isNull); // free slot
-    }
-
     immutable typeof(return) mask = haystack.length - 1;
     assert((~mask ^ mask) == typeof(return).max); // std.math.isPowerOf2(haystack.length)
 
     // search using triangular numbers as increments
-    size_t indexIncrement = 1;
-    while (indexIncrement != haystack.length &&
-           !isKeyForIndex(key, index))
+    size_t indexIncrement = 0;
+    while (indexIncrement != haystack.length)
     {
-        index = (index + indexIncrement) & mask;
+        if (haystack[index] is key || // hit slot
+            haystack[index].isNull)   // free slot
+        {
+            return index;
+        }
         indexIncrement += 1;
+        index = (index + indexIncrement) & mask;
     }
 
-    if (isKeyForIndex(key, index))
-    {
-        return index;
-    }
-    else
-    {
-        return haystack.length;
-    }
+    return haystack.length;
 }
 
 @safe pure nothrow unittest
@@ -53,16 +43,18 @@ size_t triangularProbeIndexFrom(alias hasher = hashOf, T, K)(const scope T[] hay
     foreach (immutable lengthPower; 0 .. 20)
     {
         immutable length = 2^^lengthPower;
-        dln(length);
+        dln("length:", length, " lengthPower:", lengthPower);
 
-        immutable key = T(42);
+        immutable hitKey = T(42);
+        immutable missKey = T(43);
 
         // allocate and prepare haystack
         auto haystack = new T[length];
         haystack[] = T(17);     // other values are 17
-        haystack[$/2] = key;    // set key
+        haystack[$/2] = hitKey;    // set hitKey
 
-        assert(haystack.triangularProbeIndexFrom!(FNV!(64, true))(key, lengthPower) != haystack.length);
+        assert(haystack.triangularProbeIndexFrom!(FNV!(64, true))(hitKey, lengthPower) != haystack.length);
+        assert(haystack.triangularProbeIndexFrom!(FNV!(64, true))(missKey, lengthPower) == haystack.length);
     }
 }
 

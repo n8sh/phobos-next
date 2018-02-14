19af5a84db5a6c5eb1149ec892f2f4957c46ce74
Per Nordlöw <per.nordlow@gmail.com>
2018-02-14 18:15:42 +0100

    fix

--- src/probing.d
+++ src/probing.d
@@ -2,6 +2,8 @@ module probing;
 
 /** Quadratic probing of `key` in `haystack` using triangular numbers.
  *
+ * haystack.length must be an even power of two.
+ *
  * See also: https://fgiesen.wordpress.com/2015/02/22/triangular-numbers-mod-2n/
  */
 size_t triangularFindIndex(alias hasher = hashOf, T, K)(const scope T[] haystack,
@@ -16,28 +18,17 @@ size_t triangularFindIndex(alias hasher = hashOf, T, K)(const scope T[] haystack
     }
 
     immutable typeof(return) mask = haystack.length - 1;
-    assert((~mask ^ mask) == size_t.max); // isPowerOf2(haystack.length)
+    assert((~mask ^ mask) == typeof(return).max); // std.math.isPowerOf2(haystack.length)
 
     import digestion : hashOf2;
     size_t ix = hashOf2!(hasher)(key) & mask;
-
-    if (isKeyForIx(key, ix))
-    {
-        return ix;
-    }
-
-    // if not yet decided
-
-    ix = (ix + 1) & mask;   // modulo power of two
-    dln("ix:", ix);
 
     size_t inc = 1;
     while (!isKeyForIx(key, ix) &&
            inc != haystack.length)
     {
         ix = (ix + inc) & mask;
-        dln("ix:", ix);
-        inc *= 2;
+        inc += 1;
     }
 
     if (isKeyForIx(key, ix))
@@ -63,11 +54,12 @@ size_t triangularFindIndex(alias hasher = hashOf, T, K)(const scope T[] haystack
         immutable length = 2^^lengthPower;
         dln(length);
 
-        // allocate and perpare haystack
-        auto haystack = new T[length];
-        haystack[] = T(17);
         immutable key = T(42);
-        haystack[0] = key;
+
+        // allocate and prepare haystack
+        auto haystack = new T[length];
+        haystack[] = T(17);     // other values are 17
+        haystack[$-1] = key;    // set key
 
         assert(haystack.triangularFindIndex!(FNV!(64, true))(key) != haystack.length);
     }

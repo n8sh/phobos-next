c2cec9b4c7bf62e07ef6d92b603c3028f71c7804
Per Nordlöw <per.nordlow@gmail.com>
2018-02-16 19:06:19 +0100

    fix

--- src/hacks.d
+++ src/hacks.d
@@ -3,8 +3,10 @@ module hacks;
 import std.traits : isFunctionPointer, isDelegate, functionAttributes, FunctionAttribute, SetFunctionAttributes, functionLinkage;
 
 /** Return `T` assumed to be `pure`.
-    Copied from https://dlang.org/phobos/std_traits.html#SetFunctionAttributes.
-    */
+ *
+ * Copied from: https://dlang.org/phobos/std_traits.html#SetFunctionAttributes.
+ * See also: https://forum.dlang.org/post/hmucolyghbomttqpsili@forum.dlang.org
+ */
 auto assumePure(T)(T t)
     if (isFunctionPointer!T || isDelegate!T)
 {

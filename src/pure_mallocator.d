782ffb328bd1683ed104691e4d245a32300e6eee
Per Nordlöw <per.nordlow@gmail.com>
2018-02-17 13:00:43 +0100

    comment fixes

--- src/pure_mallocator.d
+++ src/pure_mallocator.d
@@ -12,18 +12,18 @@ struct PureMallocator
     pure nothrow @nogc:
 
     /**
-    The alignment is a static constant equal to $(D platformAlignment), which
-    ensures proper alignment for any D data type.
+     * The alignment is a static constant equal to $(D platformAlignment), which
+     * ensures proper alignment for any D data type.
     */
     enum uint alignment = platformAlignment;
 
     /**
-    Standard allocator methods per the semantics defined above. The
-    $(D deallocate) and $(D reallocate) methods are $(D @system) because they
-    may move memory around, leaving dangling pointers in user code. Somewhat
-    paradoxically, $(D malloc) is $(D @safe) but that's only useful to safe
-    programs that can afford to leak memory allocated.
-    */
+     * Standard allocator methods per the semantics defined above. The
+     * $(D deallocate) and $(D reallocate) methods are $(D @system) because they
+     * may move memory around, leaving dangling pointers in user code. Somewhat
+     * paradoxically, $(D malloc) is $(D @safe) but that's only useful to safe
+     * programs that can afford to leak memory allocated.
+     */
     pragma(inline, true)
     void[] allocate(size_t bytes) shared
         @trusted
@@ -50,7 +50,7 @@ struct PureMallocator
         @system
     {
         import core.memory : pureFree;
-        pureFree(b.ptr);
+        pureFree(b.ptr);        // b.length not needed
         return true;
     }
 
@@ -74,9 +74,9 @@ struct PureMallocator
     }
 
     /**
-    Returns the global instance of this allocator type. The C heap allocator is
-    thread-safe, therefore all of its methods and `it` itself are
-    $(D shared).
-    */
+     * Returns the global instance of this allocator type. The C heap allocator is
+     * thread-safe, therefore all of its methods and `it` itself are
+     * $(D shared).
+     */
     static shared PureMallocator instance;
 }

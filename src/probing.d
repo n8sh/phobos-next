eef4c04f3715eaada49d23b736c919adb1da1a8c
Per Nordlöw <per.nordlow@gmail.com>
2018-02-25 13:43:50 +0100

    add indexPredicate to triangularProbeFromIndex

--- src/probing.d
+++ src/probing.d
@@ -4,16 +4,15 @@ module probing;
 
 import std.functional : unaryFun;
 
-/** Search for a key in `haystack` matching `predicate` starting at `index` in
+/** Search for a key in `haystack` matching `elementPredicate` starting at `index` in
  * steps of triangular numbers, 0,1,3,6,10,15,21, ... .
  *
  * Returns: index into `haystack` upon hit, `haystack.length` upon miss.
  * Note: `haystack.length` must be a power of two (or 1 or zero).
  * See also: https://fgiesen.wordpress.com/2015/02/22/triangular-numbers-mod-2n/
  */
-size_t triangularProbeFromIndex(alias predicate, T)(const scope T[] haystack,
-                                                    size_t index)
-    if (is(typeof(unaryFun!predicate(T.init))))
+size_t triangularProbeFromIndex(alias elementPredicate, alias indexPredicate, T)(const scope T[] haystack, size_t index)
+    if (is(typeof(unaryFun!elementPredicate(T.init))))
 {
     immutable typeof(return) mask = haystack.length - 1;
     assert((~mask ^ mask) == typeof(return).max); // std.math.isPowerOf2(haystack.length)
@@ -22,7 +21,7 @@ size_t triangularProbeFromIndex(alias predicate, T)(const scope T[] haystack,
     size_t indexIncrement = 0;
     while (indexIncrement != haystack.length)
     {
-        if (unaryFun!predicate(haystack[index]))
+        if (unaryFun!elementPredicate(haystack[index]))
         {
             return index;
         }
@@ -46,8 +45,9 @@ size_t triangularProbeFromIndex(alias predicate, T)(const scope T[] haystack,
     auto haystack = new T[length];
 
     // any key misses
-    assert(haystack.triangularProbeFromIndex!(_ => (_ is hitKey ||
-                                                    _.isNull))(0) == haystack.length);
+    alias elementPredicate = _ => (_ is hitKey || _.isNull);
+    enum indexPredicate = null;
+    assert(haystack.triangularProbeFromIndex!(elementPredicate, indexPredicate)(0) == haystack.length);
 }
 
 /// generic case
@@ -66,13 +66,15 @@ size_t triangularProbeFromIndex(alias predicate, T)(const scope T[] haystack,
         haystack[] = T(17);     // make haystack full
         haystack[$/2] = hitKey;
 
+        alias elementHitPredicate = _ => (_ is hitKey || _.isNull);
+        alias elementMissPredicate = _ => (_ is missKey || _.isNull);
+        enum indexPredicate = null;
+
         // key hit
-        assert(haystack.triangularProbeFromIndex!(_ => (_ is hitKey ||
-                                                        _.isNull))(lengthPower) != haystack.length);
+        assert(haystack.triangularProbeFromIndex!(elementHitPredicate, indexPredicate)(lengthPower) != haystack.length);
 
         // key miss
-        assert(haystack.triangularProbeFromIndex!(_ => (_ is missKey ||
-                                                        _.isNull))(lengthPower) == haystack.length);
+        assert(haystack.triangularProbeFromIndex!(elementMissPredicate, indexPredicate)(lengthPower) == haystack.length);
     }
 }
 

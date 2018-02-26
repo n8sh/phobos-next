634da7305549402823dc48dec2c40f6ab9314f37
Per Nordlöw <per.nordlow@gmail.com>
2018-02-26 18:18:38 +0100

    fix

--- src/open_hashmap_or_hashset.d
+++ src/open_hashmap_or_hashset.d
@@ -883,6 +883,7 @@ struct OpenHashMapOrSet(K, V = void,
 private:
     T[] _bins;            // bin elements
     size_t _count;        // total number of non-null elements stored in `_bins`
+    size_t* _dels;        // elements in bin that has been deleted
 
     /** Returns: bin index of `key`. */
     pragma(inline, true)

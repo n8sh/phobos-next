9a4cc7f576d6c32867217e59601aede99f6bbff9
Per Nordlöw <per.nordlow@gmail.com>
2018-02-26 14:06:32 +0100

    revert stuff

--- src/open_hashmap_or_hashset.d
+++ src/open_hashmap_or_hashset.d
@@ -297,7 +297,7 @@ struct OpenHashMapOrSet(K, V = void,
         Returns: `true` if element is present, `false` otherwise.
     */
     pragma(inline, true)
-    bool contains()(const scope K key) const // template-lazy, WARNING: auto ref here makes things slow
+    bool contains()(const scope K key) const // template-lazy, auto ref here makes things slow
     {
         assert(!key.isNull);
         immutable startIndex = hashToIndex(hashOf2!(hasher)(key));
@@ -701,18 +701,12 @@ struct OpenHashMapOrSet(K, V = void,
 
     static if (hasValue)        // HashMap
     {
-        scope inout(V)* opBinaryRight(string op)(const scope K key) inout return // WARNING: auto ref here makes things slow
+        scope inout(V)* opBinaryRight(string op)(const scope K key) inout return // auto ref here makes things slow
             if (op == "in")
         {
             immutable startIndex = hashToIndex(hashOf2!(hasher)(key));
-            immutable hitIndex = _bins[].triangularProbeFromIndex!(_ => (keyOf(_) is key ||
-                                                                         keyOf(_).isNull))(startIndex);
-            if (hitIndex == _bins.length) // no free slot
-            {
-                dln(_bins.length);
-                return null;    // TODO change this behaviour to always have one slot free so this can't happen
-            }
-            if (!keyOf(_bins[hitIndex]).isNull) // if hit
+            immutable hitIndex = _bins[].triangularProbeFromIndex!(_ => keyOf(_) is key)(startIndex);
+            if (hitIndex != _bins.length) // if hit
             {
                 return cast(typeof(return))&_bins[hitIndex].value;
             }
@@ -810,7 +804,7 @@ struct OpenHashMapOrSet(K, V = void,
 
         /// Indexing.
         pragma(inline, true)    // LDC must have this
-        scope ref inout(V) opIndex()(const scope K key) inout return // WARNING: auto ref here makes things slow
+        scope ref inout(V) opIndex()(const scope K key) inout return // auto ref here makes things slow
         {
             immutable startIndex = hashToIndex(hashOf2!(hasher)(key));
             immutable hitIndex = _bins[].triangularProbeFromIndex!(_ => keyOf(_) is key)(startIndex);
@@ -1336,7 +1330,6 @@ pure nothrow @nogc unittest
 
         foreach (immutable key_; 0 .. n)
         {
-            dln("key_:", key_);
             const key = K(key_);
 
             static if (X.hasValue)
@@ -1652,5 +1645,4 @@ version(unittest)
     import std.typecons : Nullable;
     import digestx.fnv : FNV;
     import array_help : s;
-    import dbgio;
 }

5bf7984605f53541b3aa2186382ce19d392b5881
Per Nordlöw <per.nordlow@gmail.com>
2018-03-04 14:18:52 +0100

    fix

--- src/array_help.d
+++ src/array_help.d
@@ -184,7 +184,7 @@ size_t* makeReallocatedBitArrayZeroPadded(alias Allocator)(size_t* input,
         // TODO make faster by setting unaligned bits, whole words and then again unaligned bits
         foreach (immutable bitIndex; currentBitCount .. newBitCount)
         {
-            import core.bitop : bts;
+            import core.bitop : btr;
             btr(input, bitIndex);   // re(set) bit to zero
         }
 

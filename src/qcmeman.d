2dc7a855a3b84b163c0bcfa0379868aeab2b0db8
Per Nordlöw <per.nordlow@gmail.com>
2018-03-16 21:41:00 +0100

    make malloc @trusted

--- src/qcmeman.d
+++ src/qcmeman.d
@@ -6,7 +6,7 @@ extern(C)
     // qualified C memory allocations
     pure nothrow @nogc:
 
-    void* malloc(size_t size) @safe; // TODO make unsafe
+    void* malloc(size_t size) @trusted;
     void* calloc(size_t nmemb, size_t size) @safe;
     void* realloc(void* ptr, size_t size);
 

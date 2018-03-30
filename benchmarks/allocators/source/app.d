87a8a496d68e07c05c69475cf20a702f94c3d4b3
Per Nordlöw <per.nordlow@gmail.com>
2018-03-31 00:47:22 +0200

    first try

---
+++ benchmarks/allocators/source/app.d
@@ -0,0 +1,22 @@
+import std.stdio;
+import std.experimental.allocator;
+import std.experimental.allocator.building_blocks;
+
+void main()
+{
+    alias FList = FreeList!(GCAllocator, 0, unbounded);
+    alias A = Segregator!(
+        8, FreeList!(GCAllocator, 1, 8),
+        16, FreeList!(GCAllocator, 9, 16),
+        // 128, Bucketizer!(FList, 1, 128, 16),
+        // 256, Bucketizer!(FList, 129, 256, 32),
+        // 512, Bucketizer!(FList, 257, 512, 64),
+        // 1024, Bucketizer!(FList, 513, 1024, 128),
+        // 2048, Bucketizer!(FList, 1025, 2048, 256),
+        // 3584, Bucketizer!(FList, 2049, 3584, 512),
+        GCAllocator
+        );
+    A a;
+    auto x = a.allocate(7);
+    a.deallocate(x);
+}

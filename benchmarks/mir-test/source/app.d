21514b018c8817d11dd6d3e974c5169651143fed
Per Nordlöw <per.nordlow@gmail.com>
2018-04-02 15:52:39 +0200

    fix

--- benchmarks/mir-test/source/app.d
+++ benchmarks/mir-test/source/app.d
@@ -3,7 +3,7 @@ import mir.ndslice;
 
 void main()
 {
-    auto matrix = slice!int(2, 2);
+    const matrix = slice!int(2, 2);
     assert(matrix == [[0, 0], [0, 0]]);
     pragma(msg, typeof(matrix));
     writeln(matrix[]);

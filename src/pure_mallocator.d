1e9e67e7af4d209d78bbfe5380405c8bb6117da3
Per Nordlöw <per.nordlow@gmail.com>
2018-04-04 17:40:12 +0200

    add comment

--- src/pure_mallocator.d
+++ src/pure_mallocator.d
@@ -3,7 +3,10 @@ module pure_mallocator;
 import std.experimental.allocator.common;
 
 /**
-   The C heap allocator purified.
+ * The C heap allocator purified.
+ *
+ * Remove when https://github.com/dlang/phobos/pull/6411 has been merged that
+ * adds `calloc`.
  */
 struct PureMallocator
 {

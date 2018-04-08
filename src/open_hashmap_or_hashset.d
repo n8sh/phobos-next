1f4b2b05cb0165913343a877d4e6f5384c4214a9
Per Nordlöw <per.nordlow@gmail.com>
2018-04-08 22:28:00 +0200

    fix

--- src/open_hashmap_or_hashset.d
+++ src/open_hashmap_or_hashset.d
@@ -32,7 +32,7 @@ import pure_mallocator : PureMallocator;
  * See_Also: https://forum.dlang.org/post/ejqhcsvdyyqtntkgzgae@forum.dlang.org
  *
  * TODO contains should try to fill in holes if this is mutable (via static if)
- * similar to when sentinel-based search can be used
+ * similar to when sentinel-based search can be used in find
  *
  * TODO fix and activate constness of `byKeyValue`
  *

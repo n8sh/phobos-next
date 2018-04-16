ee19f198eac91954420055596cb70e1264d236a8
Per Nordlöw <per.nordlow@gmail.com>
2018-04-16 18:43:18 +0200

    fix

--- src/sso_string.d
+++ src/sso_string.d
@@ -2,6 +2,17 @@ module sso_string;
 
 struct SSOArray(T, uint smallSize)
 {
+    this(T[] elements)
+    {
+        assert(0, "construct from elements");
+    }
+private:
+    alias Large = T[];
+    struct Small
+    {
+        T[smallSize] data;
+        ubyte length;
+    }
     union
     {
         Large large;

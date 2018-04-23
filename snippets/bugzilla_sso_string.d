a19f1c718d7aefc990a347fbcebf0ec2f1203dd7
Per Nordlöw <per.nordlow@gmail.com>
2018-04-23 11:38:41 +0200

    fix

---
+++ snippets/bugzilla_sso_string.d
@@ -0,0 +1,99 @@
+/** Small-size-optimized (SSO) variant of `string`.
+ *
+ * Store on the stack if constructed with <= `smallCapacity` number of
+ * characters, otherwise on the GC heap.
+ *
+ * See_Also: https://forum.dlang.org/post/pb87rn$2icb$1@digitalmars.com
+ */
+struct SSOString
+{
+    private alias E = immutable(char); // immutable element type
+    private alias ME = char;           // mutable element type
+
+    pure nothrow @nogc:
+
+    scope E[] opSlice() const return @trusted // TODO @safe for -dip1000?
+    {
+        if (isLarge)
+        {
+            union RawLarge
+            {
+                Raw raw;
+                Large large;
+            }
+            RawLarge copy = void;
+            copy.large = cast(Large)large;
+            copy.raw.length /= 2; // adjust length
+            return copy.large;
+        }
+        else
+        {
+            return small.data[0 .. small.length/2]; // scoped. TODO use .ptr when proved stable
+        }
+    }
+
+    /// ditto
+    scope E[] opSlice(size_t i, size_t j) const return @trusted // TODO @safe for -dip1000?
+    {
+        pragma(inline, true);
+        return opSlice()[i .. j];
+    }
+
+    /** Get as `string`. */
+    @property scope E[] toString() const return @trusted
+    {
+        return opSlice();
+    }
+
+private:
+
+    /** Returns: `true` iff this is a large string, otherwise `false.` */
+    @property bool isLarge() const @trusted
+    {
+        pragma(inline, true);
+        return large.length & 1; // first bit discriminates small from large
+    }
+
+    struct Raw                  // same memory layout as `E[]`
+    {
+        size_t length;          // can be bit-fiddled without GC allocation
+        E* ptr;
+    }
+
+    alias Large = E[];
+
+    enum smallCapacity = Large.sizeof - Small.length.sizeof;
+    static assert(smallCapacity > 0, "No room for small source for E being " ~ E.stringof);
+    version(LittleEndian) // see: http://forum.dlang.org/posting/zifyahfohbwavwkwbgmw
+    {
+        struct Small
+        {
+            ubyte length; // TODO only first 4 bits are needed to represent a length between 0-15, use other 4 bits
+            E[smallCapacity] data;
+        }
+    }
+    else
+    {
+        static assert(0, "TODO Add BigEndian support and test");
+    }
+
+    union
+    {
+        Raw raw;
+        Large large;
+        Small small;
+    }
+}
+
+///
+@safe pure nothrow @nogc unittest
+{
+    alias S = SSOString;
+
+    // TODO this shouldn't compile
+    string f1() @safe pure nothrow @nogc
+    {
+        S x;
+        return x[];             // TODO should fail with -dip1000
+    }
+}

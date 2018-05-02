93ae4a80f6ebf411133cb0c3e0311e07e7d7a430
Per Nordlöw <per.nordlow@gmail.com>
2018-05-02 10:03:32 +0200

    *** empty log message ***

--- src/cconv.d
+++ src/cconv.d
@@ -17,10 +17,13 @@ string toString(const double value,
     return fromStringz(buffer.ptr);
 }
 
+///
 @safe pure nothrow unittest
 {
-    // import dbgio;
-    // dln(1234567.123456789123456789.toString(100));
+    assert(3.14.toString(3) == `3.14`);
+    assert(3.141.toString(3) == `3.14`);
+    assert(3.141.toString(4) == `3.141`);
+    assert(3.141.toString(5) == `3.141`);
     assert(1234567.123456789123456789.toString(20) == `1234567.1234567892`);
 }
 

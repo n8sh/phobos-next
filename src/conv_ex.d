6a8a174532a178871e5b2a1ed1db6445981c0156
Per Nordlöw <per.nordlow@gmail.com>
2018-06-18 22:49:23 +0200

    *** empty log message ***

--- src/conv_ex.d
+++ src/conv_ex.d
@@ -8,6 +8,7 @@ import std.range : isInputRange, ElementType;
  *
  * TODO add overload for std.conv : to with default value to make this @nogc
  *
+ * See_Also: https://forum.dlang.org/post/kdjbkqbnspzshdqtsntg@forum.dlang.org
  * See_Also: http://forum.dlang.org/post/tsszfamjalzviqjhpdcr@forum.dlang.org
  */
 T toDefaulted(T, S, U)(S value, /*lazy*/ U defaultValue)

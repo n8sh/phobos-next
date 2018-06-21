52ce0ed32506739c01479661496f2a7ad9eeca4d
Per Nordlöw <per.nordlow@gmail.com>
2018-06-22 00:26:01 +0200

    *** empty log message ***

--- src/conv_ex.d
+++ src/conv_ex.d
@@ -79,7 +79,7 @@ if (is(T == enum))
 ///
 @safe pure unittest
 {
-    enum E { unknown, x, y, z, z_, }
+    enum E { unknown, x, y, z, }
     assert(E.x.toString == "x");
     assert(E.y.toString == "y");
 }

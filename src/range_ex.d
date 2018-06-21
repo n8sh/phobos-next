e764c33f7c2cab6b0d21c05959a262778a502e20
Per Nordlöw <per.nordlow@gmail.com>
2018-06-21 12:31:10 +0200

    *** empty log message ***

--- src/range_ex.d
+++ src/range_ex.d
@@ -161,17 +161,18 @@ alias takeBack = backPop;
 }
 
 /** Sliding Splitter.
-    See_Also: http://forum.dlang.org/thread/dndicafxfubzmndehzux@forum.dlang.org
-    See_Also: http://forum.dlang.org/thread/uzrbmjonrkixojzflbig@forum.dlang.org#epost-viwkavbmwouiquoqwntm:40forum.dlang.org
-
-    TODO Use size_t for _lower and _upper instead and reserve _upper =
-    size_t.max for emptyness?
-
-    TODO Should lower and upper operate on code units instead of code
-    point if isNarrowString!Range. ?
-
-    TODO generalize with stride
-*/
+ *
+ * See_Also: http://forum.dlang.org/thread/dndicafxfubzmndehzux@forum.dlang.org
+ * See_Also: http://forum.dlang.org/thread/uzrbmjonrkixojzflbig@forum.dlang.org#epost-viwkavbmwouiquoqwntm:40forum.dlang.org
+ *
+ * TODO Use size_t for _lower and _upper instead and reserve _upper = size_t.max
+ * for emptyness?
+ *
+ * TODO Should lower and upper operate on code units instead of code point if
+ * isNarrowString!Range. ?
+ *
+ * TODO generalize with stride
+ */
 struct SlidingSplitter(Range)
     if (isSomeString!Range ||
         (hasSlicing!Range &&

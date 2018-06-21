6583171cd16a39055a49b533a1f4df8178e5a9c8
Per Nordlöw <per.nordlow@gmail.com>
2018-06-22 00:25:07 +0200

    *** empty log message ***

--- src/conv_ex.d
+++ src/conv_ex.d
@@ -60,6 +60,30 @@ if (is(T == enum))
     assert("_".toDefaulted!(E)(E.init) == E.unknown);
 }
 
+/** Faster implementation of `std.conv.to`.
+ */
+string toString(T)(T value)
+if (is(T == enum))
+{
+    import std.conv : to;
+    final switch (value)
+    {
+        static foreach (member; __traits(allMembers, T)) // instead of slower `EnumMembers`
+        {
+        case __traits(getMember, T, member):
+        return __traits(getMember, T, member).to!string;
+        }
+    }
+}
+
+///
+@safe pure unittest
+{
+    enum E { unknown, x, y, z, }
+    assert(E.x.toString == "x");
+    assert(E.y.toString == "y");
+}
+
 /** More tolerant variant of `std.conv.to`.
 */
 auto tolerantTo(U, S)(S value,

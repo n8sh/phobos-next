ae149289d5de51f07eb6e59bab036ec3a6ebd4ef
Per Nordlöw <per.nordlow@gmail.com>
2018-06-26 13:15:24 +0200

    *** empty log message ***

--- src/enum_ex.d
+++ src/enum_ex.d
@@ -63,3 +63,18 @@ if (is(T == enum))
     assert(E.y.toStringFaster == "y");
     assert(E.z.toStringFaster == "z");
 }
+
+/** Faster implementation of `std.conv.to` for enumerations with no aliases.
+ */
+string toStringNonAliases(T)(T value) @safe pure nothrow @nogc
+if (is(T == enum))              // TODO check for no aliases
+{
+    final switch (value)
+    {
+        static foreach (member; __traits(allMembers, T))
+        {
+        case __traits(getMember, T, member):
+            return member;
+        }
+    }
+}

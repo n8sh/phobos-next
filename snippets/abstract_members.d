8a0834b17281541dce7682464a48758b909400b3
Per Nordlöw <per.nordlow@gmail.com>
2018-07-02 13:33:29 +0200

    *** empty log message ***

--- snippets/abstract_members.d
+++ snippets/abstract_members.d
@@ -1,2 +1,35 @@
 /// Test diagnostics for abstract members.
 module abstract_members;
+
+enum Rel { subkindOf, partOf }
+
+@safe pure:
+
+class Edge
+{
+    @safe pure:
+    abstract Rel rel() const nothrow @nogc;
+}
+
+class SubkindOf : Edge
+{
+    @safe pure:
+    override Rel rel() const nothrow @nogc
+    {
+        return Rel.subkindOf;
+    }
+}
+
+class PartOf : Edge
+{
+    override Rel rel() const nothrow @nogc
+    {
+        return Rel.subkindOf;
+    }
+}
+
+@safe pure nothrow unittest
+{
+    auto edge = new SubkindOf();
+    assert(edge.rel == Rel.subkindOf);
+}

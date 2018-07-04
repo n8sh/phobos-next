241baee01e917b1a9d4f5b2a44b15d2e7cb57a4f
Per Nordlöw <per.nordlow@gmail.com>
2018-07-04 11:36:59 +0200

    *** empty log message ***

--- src/open_hashmap_or_hashset.d
+++ src/open_hashmap_or_hashset.d
@@ -31,7 +31,7 @@ import pure_mallocator : PureMallocator;
  * See_Also: https://en.wikipedia.org/wiki/Lazy_deletion
  * See_Also: https://forum.dlang.org/post/ejqhcsvdyyqtntkgzgae@forum.dlang.org
  *
- * TODO support key being structs with const members: `struct Rel { const string name; }`
+ * TODO modify existing unittest for `struct Rel { const string name; }`
  *
  * TODO use allocator.dispose() instead of allocator.deallocate() as in
  * https://github.com/dlang-community/containers

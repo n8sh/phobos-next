f9f34d2b11d18d170a34a7df87473578a3240ed5
Per Nordlöw <per.nordlow@gmail.com>
2018-08-20 13:40:06 +0200

    *** empty log message ***

---
+++ src/expected.d
@@ -0,0 +1,38 @@
+module expected;
+
+@safe pure:
+
+struct Expected(Result, Error)
+{
+    this(Result result) @trusted
+    {
+        _result = result;
+        _ok = true;
+    }
+
+    this(Error error) @trusted
+    {
+        _error = error;
+        _ok = true;
+    }
+
+    bool isOk() const { return _ok; }
+
+    void orElse(alias elseWork)()
+    {
+    }
+
+private:
+    union
+    {
+        Result _result;
+        Error _error;
+    }
+
+    bool _ok = false;
+}
+
+@safe pure nothrow @nogc unittest
+{
+    auto x = Expected!(string, int)();
+}

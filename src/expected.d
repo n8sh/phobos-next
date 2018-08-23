c06ffb5a8982b579cd28edfcc64f7a1f439d5fae
Per Nordlöw <per.nordlow@gmail.com>
2018-08-23 14:05:15 +0200

    *** empty log message ***

--- src/expected.d
+++ src/expected.d
@@ -40,8 +40,8 @@ private struct Unexpected(E)
 /** Union (sum) type of either an expected value of type `T` or an unexpected
  * value of type `E` (being an instance of type `Unexpected!E`).
  *
- * `E` is typically an error code (for instance C `errno` int) or a subclass of
- * `Exception`.
+ * `E` is typically an error code (for instance C/C++'s' `errno` int) or a
+ * subclass of `Exception`.
  *
  * See_Also: https://www.youtube.com/watch?v=nVzgkepAg5Y
  */

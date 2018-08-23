dafb6ba3e7e40d3fabcbedc87a9114f46bbad42e
Per Nordlöw <per.nordlow@gmail.com>
2018-08-23 13:22:48 +0200

    *** empty log message ***

--- src/expected.d
+++ src/expected.d
@@ -2,6 +2,8 @@
  *
  * See_Also: https://github.com/dlang/phobos/pull/6665
  *
+ * TODO merge in https://run.dlang.io/?compiler=dmd&source=%2F%2Bdub.sdl:%0Adependency%20%22sumtype%22%20version%3D%22~%3E0.5.2%22%0A%2B%2F%0Aimport%20std.stdio;%0Aimport%20std.traits%20:%20isInstanceOf;%0A%0Astruct%20S%20%7B%0A%20%20%20%20@disable%20this();%0A%20%20%20%20~this()%20pure%20@safe%20%7B%20%7D%0A%20%20%20%20this(int%20i)%20pure%20@safe%20%7B%20this.i%20%3D%20i;%20%7D%0A%20%20%20%20void%20opAssign(S)%20%7B%20assert(false);%20%7D%0A%20%20%20%20immutable%20int%20i;%0A%20%20%20%20invariant%20%7B%20assert(i%20!%3D%200);%20%7D%0A%7D%0A%0Aauto%20toInt(string%20str)%20pure%20@safe%20%7B%0A%20%20%20%20try%20%7B%0A%20%20%20%20%20%20%20%20import%20std.conv:%20to;%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20return%20expected!(S,%20string)(S(str.to!int));%0A%20%20%20%20%7D%20catch%20(Exception%20ex)%20%7B%0A%20%20%20%20%20%20%20%20return%20unexpected!(S,%20string)(ex.msg);%0A%20%20%20%20%7D%0A%7D%0A%0Avoid%20main()%20@safe%20%7B%0A%20%20%20%20toInt(%221%22).thenElse!((S%20val)%20%7B%0A%20%20%20%20%20%20%20%20writeln(val.i);%0A%20%20%20%20%7D,%20(string%20error)%20%7B%0A%20%20%20%20%20%20%20%20writeln(error);%0A%20%20%20%20%7D);%0A%20%20%20%20toInt(%221fff%22).thenElse!((S%20val)%20%7B%0A%20%20%20%20%20%20%20%20writeln(val.i);%0A%20%20%20%20%7D,%20(string%20error)%20%7B%0A%20%20%20%20%20%20%20%20writeln(error);%0A%20%20%20%20%7D);%0A%7D%0A%0Astruct%20Unexpected(T)%20%7B%0A%20%20%20%20T%20value;%0A%7D%0A%0Astruct%20Expected(T_,%20U_)%20%7B%0A%20%20%20%20import%20sumtype%20:%20SumType;%0A%20%20%20%20%0A%20%20%20%20alias%20T%20%3D%20T_,%20U%20%3D%20U_;%0A%20%20%20%20%0A%20%20%20%20SumType!(T,%20Unexpected!U)%20sum;%0A%20%20%20%20%0A%20%20%20%20this()(auto%20ref%20T%20value)%20%7B%0A%20%20%20%20%20%20%20%20sum%20%3D%20value;%0A%20%20%20%20%7D%0A%20%20%20%20%0A%20%20%20%20this()(auto%20ref%20Unexpected!U%20value)%20%7B%0A%20%20%20%20%20%20%20%20sum%20%3D%20value;%0A%20%20%20%20%7D%0A%7D%0A%0Atemplate%20thenElse(alias%20thenDo,%20alias%20elseDo)%0A%7B%0A%20%20%20%20auto%20thenElse(E)(E%20expected)%0A%20%20%20%20if%20(isInstanceOf!(Expected,%20E))%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20import%20sumtype%20:%20match;%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20return%20expected.sum.match!((E.T%20value)%20%3D%3E%20thenDo(value),%20(Unexpected!(E.U)%20error)%20%3D%3E%20elseDo(error.value));%0A%20%20%20%20%7D%0A%7D%0A%0Aauto%20expected(T,%20U)(auto%20ref%20T%20value)%20%7B%0A%20%20%20%20return%20Expected!(T,%20U)(value);%0A%7D%0A%0Aauto%20unexpected(T,%20U)(auto%20ref%20U%20value)%7B%0A%20%20%20%20return%20Expected!(T,%20U)(Unexpected!U(value));%0A%7D&args=-unittest%20-debug
+ *
  * TODO https://dlang.org/phobos/std_typecons.html#.apply
  *
  * TODO I'm not convinced about the naming
@@ -33,12 +35,6 @@ private struct Unexpected(E)
 {
     E value;
     alias value this;
-}
-
-/// Instantiator for `Unexpected`.
-auto unexpected(T, E)(auto ref E unexpectedValue)
-{
-    return Expected!(T, E)(Unexpected!E(unexpectedValue));
 }
 
 /** Union (sum) type of either an expected value of type `T` or an unexpected
@@ -189,9 +185,16 @@ private:
     bool _ok = true;
 }
 
-auto expected(T, E)(auto ref T value)
+/// Insantiator for `Expected` from an expected value `expectedValue.`
+auto expected(T, E)(auto ref T expectedValue)
 {
-    return Expected!(T, E)(value);
+    return Expected!(T, E)(expectedValue);
+}
+
+/// Insantiator for `Expected` from an unexpected value `unexpectedValue.`
+auto unexpected(T, E)(auto ref E unexpectedValue)
+{
+    return Expected!(T, E)(Unexpected!E(unexpectedValue));
 }
 
 @safe pure nothrow @nogc unittest

b25a60433ff1aef57a884422f197546405d4b356
Per Nordlöw <per.nordlow@gmail.com>
2018-09-11 22:40:48 +0200

    *** empty log message ***

--- benchmarks/containers/source/app.d
+++ benchmarks/containers/source/app.d
@@ -33,7 +33,7 @@ void main()
 
     import std.conv : to;
 
-    immutable n = 1024*1024;
+    immutable n = 400_000;
 
     auto testSource = iota(0, n).array;
     const useRandomShuffledSource = true;

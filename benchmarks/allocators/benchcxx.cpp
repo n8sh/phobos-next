00d99fd8d9ac76fd3db71e9d211c6a3f4ceeb367
Per Nordlöw <per.nordlow@gmail.com>
2018-09-18 16:17:20 +0200

    *** empty log message ***

---
+++ benchmarks/allocators/benchcxx.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <sys/time.h>
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+    const auto value = "alpha_beta_gamma_delta";
+    const auto count = 1000000;
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    const char* iptr = nullptr;
+    for (auto i = 0; i < count; ++i)
+    {
+        auto x = new string(value);
+        iptr = x->c_str();
+    }
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    double dif = duration_cast<nanoseconds>( t2 - t1 ).count() / count;
+    cout << "Allocating took " << dif " ns/string";
+    return 0;
+}

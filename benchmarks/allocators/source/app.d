83a75b55b8d0d300406c83a635be900e1e497f05
Per Nordlöw <per.nordlow@gmail.com>
2018-09-21 11:23:23 +0200

    *** empty log message ***

--- benchmarks/allocators/source/app.d
+++ benchmarks/allocators/source/app.d
@@ -47,7 +47,7 @@ class Graph
     }
 
     Type make(Type, Args...)(Args args)
-        if (is(Type == class))
+    if (is(Type == class))
     {
         pragma(inline, true);
         return _allocator.make!Type(args);

2315ba1f6cb2cf725debb8e1fc8b143e9ed5095e
Per Nordlöw <per.nordlow@gmail.com>
2018-09-24 11:57:42 +0200

    *** empty log message ***

--- snippets/gc_overhead.d
+++ snippets/gc_overhead.d
@@ -1,5 +1,13 @@
 import std.stdio;
 
+void* mallocAndFreeBytes(size_t byteCount)()
+{
+    import core.memory : pureMalloc, pureFree;
+    void* ptr = pureMalloc(byteCount);
+    pureFree(ptr);
+    return ptr;                 // for side-effects
+}
+
 void main(string[] args)
 {
     import std.datetime.stopwatch : benchmark;
@@ -8,13 +16,19 @@ void main(string[] args)
     immutable benchmarkCount = 1;
 
     // GC
-    foreach (const i; 0 .. 10)
+    static foreach (const i; 0 .. 10)
     {
-        import core.memory : GC;
-        const byteCount = i*100_000_000;
-        const array = new byte[byteCount]; // one Gig
-        const Duration[1] results = benchmark!(GC.collect)(benchmarkCount);
-        writefln("%s bytes: Calling GC.collect() took %s nsecs after %s",
-                 byteCount, cast(double)results[0].total!"nsecs"/benchmarkCount, array.ptr);
+        {
+            enum byteCount = i*100_000_000;
+            const Duration[1] resultsC = benchmark!(mallocAndFreeBytes!(i))(benchmarkCount);
+            writefln("%s bytes: Calling mallocAndFreeBytes took %s nsecs",
+                     byteCount, cast(double)resultsC[0].total!"nsecs"/benchmarkCount);
+
+            import core.memory : GC;
+            const dArray = new byte[byteCount]; // one Gig
+            const Duration[1] resultsD = benchmark!(GC.collect)(benchmarkCount);
+            writefln("%s bytes: Calling GC.collect() took %s nsecs after %s",
+                     byteCount, cast(double)resultsD[0].total!"nsecs"/benchmarkCount, dArray.ptr);
+        }
     }
 }

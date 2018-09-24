b285b44bb5466911bc657501584fc959dbd441be
Per Nordlöw <per.nordlow@gmail.com>
2018-09-24 11:40:55 +0200

    *** empty log message ***

--- snippets/gc_overhead.d
+++ snippets/gc_overhead.d
@@ -8,17 +8,11 @@ void main(string[] args)
 
     immutable benchmarkCount = 100;
 
-    const Duration[1] results = benchmark!(GC.collect)(benchmarkCount);
-    writefln("Calling GC.collect() took %s nsecs",
-             cast(double)results[0].total!"nsecs"/benchmarkCount);
-
-    const array1 = new char[1_000_000_000]; // one Gig
-    const Duration[1] results1 = benchmark!(GC.collect)(benchmarkCount);
-    writefln("Calling GC.collect() took %s nsecs after %s",
-             cast(double)results1[0].total!"nsecs"/benchmarkCount, array1.ptr);
-
-    const array2 = new char[1_000_000_000]; // one Gig
-    const Duration[1] results2 = benchmark!(GC.collect)(benchmarkCount);
-    writefln("Calling GC.collect() took %s nsecs after %s",
-             cast(double)results[0].total!"nsecs"/benchmarkCount, array2.ptr);
+    foreach (const i; 0 .. 10)
+    {
+        const array = new char[i*100_000_000]; // one Gig
+        const Duration[1] results = benchmark!(GC.collect)(benchmarkCount);
+        writefln("Calling GC.collect() took %s nsecs after %s",
+                 cast(double)results[0].total!"nsecs"/benchmarkCount, array.ptr);
+    }
 }

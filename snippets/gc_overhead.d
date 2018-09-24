15e13b035638d2a02e4136235412a761c07c70e2
Per Nordlöw <per.nordlow@gmail.com>
2018-09-24 11:23:18 +0200

    *** empty log message ***

--- snippets/gc_overhead.d
+++ snippets/gc_overhead.d
@@ -2,5 +2,13 @@ import std.traits, std.meta, std.range, std.algorithm, std.stdio;
 
 void main(string[] args)
 {
+    import std.datetime.stopwatch : benchmark;
+    import core.time : Duration;
+    import core.memory : GC;
 
+    immutable benchmarkCount = 100_000;
+    const Duration[1] results = benchmark!(GC.collect)(benchmarkCount);
+
+    writefln("Calling GC.collect() took %s ns",
+             cast(double)results[0].total!"nsecs"/benchmarkCount);
 }

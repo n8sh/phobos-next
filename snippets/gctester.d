e4feba029b526f890ee12f277e54e6f99bbdb521
Per Nordlöw <per.nordlow@gmail.com>
2018-10-05 02:59:25 +0200

    *** empty log message ***

--- snippets/gctester.d
+++ snippets/gctester.d
@@ -4,6 +4,38 @@ import std.stdio;
 
 void main(string[] args)
 {
+    benchmarkAllocateStrings();
+}
+
+import std.datetime.stopwatch : benchmark;
+
+void benchmarkAllocateStrings() @trusted
+{
+    immutable benchmarkCount = 100_000;
+
+    static immutable value = "alpha_beta_gamma_delta";
+    immutable(char)* latestPtr;
+
+    void testNewAllocation() @safe pure
+    {
+        auto x = value.idup;
+        latestPtr = &x[0];
+    }
+
+    import core.time : Duration;
+    import core.memory : GC;
+
+    // GC.disable();
+    const Duration[1] results = benchmark!(testNewAllocation)(benchmarkCount);
+    // GC.enable();
+
+    writefln("Allocating string took %s ns",
+             cast(double)results[0].total!"nsecs"/benchmarkCount);
+}
+
+void simpleBenchmark()
+{
+
     const n = 1024*1024;
     alias T = long;
     size_t xx = 0;

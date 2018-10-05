4432c233ab958a0d8b0d09b6e6152af7c1d70264
Per Nordlöw <per.nordlow@gmail.com>
2018-10-05 09:20:54 +0200

    *** empty log message ***

--- snippets/gctester.d
+++ snippets/gctester.d
@@ -30,7 +30,7 @@ void benchmarkAllocate(T)() @trusted
     const Duration[1] results = benchmark!(testNewAllocation)(benchmarkCount);
     // GC.enable();
 
-    writefln("Allocating a %s took %s ns ptrSum:%s",
+    writefln("- new a() %s took %s ns ptrSum:%s",
              T.stringof,
              cast(double)results[0].total!"nsecs"/benchmarkCount,
              ptrSum);

e73f683597c16f1d113f076e03fce4e9113d16ac
Per Nordlöw <per.nordlow@gmail.com>
2018-10-06 21:36:00 +0200

    *** empty log message ***

--- snippets/gctester.d
+++ snippets/gctester.d
@@ -86,7 +86,8 @@ size_t benchmarkAllocation(E, uint n)() @trusted
 
     writef("-");
 
-    writef(" size:%4s:  new:%4.1f ns/w  GC.malloc:%4.1f ns/w  pureMalloc:%4.1f ns/w  pureCalloc:%4.1f ns/w", T.sizeof,
+    writef(" size:%4s:  new:%4.1f ns/w  GC.malloc:%4.1f ns/w  pureMalloc:%4.1f ns/w  pureCalloc:%4.1f ns/w",
+           T.sizeof,
            cast(double)results[0].total!"nsecs"/(benchmarkCount*iterationCount*n),
            cast(double)results[1].total!"nsecs"/(benchmarkCount*iterationCount*n),
            cast(double)results[2].total!"nsecs"/(benchmarkCount*iterationCount*n),

9f48eb9828bb517fba80282ea75caedf4e427d72
Per Nordlöw <per.nordlow@gmail.com>
2018-10-19 23:48:01 +0200

    bump

--- src/pure_mallocator.d
+++ src/pure_mallocator.d
@@ -1,6 +1,6 @@
 module pure_mallocator;
 
-static if (__VERSION__ >= 2083)
+static if (__VERSION__ >= 2084)
 {
     static assert(0, "Use std.experimental.allocator.building_blocks.mallocator instead of this module");
 }

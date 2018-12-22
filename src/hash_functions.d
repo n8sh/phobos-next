c9f1cc4aab1c7105e9bd72c2c9fdf05886a5ce40
Per Nordlöw <per.nordlow@gmail.com>
2018-12-22 23:40:17 +0100

    *** empty log message ***

--- src/hash_functions.d
+++ src/hash_functions.d
@@ -21,6 +21,91 @@ unittest
     auto x = typeidHashOf(cast(int)17);
 }
 
+hash_t hashOfPolymorphic(Class)(Class aClassInstance) @trusted pure nothrow @nogc
+    if (is(Class == class))
+    {
+        assert(Class.alignof == 8);
+        return (cast(hash_t)(cast(void*)typeid(Class)) >> 3) ^ hashOf(aClassInstance);
+    }
+
+version(unittest)
+{
+    private static:
+
+    class Thing
+    {
+    }
+
+    class Expr : Thing
+    {
+        @safe pure nothrow @nogc:
+        alias Data = string;
+        this(Data data)
+        {
+            this.data = data;
+        }
+        @property override hash_t toHash() const @safe pure nothrow @nogc
+        {
+            return hashOf(data);
+        }
+        Data data;
+    }
+
+    class NounExpr : Expr
+    {
+        @safe pure nothrow @nogc:
+        this(Data data)
+        {
+            super(data);
+        }
+        @property override hash_t toHash() const @safe pure nothrow @nogc
+        {
+            return hashOf(data);
+        }
+    }
+
+    class Year : Thing
+    {
+        @safe pure nothrow @nogc:
+        alias Data = long;
+        @property override hash_t toHash() const @safe pure nothrow @nogc
+        {
+            return hashOf(data);
+        }
+        Data data;
+    }
+}
+
+@safe pure nothrow unittest
+{
+    auto car1 = new Expr("car");
+    auto car2 = new Expr("car");
+    auto bar1 = new Expr("bar");
+    auto ncar = new NounExpr("car");
+
+    void testEqual() @safe pure nothrow @nogc
+    {
+        assert(hashOf(car1) == hashOf(car2));
+        assert(hashOfPolymorphic(car1) == hashOfPolymorphic(car2));
+    }
+
+    void testDifferent1() @safe pure nothrow @nogc
+    {
+        assert(hashOf(car1) != hashOf(bar1));
+        assert(hashOfPolymorphic(car1) != hashOfPolymorphic(bar1));
+    }
+
+    void testDifferent2() @safe pure nothrow @nogc
+    {
+        assert(hashOf(car1) == hashOf(ncar));
+        assert(hashOfPolymorphic(car1) != hashOfPolymorphic(ncar));
+    }
+
+    testEqual();
+    testDifferent1();
+    testDifferent2();
+}
+
 pure @nogc:
 
 /** Dummy-hash for benchmarking performance of HashSet. */

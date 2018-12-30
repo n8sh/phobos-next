a1b0b350a80c027f561e24a11368c06ab29ad52a
nordlow <per.nordlow@gmail.com>
2018-12-30 09:51:01 +0100

    *** empty log message ***

--- src/pythagorean_triples.d
+++ src/pythagorean_triples.d
@@ -2,7 +2,7 @@ module pythogorean_triples;
 
 import std.algorithm;
 
-struct PossiblePythags(T)
+struct PossiblePythagoreanTriples(T)
 {
     struct Triple
     {
@@ -33,7 +33,7 @@ struct PossiblePythags(T)
 
 auto pythagoreanTriples(T = size_t)()
 {
-    return PossiblePythags!T().filter!(p => p.x * p.x + p.y * p.y == p.z * p.z);
+    return PossiblePythagoreanTriples!T().filter!(p => p.x * p.x + p.y * p.y == p.z * p.z);
 }
 
 @safe unittest

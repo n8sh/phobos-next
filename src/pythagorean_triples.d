5367a7f11687e5d2b74d2076b13329ddc02bef00
nordlow <per.nordlow@gmail.com>
2018-12-30 09:19:34 +0100

    *** empty log message ***

--- src/pythagorean_triples.d
+++ src/pythagorean_triples.d
@@ -1,1 +1,37 @@
 module pythogorean_triples;
+
+struct PossiblePythags(T)
+{
+    struct Triple
+    {
+    }
+    T z = 2;
+    T x = 1;
+    T y = 1;
+
+    auto front()
+    {
+        return tuple!(T, "x", T, "y", T, "z")(x, y, z);
+    }
+
+    void popFront()
+    {
+        if (++y == z)
+        {
+            if (++x == z)
+            {
+                ++z;
+                x = 1;
+            }
+            y = x;
+        }
+    }
+
+    enum empty = false;
+}
+
+auto pythagrange(T = size_t)()
+{
+    return PossiblePythags!T()
+    .filter!(p => p.x * p.x + p.y * p.y == p.z * p.z);
+}

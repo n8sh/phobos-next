4c3e3ea1d0c1d253efbc9bab5b28f8f34b88c081
nordlow <per.nordlow@gmail.com>
2019-01-01 16:34:57 +0100

    *** empty log message ***

--- snippets/return_ref_check.d
+++ snippets/return_ref_check.d
@@ -2,23 +2,37 @@ struct S(E)
 {
     @safe pure nothrow @nogc:
 
-    @property ref E x() return { return _x; }
-    @property E* xptr() return { return &_x; }
+    @property ref E x() return { return _small; }
+    @property E* xptr() return { return &_small; }
 
-    inout(E)[] opSlice() inout return @trusted { return (&_x)[0 .. 1]; }
+    inout(E)[] opSlice() inout return @trusted { return (&_small)[0 .. 1]; }
 
-    private E _x;
+    static if (is(E == char))
+    {
+        inout(E)[] toString() inout return @trusted { return opSlice(); }
+    }
+
+private:
+    union
+    {
+        E _small;
+    }
 }
 
 ///
 @safe pure unittest
 {
-    alias E = int;
+    alias E = char;
     static assert(!__traits(compiles, { ref E escape_x() { S s; return s.x; }}));
     static assert(!__traits(compiles, { E* escape_xptr() { S s; return s.xptr; } }));
     E[] escape_opSlice()
     {
         S!E s;
         return s[];
+    }
+    E[] escape_toString()
+    {
+        S!E s;
+        return s.toString;
     }
 }

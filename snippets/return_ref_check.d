c74932171c81538fd917044752bfd87a67f1a002
nordlow <per.nordlow@gmail.com>
2019-01-01 16:30:15 +0100

    *** empty log message ***

--- snippets/return_ref_check.d
+++ snippets/return_ref_check.d
@@ -5,9 +5,10 @@ struct S(E)
     @property ref E x() return { return _x; }
     @property E* xptr() return { return &_x; }
 
-    inout(E)[] opSlice() inout return @trusted { return (&_x)[0 .. 1]; }
+    inout(E)[] opSlice() inout return @trusted { return _small[]; }
 
     private E _x;
+    private E[2] _small;
 }
 
 ///
@@ -16,9 +17,9 @@ struct S(E)
     alias E = int;
     static assert(!__traits(compiles, { ref E escape_x() { S s; return s.x; }}));
     static assert(!__traits(compiles, { E* escape_xptr() { S s; return s.xptr; } }));
-    // E[] escape_opSlice()
-    // {
-    //     S!E s;
-    //     return s[];
-    // }
+    E[] escape_opSlice()
+    {
+        S!E s;
+        return s[];
+    }
 }

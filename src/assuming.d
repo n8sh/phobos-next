269d56c494faf60f148731999a6ca6513465261e
nordlow <per.nordlow@gmail.com>
2019-01-02 14:15:32 +0100

    Move to assuming

--- src/assuming.d
+++ src/assuming.d
@@ -1,19 +1,50 @@
 module assuming;
 
+import std.traits : isFunctionPointer, isDelegate, functionAttributes,
+    FunctionAttribute, SetFunctionAttributes, functionLinkage;
+
 /**
    * See_Also: http://forum.dlang.org/post/nq4eol$2h34$1@digitalmars.com
    * See_Also: https://dpaste.dzfl.pl/8c5ec90c5b39
    */
-void assumeNogc(alias Func, T...)(T xs)
-    @nogc
+void assumeNogc(alias Func, T...)(T xs) @nogc
 {
-    import std.traits : isFunctionPointer, isDelegate, functionAttributes, FunctionAttribute, SetFunctionAttributes, functionLinkage;
-    static auto assumeNogcPtr(T)(T f)
-        if (isFunctionPointer!T ||
-            isDelegate!T)
+    static auto assumeNogcPtr(T)(T f) if (isFunctionPointer!T || isDelegate!T)
     {
         enum attrs = functionAttributes!T | FunctionAttribute.nogc;
         return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs)) f;
-    } {}
+    }
+
+    {
+    }
     assumeNogcPtr(&Func!T)(xs);
+}
+
+/** Return `T` assumed to be `pure`.
+ *
+ * Copied from: https://dlang.org/phobos/std_traits.html#SetFunctionAttributes.
+ * See_Also: https://forum.dlang.org/post/hmucolyghbomttqpsili@forum.dlang.org
+ */
+auto assumePure(T)(T t) if (isFunctionPointer!T || isDelegate!T)
+{
+    enum attrs = functionAttributes!T | FunctionAttribute.pure_;
+    return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs)) t;
+}
+
+int f(int x)
+{
+    return x + 1;
+}
+
+void g() pure
+{
+    static assert(!__traits(compiles, { auto x = f(42); }));
+    auto pureF = assumePure(&f);
+    assert(pureF(42) == 43);
+}
+
+auto assumePureNogc(T)(T t) if (isFunctionPointer!T || isDelegate!T)
+{
+    enum attrs = functionAttributes!T | FunctionAttribute.pure_ | FunctionAttribute.nogc;
+    return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs)) t;
 }

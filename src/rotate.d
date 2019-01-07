1092af218d132cefaab9283efd2d979e6bbe41e1
nordlow <per.nordlow@gmail.com>
2019-01-07 15:46:40 +0100

    *** empty log message ***

--- src/rotate.d
+++ src/rotate.d
@@ -1,6 +1,6 @@
 module rotate;
 
-/** Rotate `x` by `n`.
+/** Rotate `x` left by `n` bits.
  *
  * Should compile to a single CPU instruction (ROL).
  */
@@ -8,4 +8,14 @@ ulong rotateLeft(ulong x, ubyte n) @safe pure nothrow @nogc
 {
     pragma(inline, true);
     return (x << n) | (x >> (64 - n));
+}
+
+/** Rotate `x` right by `n` bits.
+ *
+ * Should compile to a single CPU instruction (ROR).
+ */
+ulong rotateRight(ulong x, ubyte n) @safe pure nothrow @nogc
+{
+    pragma(inline, true);
+    return (x >> n) | (x << (64 - n));
 }

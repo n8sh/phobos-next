df01acecb8be3340ec0fae66a18873b856806c0b
nordlow <per.nordlow@gmail.com>
2019-01-07 14:44:48 +0100

    *** empty log message ***

--- src/conv_itos.d
+++ src/conv_itos.d
@@ -1,6 +1,6 @@
 module conv_itos;
 
-const(uint) fastLog10(const uint val) pure nothrow @nogc @safe
+const(uint) fastLog10(const uint val) @safe pure nothrow @nogc
 {
     return (val < 10) ? 0 : (val < 100) ? 1 : (val < 1000) ? 2 : (val < 10000) ? 3
     : (val < 100000) ? 4 : (val < 1000000) ? 5 : (val < 10000000) ? 6
@@ -12,7 +12,7 @@ private static immutable fastPow10tbl_32bit_unsigned = [
     1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
     ];
 
-string itos(const uint val) pure nothrow @trusted
+string itos(const uint val) @trusted pure nothrow
 {
     immutable length = fastLog10(val) + 1;
     char[] result;
@@ -31,5 +31,4 @@ static assert(mixin(uint.max.itos) == uint.max);
 
 @safe pure unittest
 {
-
 }

601d5dcf8a27e70a43f65ece892c725f5ea14381
nordlow <per.nordlow@gmail.com>
2019-01-11 14:42:30 +0100

    *** empty log message ***

--- src/open_hashmap_or_hashset.d
+++ src/open_hashmap_or_hashset.d
@@ -38,40 +38,6 @@ private template defaultKeyEqualPredOf(T)
     }
 }
 
-/** Returns `true` iff `lhs` and `rhs` are equal.
- *
- * Opposite to druntime version, implementation is parameterized on object type
- * `T` enabling correct propagation of function qualifiers of `lhs.opEquals(rhs)`.
- */
-bool opEqualsDerived(T)(const T lhs, const T rhs)
-if (is(T == class))
-{
-    // If aliased to the same object or both null => equal
-    if (lhs is rhs) return true;
-
-    // If either is null => non-equal
-    if (lhs is null || rhs is null) return false;
-
-    return lhs.opEquals(rhs);
-
-    version(none)
-    {
-        // If same exact type => one call to method opEquals
-        if (typeid(lhs) is typeid(rhs)//  ||
-            // TODO !__ctfe && typeid(lhs).opEquals(typeid(rhs))
-            )
-            /* CTFE doesn't like typeid much. 'is' works, but opEquals doesn't
-               (issue 7147). But CTFE also guarantees that equal TypeInfos are
-               always identical. So, no opEquals needed during CTFE. */
-        {
-            return lhs.opEquals(rhs);
-        }
-
-        // General case => symmetric calls to method opEquals
-        return lhs.opEquals(rhs) && rhs.opEquals(lhs);
-    }
-}
-
 ///
 @safe pure nothrow unittest
 {
@@ -94,8 +60,6 @@ if (is(T == class))
         int x;
     }
     static assert(defaultKeyEqualPredOf!(C) == "a is b");
-    assert( opEqualsDerived(new C(42), new C(42)));
-    assert(!opEqualsDerived(new C(42), new C(43)));
 }
 
 @safe:

fe16f9b530332499e6c6c24e6a04947be2fcfad4
nordlow <per.nordlow@gmail.com>
2019-01-31 13:19:45 +0100

    *** empty log message ***

--- src/inplace_substitution.d
+++ src/inplace_substitution.d
@@ -3,8 +3,7 @@ module inplace_substitution;
 import std.ascii : isASCII;
 
 /** Substitute `from` by `to` in-place in `source`. */
-void substituteInPlaceASCII(dchar from, dchar to)(scope char[] source)
-    @safe pure nothrow @nogc
+void substituteInPlaceASCII(dchar from, dchar to)(scope char[] source) @safe pure nothrow @nogc
 if (isASCII(from) &&
     isASCII(to))
 {
@@ -18,8 +17,9 @@ if (isASCII(from) &&
 }
 
 /** Returns: in-place substitution `from` by `to` in `source`. */
-char[] substitutedInPlaceASCII(dchar from, dchar to)(return scope char[] source)
-    @safe pure nothrow @nogc
+char[] substitutedInPlaceASCII(dchar from, dchar to)(return scope char[] source) @safe pure nothrow @nogc
+if (isASCII(from) &&
+    isASCII(to))
 {
     substituteInPlaceASCII!(from, to)(source);
     return source;

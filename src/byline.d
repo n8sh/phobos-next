2c12a0370e504235df210d0da224d5723f110c30
nordlow <per.nordlow@gmail.com>
2019-02-10 11:50:49 +0100

    *** empty log message ***

--- src/byline.d
+++ src/byline.d
@@ -15,16 +15,18 @@ import std.traits : isNarrowString;
 import std.range : hasSlicing, hasLength;
 
 /** Split Input by line.
-    See_Also: http://forum.dlang.org/thread/fjqpdfzmitcxxzpwlbgb@forum.dlang.org#post-rwxrytxqqurrazifugje:40forum.dlang.org
-    TODO Restrict using isSomeString!Range?
-    // TODO This should fail with better error message:
-    // assert(equal((cast(ubyte[])"a\nb").byLine!(Newline.any), ["a", "b"]));
-    */
-auto byLine(Newline nl = Newline.any,
-            Range)(Range input)
+ *
+ * See_Also: http://forum.dlang.org/thread/fjqpdfzmitcxxzpwlbgb@forum.dlang.org#post-rwxrytxqqurrazifugje:40forum.dlang.org
+ *
+ * TODO Restrict using isSomeString!Range?
+ *
+ * TODO This should fail with better error message:
+ * assert(equal((cast(ubyte[])"a\nb").byLine!(Newline.any), ["a", "b"]));
+ */
+auto byLine(Newline nl, Range)(Range input)
 if ((hasSlicing!Range &&
-         hasLength!Range) ||
-        isNarrowString!Range)
+     hasLength!Range) ||
+    isNarrowString!Range)
 {
     static if (nl == Newline.native)
     {
@@ -42,36 +44,40 @@ if ((hasSlicing!Range &&
     }
     else
     {
-        static if (nl == Newline.any)
+        static if (nl == Newline.unix)
+        {
+            import splitter_ex : splitterASCIIAmong;
+            return input.splitterASCIIAmong!('\n');
+        }
+        else static if (nl == Newline.win)
+        {
+            import std.algorithm: splitter;
+            return input.splitter("\r\n");
+        }
+        else static if (nl == Newline.mac)
+        {
+            import splitter_ex : splitterASCIIAmong;
+            return input.splitterASCIIAmong!('\r');
+        }
+        else static if (nl == Newline.any)
         {
             // TODO Use ctRegex instead?
             import std.regex: splitter, regex;
             return input.splitter(regex("\n|\r\n|\r"));
         }
-        else
-        {
-            import std.algorithm: splitter;
-            static if (nl == Newline.win)
-            {
-                return input.splitter("\r\n");
-            }
-            else static if (nl == Newline.mac)
-            {
-                return input.splitter('\r');
-            }
-            else static if (nl == Newline.unix)
-            {
-                return input.splitter('\n');
-            }
-        }
     }
 }
 
-@safe unittest
+///
+@safe pure nothrow unittest
 {
-    import std.algorithm: equal;
-    assert(equal("a\nb".byLine!(Newline.any), ["a", "b"]));
+    // assert(equal("a\nb".byLine!(Newline.any), ["a", "b"]));
     assert(equal("a\r\nb".byLine!(Newline.win), ["a", "b"]));
     assert(equal("a\rb".byLine!(Newline.mac), ["a", "b"]));
     assert(equal("a\nb".byLine!(Newline.unix), ["a", "b"]));
+}
+
+version(unittest)
+{
+    import std.algorithm: equal;
 }

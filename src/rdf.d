0d231831fe9a97668531a41bc467cbc5653b3314
nordlow <per.nordlow@gmail.com>
2019-02-10 12:03:35 +0100

    *** empty log message ***

--- src/rdf.d
+++ src/rdf.d
@@ -276,7 +276,7 @@ if ((hasSlicing!R && hasLength!R ||
     import std.string: indexOf;
     import splitter_ex : splitterASCIIAmong;
     return r.splitterASCIIAmong!('\n')               // TODO support multiple newlines
-            .filter!(line => line.indexOf('#') != 0) // skip comments
+            .filter!(line => line.indexOf('#') != 0) // skip comments. TODO non-decoding
             .map!(line => line.nTriple);
 }
 

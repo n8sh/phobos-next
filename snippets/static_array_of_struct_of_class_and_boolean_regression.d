ceda83580a9e16d2e47661a970d45ce556214f24
nordlow <per.nordlow@gmail.com>
2019-02-12 14:53:26 +0100

    *** empty log message ***

--- snippets/static_array_of_struct_of_class_and_boolean_regression.d
+++ snippets/static_array_of_struct_of_class_and_boolean_regression.d
@@ -20,6 +20,9 @@ unittest
     auto s1 = [S(c)].staticArray;
     S[1] s2 = [S(c)];
 
+    writeln(cast(void*)s1[0].c);
+    writeln(cast(void*)s2[0].c);
+
     assert(s1[0].c is
            s2[0].c);
 }

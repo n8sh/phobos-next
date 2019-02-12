fe8e769bd1a104de2bb0453aa9da5b3d4d1dd0c0
nordlow <per.nordlow@gmail.com>
2019-02-12 14:51:24 +0100

    *** empty log message ***

---
+++ snippets/static_array_of_struct_of_class_and_boolean_regression.d
@@ -0,0 +1,25 @@
+class C
+{
+    this(int i) { this.i = i; }
+    int i;
+}
+
+struct S
+{
+    C c;
+    bool b;                  // removing this prevents bug
+}
+
+unittest
+{
+    import std.stdio : writeln;
+    import std.array : staticArray;
+
+    auto c = new C(42);
+
+    auto s1 = [S(c)].staticArray;
+    S[1] s2 = [S(c)];
+
+    assert(s1[0].c is
+           s2[0].c);
+}

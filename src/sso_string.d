8f1b8e5038e00e351aa320ac82d463b3ed29e8d3
nordlow <per.nordlow@gmail.com>
2019-02-20 23:16:29 +0100

    *** empty log message ***

--- src/sso_string.d
+++ src/sso_string.d
@@ -14,7 +14,7 @@ module sso_string;
  * See_Also: https://forum.dlang.org/post/pb87rn$2icb$1@digitalmars.com
  *
  * TODO Use extra bits in Short.length for
- * - lowercase English into 120/5 = 25 chars
+ * - lowercase English into 128/5 = 25 chars
  * - uppercase English into 120/5 = 25 chars
  * - mixedcase English into 120/6 = 20 chars
  */

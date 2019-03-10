7708776879cee58f2bde6b4581ab3ec1a8e7dcea
nordlow <per.nordlow@gmail.com>
2019-03-10 12:41:34 +0100

    *** empty log message ***

--- src/basic_array.d
+++ src/basic_array.d
@@ -34,9 +34,7 @@ if (!is(Unqual!T == bool) &&             // use `BitArray` instead
     import std.range : isInputRange, ElementType, isInfinite;
     import std.traits : hasElaborateDestructor, hasIndirections, hasAliasing,
         isMutable, TemplateOf, isArray, isAssignable, isCopyable, isType, hasFunctionAttributes, isIterable;
-    version(LDC) { import std.algorithm.mutation : move, moveEmplace;
-        static if (__VERSION__ >= 2085) { static assert(0, "Use core.lifetime instead"); }
-    } else import core.lifetime : move, moveEmplace;
+    import core.lifetime : move, moveEmplace;
     import std.algorithm : moveEmplaceAll;
     import std.conv : emplace;
 

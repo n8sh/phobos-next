f3a13cff247358b642c46474c64a7085e657eab4
nordlow <per.nordlow@gmail.com>
2019-03-10 12:49:58 +0100

    *** empty log message ***

--- src/pure_gc_allocator.d
+++ src/pure_gc_allocator.d
@@ -1,6 +1,6 @@
 module pure_gc_allocator;
 
-static if (__VERSION__ >= 2085)
+static if (__VERSION__ >= 2086)
 {
     version(LDC) static assert(0, "TODO Use std.experimental.allocator.building_blocks.gc_allocator.d instead of this module");
 }

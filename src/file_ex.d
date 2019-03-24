1a27f2f608edebd9474736d5f69fb7cab145529b
nordlow <per.nordlow@gmail.com>
2019-03-24 11:47:17 +0100

    *** empty log message ***

--- src/file_ex.d
+++ src/file_ex.d
@@ -1,15 +1,15 @@
 module file_ex;
 
 /** Read file $(D path) into raw array with one extra terminating zero byte.
-
-    This extra terminating zero (null) byte at the end is typically used as a
-    sentinel value to speed up textual parsers.
-
-    TODO add or merge to Phobos?
-
-    See_Also: https://en.wikipedia.org/wiki/Sentinel_value
-    See_Also: http://forum.dlang.org/post/pdzxpkusvifelumkrtdb@forum.dlang.org
-*/
+ *
+ * This extra terminating zero (null) byte at the end is typically used as a
+ * sentinel value to speed up textual parsers.
+ *
+ * TODO add or merge to Phobos?
+ *
+ * See_Also: https://en.wikipedia.org/wiki/Sentinel_value
+ * See_Also: http://forum.dlang.org/post/pdzxpkusvifelumkrtdb@forum.dlang.org
+ */
 immutable(void)[] rawReadNullTerminated(string path) @trusted
 {
     import std.stdio : File;

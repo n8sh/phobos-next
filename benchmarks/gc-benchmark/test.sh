21989c7dc5f9cb99398955ffdb7ebd421f61ebdc
Per Nordlöw <per.nordlow@gmail.com>
2019-06-12 14:56:18 +0200

    *** empty log message ***

--- benchmarks/gc-benchmark/test.sh
+++ benchmarks/gc-benchmark/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Segregated GC
+dub run --build=release-nobounds
+echo
+
 # Conservative GC
 dub run --build=release-nobounds -- --DRT-gcopt=gc:conservative
+echo
 
 # Precise GC with four-way-parallel marking
-dub run --build=release-nobounds -- --DRT-gcopt=gc:precise --DRT-gcopt=parallel:4
-
-# Segregated GC
-dub run --build=release-nobounds -- --DRT-gcopt=gc:segregated
+dub run --build=release-nobounds -- --DRT-gcopt=gc:precise
+echo

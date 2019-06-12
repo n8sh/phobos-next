95d66db9a0b80a9a605591a8be3b324fa7db693d
Per Nordlöw <per.nordlow@gmail.com>
2019-06-12 14:56:20 +0200

    *** empty log message ***

--- benchmarks/gc-benchmark/source/register_segregated_gc.d
+++ benchmarks/gc-benchmark/source/register_segregated_gc.d
@@ -3,20 +3,23 @@
 import core.gc.gcinterface, core.gc.registry;
 import segregated_gc;
 
-extern (C) pragma(crt_constructor) void registerSegregatedGC()
+version(segregated_gc)
 {
-    registerGCFactory("segregated", &createSegregatedGC);
-}
+    extern (C) pragma(crt_constructor) void registerSegregatedGC()
+    {
+        registerGCFactory("segregated", &createSegregatedGC);
+    }
 
-GC createSegregatedGC()
-{
-    __gshared instance = new SegregatedGC;
-    instance.initialize();
-    return instance;
-}
+    GC createSegregatedGC()
+    {
+        __gshared instance = new SegregatedGC;
+        instance.initialize();
+        return instance;
+    }
 
 /* The new GC is added to the list of available garbage collectors that can be
  * selected via the usual configuration options, e.g. by embedding rt_options
  * into the binary:
  */
-extern (C) __gshared string[] rt_options = ["gcopt=gc:segregated"];
+    extern (C) __gshared string[] rt_options = ["gcopt=gc:segregated"];
+}

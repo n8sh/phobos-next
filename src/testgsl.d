cdf9226c5db6cc83351266c4cd1e6ecc46696860
Per Nordlöw <per.nordlow@gmail.com>
2019-07-06 23:02:43 +0200

    *** empty log message ***

---
+++ src/testgsl.d
@@ -0,0 +1,50 @@
+#!/usr/bin/env dub
+/+ dub.json:
+ {
+ "name": "testgsl",
+ "dependencies": {"gsl": "~>0.1.8"}
+ }
+ +/
+
+/*
+ *  Compile example:
+ *  dub run --single testgsl.d
+ */
+
+import std.range : iota, array;
+import std.stdio : writeln;
+import gsl.monte;
+
+struct my_f_params { double a; double b; double c; }
+
+extern(C) double my_f(double* x, size_t dim, void* params)
+{
+    auto fp = cast(my_f_params*)params;
+    assert(dim == 2);
+    return (fp.a * x[0] * x[0] +
+            fp.b * x[0] * x[1] +
+            fp.c * x[1] * x[1]);
+}
+
+double gsl_monte_fn_eval(gsl_monte_function* F, double* x)
+{
+    return (*(F.f))(x, F.dim, F.params);
+}
+
+void test_gsl_integration()
+{
+    gsl_monte_function F;
+    my_f_params params = { 3.0, 2.0, 1.0 };
+
+    F.f = &my_f;
+    F.dim = 2;
+    F.params = &params;
+
+    double[2] x = [2, 2];
+    assert(gsl_monte_fn_eval(&F, x.ptr) == 24);
+}
+
+void main()
+{
+    test_gsl_integration();
+}

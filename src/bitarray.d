380ca532bd9fff6836f205153c5d09ec4d32afc7
Per Nordlöw <per.nordlow@gmail.com>
2019-07-13 16:44:39 +0200

    *** empty log message ***

--- src/bitarray.d
+++ src/bitarray.d
@@ -38,7 +38,7 @@ struct BitArray(bool wordAlignedLength = false,
 
     /** Construct with `length` number of zero bits stored in `blocks`. */
     private static typeof(this) withLengthAndBlocks(size_t length,
-                                                    Block[] blocks) @trusted
+                                                    const scope Block[] blocks) @trusted
     {
         typeof(return) that;
         that._blockCount = blocks.length;

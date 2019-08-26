fbb84c8f98cfdbbd5d678aa08d83f644b19f3fa2
Per Nordlöw <per.nordlow@gmail.com>
2019-08-26 11:19:02 +0200

    *** empty log message ***

--- src/soa.d
+++ src/soa.d
@@ -130,8 +130,8 @@ private:
     void grow() @trusted
     {
         import std.algorithm.comparison : max;
-        size_t newCapacity = max(1, _capacity * _growthFactor);
-        size_t expandSize = newCapacity - _capacity;
+        const newCapacity = max(1, _capacity * _growthFactor);
+        const expandSize = newCapacity - _capacity;
 
         if (_capacity is 0)
         {

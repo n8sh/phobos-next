c945c76433e7364f8e55cb81dbef8bbe8f42c709
Per Nordlöw <per.nordlow@gmail.com>
2019-10-11 11:44:02 +0200

    *** empty log message ***

--- snippets/copy_ctor.d
+++ snippets/copy_ctor.d
@@ -2,6 +2,26 @@
 
 import std.traits, std.meta, std.range, std.algorithm, std.stdio;
 
-void main(string[] args)
+// https://github.com/dlang/DIPs/blob/master/DIPs/accepted/DIP1018.md
+
+struct A
 {
+    this(int x)
+    {
+        this.x = x;
+    }
+    this(ref return scope A rhs) // copy ctor
+    {
+        writeln("copying ", rhs.x);
+        this.x = rhs.x;
+    }
+    int x;
+}
+
+void main()
+{
+    auto a = A(42);
+    A b = a;            // calls copy constructor implicitly - prints "x"
+    A c = A(b);         // calls constructor explicitly
+    // immutable A d = a;  // calls copy constructor implicittly - prints 7
 }

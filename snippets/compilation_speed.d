77d76fad0bb554a2b6729118d95414c694cc7773
Per Nordlöw <per.nordlow@gmail.com>
2019-10-24 23:54:31 +0200

    *** empty log message ***

---
+++ snippets/compilation_speed.d
@@ -0,0 +1,25 @@
+bool startsWith(T)(scope const(T)[] haystack,
+                   scope const(T)[] needle)
+{
+    if (haystack.length >= needle.length)
+    {
+        return haystack[0 .. needle.length] == needle; // range check is elided by LDC in release builds
+    }
+    return false;
+}
+
+///
+@safe pure nothrow @nogc unittest
+{
+    import std.meta : AliasSeq;
+    foreach (T; AliasSeq!(char, wchar, dchar,
+                          short, ushort,
+                          int, uint,
+                          long, ulong,
+                          float, double, real))
+    {
+        T[] x;
+        T[] y;
+        assert(x.startsWith(y));
+    }
+}

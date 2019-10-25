8f972a2a0dc88511716440217e6ddad182b8440b
Per Nordlöw <per.nordlow@gmail.com>
2019-10-25 08:46:23 +0200

    *** empty log message ***

--- src/array_algorithm.d
+++ src/array_algorithm.d
@@ -2,9 +2,48 @@ module array_algorithm;
 
 import std.range.primitives : ElementType;
 
+/** Array-overload for `startsWith` with no explicit predicate predicate. */
+bool startsWith(T)(scope const(T)[] haystack,
+                   scope const(T)[] needle)
+{
+    if (haystack.length >= needle.length)
+    {
+        return haystack[0 .. needle.length] == needle; // range check is elided by LDC in release builds
+    }
+    return false;
+}
+
+///
+@safe pure nothrow @nogc unittest
+{
+    auto x = "beta version";
+    assert(x.startsWith("beta"));
+    assert(!x.startsWith("_"));
+}
+
+/** Array-overload for `endsWith` with no explicit predicate predicate. */
+bool endsWith(T)(scope const(T)[] haystack,
+                 scope const(T)[] needle)
+{
+    if (haystack.length >= needle.length)
+    {
+        return haystack[$ - needle.length .. $] == needle; // range check is elided by LDC in release builds
+    }
+    return false;
+}
+
+///
+@safe pure nothrow @nogc unittest
+{
+    auto x = "beta version";
+    assert(x.endsWith("version"));
+    assert(!x.startsWith("_"));
+}
+
 /** Overload of `std.array.array` that creates a static array of length `n`.
-    TODO Better name: {make,array}{N,Exactly}
-    TODO could we find a way to propagate length at compile-time?
+ *
+ * TODO Better name: {make,array}{N,Exactly}
+ * TODO could we find a way to propagate length at compile-time?
  */
 ElementType!R[n] toStaticArray(size_t n, R)(R r)
 {

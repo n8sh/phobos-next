86d5d16cf4973a4506ec89335e3f6e9f35e9762a
Per Nordlöw <per.nordlow@gmail.com>
2019-10-25 12:06:36 +0200

    *** empty log message ***

--- src/magics.d
+++ src/magics.d
@@ -20,8 +20,8 @@ void scanMagicFiles(string dir)
     import std.stdio: write, writeln, File;
     import std.array: array;
     import std.range: front, empty;
-    import array_algorithm : startsWith;
-    import std.algorithm : find, strip;
+    import array_algorithm : startsWith, strip;
+    import std.algorithm : find;
     import std.ascii: isDigit;
     import std.uni: isAlpha, isWhite;
     import std.algorithm.iteration : splitter;

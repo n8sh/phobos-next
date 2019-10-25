ef73a210af35065dd2bb57d6018edcc0768968fb
Per Nordlöw <per.nordlow@gmail.com>
2019-10-25 11:46:53 +0200

    *** empty log message ***

--- src/array_algorithm.d
+++ src/array_algorithm.d
@@ -20,7 +20,7 @@ bool startsWith(T)(scope const(T)[] haystack,
 bool startsWith(T)(scope const(T)[] haystack,
                    scope const T needle)
 {
-    static if (is(T : char))  { assert(needle < 128); } // TODO convert needle to string and call itself
+    static if (is(T : char)) { assert(needle < 128); } // TODO convert needle to string and call itself
     if (haystack.length >= 1)
     {
         return haystack[0] == needle;
@@ -51,7 +51,7 @@ bool endsWith(T)(scope const(T)[] haystack,
 bool endsWith(T)(scope const(T)[] haystack,
                  scope const T needle)
 {
-    static if (is(T : char))  { assert(needle < 128); } // TODO convert needle to string and call itself
+    static if (is(T : char)) { assert(needle < 128); } // TODO convert needle to string and call itself
     if (haystack.length >= 1)
     {
         return haystack[$ - 1] == needle;
@@ -86,7 +86,7 @@ bool skipOver(T)(scope ref inout(T)[] haystack,
 bool skipOver(T)(scope ref inout(T)[] haystack,
                  scope const T needle)
 {
-    static if (is(T : char))  { assert(needle < 128); } // TODO convert needle to string and call itself
+    static if (is(T : char)) { assert(needle < 128); } // TODO convert needle to string and call itself
     if (startsWith(haystack, needle))
     {
         haystack = haystack[1 .. $];
@@ -123,7 +123,7 @@ bool skipOverBack(T)(scope ref inout(T)[] haystack,
 bool skipOverBack(T)(scope ref inout(T)[] haystack,
                      scope const T needle)
 {
-    static if (is(T : char))  { assert(needle < 128); } // TODO convert needle to string and call itself
+    static if (is(T : char)) { assert(needle < 128); } // TODO convert needle to string and call itself
     if (endsWith(haystack, needle))
     {
         haystack = haystack[0 .. $ - 1];
@@ -149,7 +149,7 @@ bool skipOverBack(T)(scope ref inout(T)[] haystack,
 inout(T)[] stripLeft(T)(scope return inout(T)[] haystack,
                         scope const T needle)
 {
-    static if (is(T : char))  { assert(needle < 128); } // TODO convert needle to string and call itself
+    static if (is(T : char)) { assert(needle < 128); } // TODO convert needle to string and call itself
     size_t offset = 0;
     while (offset != haystack.length &&
            haystack[offset] == needle)
@@ -180,7 +180,7 @@ inout(char)[] stripLeft()(scope return inout(char)[] haystack) @safe pure nothro
 inout(T)[] stripRight(T)(scope return inout(T)[] haystack,
                          scope const T needle)
 {
-    static if (is(T : char))  { assert(needle < 128); } // TODO convert needle to string and call itself
+    static if (is(T : char)) { assert(needle < 128); } // TODO convert needle to string and call itself
     size_t offset = haystack.length;
     while (offset != 0 &&
            haystack[offset - 1] == needle)
@@ -202,6 +202,37 @@ inout(char)[] stripRight()(scope return inout(char)[] haystack) @safe pure nothr
     assert("beta  ".stripRight(' ') == "beta");
     assert("beta    ".stripRight(' ') == "beta");
     assert("beta    ".stripRight() == "beta");
+}
+
+/** Array-overload for `strip` with ASCII-char needle and default predicate.
+ *
+ * See_Also: https://forum.dlang.org/post/dhxwgtaubzbmjaqjmnmq@forum.dlang.org
+ */
+inout(T)[] strip(T)(scope return inout(T)[] haystack,
+                    scope const T needle)
+{
+    static if (is(T : char)) { assert(needle < 128); } // TODO convert needle to string and call itself
+    size_t offset = 0;
+    while (offset != haystack.length &&
+           haystack[offset] == needle)
+    {
+        offset += 1;
+    }
+    return haystack[offset .. $];
+}
+inout(char)[] strip()(scope return inout(char)[] haystack) @safe pure nothrow @nogc // template-lazy
+{
+    return haystack.strip(' ');
+}
+
+///
+@safe pure nothrow @nogc unittest
+{
+    assert("beta".strip(' ') == "beta");
+    assert(" beta".strip(' ') == "beta");
+    assert("  beta".strip(' ') == "beta");
+    assert("   beta".strip(' ') == "beta");
+    assert("   beta".strip() == "beta");
 }
 
 /** Overload of `std.array.array` that creates a static array of length `n`.

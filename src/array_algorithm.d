0bf366867a2160f57e37f3024ec799682eba8015
Per Nordlöw <per.nordlow@gmail.com>
2019-10-25 12:01:54 +0200

    *** empty log message ***

--- src/array_algorithm.d
+++ src/array_algorithm.d
@@ -4,8 +4,6 @@
  */
 module array_algorithm;
 
-import std.range.primitives : ElementType;
-
 /** Array-overload for `startsWith` with default predicate. */
 bool startsWith(T)(scope const(T)[] haystack,
                    scope const(T)[] needle)
@@ -247,43 +245,4 @@ inout(char)[] strip()(scope return inout(char)[] haystack) @safe pure nothrow @n
     assert("   beta   ".strip(' ') == "beta");
     assert(" _ beta _ ".strip(' ') == "_ beta _");
     assert(" _  beta _ ".strip(' ') == "_  beta _");
-}
-
-/** Overload of `std.array.array` that creates a static array of length `n`.
- *
- * TODO Better name: {make,array}{N,Exactly}
- * TODO could we find a way to propagate length at compile-time?
- */
-ElementType!R[n] toStaticArray(size_t n, R)(R r)
-{
-    assert(r.length == n);
-    typeof(return) dst;
-    import std.algorithm.mutation : copy;
-    r.copy(dst[]);
-    return dst;
-}
-
-/** Static array overload for `std.algorithm.iteration.map`.
- *
- * See_Also: http://forum.dlang.org/thread/rqlittlysttwxwphlnmh@forum.dlang.org
- * TODO Add to Phobos
- */
-typeof(fun(E.init))[n] map(alias fun, E, size_t n)(const E[n] src)
-{
-    import std.algorithm.iteration : map;
-    return src[].map!fun.toStaticArray!n;
-}
-
-///
-@safe pure nothrow unittest
-{
-    import std.meta : AliasSeq;
-    foreach (E; AliasSeq!(int, double))
-    {
-        enum n = 42;
-        E[n] c;
-        const result = c.map!(_ => _^^2);
-        static assert(c.length == result.length);
-        static assert(is(typeof(result) == const(E)[n]));
-    }
 }

7a9872da05259ac7f041f822e366a039ab24af3c
Per Nordlöw <per.nordlow@gmail.com>
2019-10-28 15:37:09 +0100

    *** empty log message ***

--- src/rdf.d
+++ src/rdf.d
@@ -1,6 +1,7 @@
-/** Parsing of RDF n-triples.
+/** Parsing of RDF N-Triples.
  *
  * TODO parse N-Quads
+ * TODO pasre RDF/XML
  *
  * See_Also: https://en.wikipedia.org/wiki/N-Triples#N-Quads
  * See_Also: https://www.ida.liu.se/~robke04/include/publications.shtml

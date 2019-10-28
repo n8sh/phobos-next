9009c3fc0c18ccdc7409c0438b25c026b12ce02d
Per Nordlöw <per.nordlow@gmail.com>
2019-10-28 13:40:42 +0100

    *** empty log message ***

--- src/array_algorithm.d
+++ src/array_algorithm.d
@@ -9,7 +9,7 @@
  */
 module array_algorithm;
 
-version = runAsBetterC; // Run_As: /usr/bin/dmd -betterC -unittest -run array_algorithm.d
+version = runAsBetterC; // Run_As: dmd -betterC -unittest -run array_algorithm.d
 
 /** Array-overload for `startsWith` with default predicate.
  *

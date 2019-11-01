d589abc6d2af4f18b3cc8f63150e58384e14619a
Per Nordlöw <per.nordlow@gmail.com>
2019-11-01 10:09:43 +0100

    *** empty log message ***

--- src/rdf.d
+++ src/rdf.d
@@ -13,6 +13,8 @@
  * See_Also: https://en.wikipedia.org/wiki/N-Triples#N-Quads
  *
  * See_Also: https://www.ida.liu.se/~robke04/include/publications.shtml
+ *
+ * TODO decode `subject` and `object` only when their types are IRIs.
  */
 module rdf;
 

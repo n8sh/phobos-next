2f8270eb79ad1d2e5d265693c8ee3c1faef541f7
Per Nordlöw <per.nordlow@gmail.com>
2019-11-09 16:01:10 +0100

    *** empty log message ***

--- src/rdf.d
+++ src/rdf.d
@@ -29,6 +29,134 @@ enum ObjectFormat { IRI, blankNode, literal }
  */
 auto parseNTriple(scope return inout(char)[] line) @safe pure
 {
+    import array_algorithm : skipOverBack;
+
+    assert(line.length >= 4);
+
+    // strip suffix
+    line.skipOverBack('.');
+    line.skipOverBack(' ');
+
+    import array_algorithm : indexOf;
+
+    // subject IRI
+    const ix0 = line.indexOf(' '); // TODO use array_algorithm.findSplit(' ')
+    assert(ix0 != -1);
+    const subject = line[0 .. ix0];
+    line = line[ix0 + 1 .. $];
+
+    // predicate IRI
+    const ix1 = line.indexOf(' '); // TODO use array_algorithm.findSplit(' ')
+    assert(ix1 != -1);
+    const predicate = line[0 .. ix1];
+    line = line[ix1 + 1 .. $];
+
+    auto nt = inout(NTriple)(subject, predicate, line);
+    (cast(NTriple)nt).parse();  // hack to make `inout` work
+    return nt;
+}
+
+/** RDF N-Triple.
+ *
+ * Parameterized on element type $(D Chars). Use NTriple!(char[]) to avoid
+ * GC-allocations when parsing files using File.byLine which returns a volatile
+ * reference to a temporary char[] buffer. If The NTriples are to be stored
+ * permanently in memory use NTriple!string.
+ *
+ * See_Also: https://en.wikipedia.org/wiki/N-Triples
+ */
+struct NTriple
+{
+    import array_algorithm : skipOver, skipOverBack, startsWith, endsWith;
+
+    alias Chars = const(char)[];
+
+    /** Parse `subject`, `predicate` and `object`.
+     *
+     * Fails for:
+     * - subject: <http://dbpedia.org/resource/CT_Rei_Pel%C3%A9>
+     * - predicate: <http://xmlns.com/foaf/0.1/homepage>
+     * - object: <http://www.santosfc.com.br/clube/default.asp?c=Sedes&st=CT%20Rei%20Pel%E9>
+     */
+    void parse() @safe pure scope nothrow @nogc
+    {
+        // subject: Ref: https://www.w3.org/TR/n-triples/#grammar-production-subject
+        if (subject.skipOver('<')) // IRIREF (https://www.w3.org/TR/n-triples/#grammar-production-IRIREF)
+        {
+            const ok = subject.skipOverBack('>');
+            assert(ok);
+            subjectType = SubjectFormat.IRI;
+        }
+        else                // BLANK_NODE_LABEL
+        {
+            subjectType = SubjectFormat.blankNode;
+        }
+
+        // predicate: Ref: https://www.w3.org/TR/n-triples/#grammar-production-predicate
+        assert(predicate.startsWith('<'));
+        assert(predicate.endsWith('>'));
+        predicate = predicate[1 .. $ - 1]; // IRIREF (https://www.w3.org/TR/n-triples/#grammar-production-IRIREF)
+
+        // object: Ref: https://www.w3.org/TR/n-triples/#grammar-production-object
+        if (object.skipOver('<')) // IRIREF (https://www.w3.org/TR/n-triples/#grammar-production-IRIREF)
+        {
+            const ok = object.skipOverBack('>');
+            assert(ok);
+            objectType = ObjectFormat.IRI;
+        }
+        else if (object.skipOver('"')) // literal (https://www.w3.org/TR/n-triples/#grammar-production-literal)
+        {
+            import array_algorithm : findSplit;
+            // import std.ascii : isLower;
+            if (const split = object.findSplit(`"@`))
+            {
+                objectLanguageCode = split.post;
+                object = split.pre;
+            }
+            else if (auto hit = object.findSplit(`"^^`))
+            {
+                const objectdataType = hit.post;
+                assert(objectdataType.startsWith('<'));
+                assert(objectdataType.endsWith('>'));
+                objectDataTypeIRI = objectdataType[1 .. $ - 1];
+                object = hit.pre;
+            }
+            else
+            {
+                const ok = object.skipOverBack('"');
+                if (!ok)
+                {
+                    assert("No matching double-quote in object ");
+                }
+                assert(ok);
+            }
+
+            // dbg(`object:"`, object, `" lang:"`, objectLanguageCode, `" typeIRI:"`, objectDataTypeIRI, `"`);
+            objectType = ObjectFormat.literal;
+        }
+        else                // BLANK_NODE_LABEL (https://www.w3.org/TR/n-triples/#grammar-production-BLANK_NODE_LABEL)
+        {
+            objectType = ObjectFormat.blankNode;
+        }
+    }
+
+    Chars subject;
+    Chars predicate;
+
+    Chars object;
+    Chars objectLanguageCode;
+    Chars objectDataTypeIRI;
+
+    SubjectFormat subjectType;
+    ObjectFormat objectType;
+}
+
+/** Decode $(D S) into an RDF N-Triple.
+ *
+ * See_Also: https://www.w3.org/TR/n-triples/
+ */
+auto parseNTriple_inout(scope return inout(char)[] line) @safe pure
+{
     /** RDF N-Triple.
      *
      * Parameterized on element type $(D Chars). Use NTriple!(char[]) to avoid
@@ -155,7 +283,7 @@ auto parseNTriple(scope return inout(char)[] line) @safe pure
 @safe pure unittest
 {
     const x = `<http://dbpedia.org/resource/180%C2%B0_(Gerardo_album)> <http://dbpedia.org/ontology/artist> <http://dbpedia.org/resource/Gerardo_Mej%C3%ADa> .`;
-    auto nt = x.parseNTriple;
+    auto nt = x.parseNTriple_inout;
     static assert(is(typeof(nt.subject) == immutable(string))); // TODO should be `string` or `const(char)[]`
     static assert(is(typeof(nt.predicate) == immutable(string))); // TODO should be `string` or `const(char)[]`
     static assert(is(typeof(nt.object) == immutable(string))); // TODO should be `string` or `const(char)[]`
@@ -172,7 +300,7 @@ auto parseNTriple(scope return inout(char)[] line) @safe pure
 @safe pure unittest
 {
     const x = `<http://dbpedia.org/resource/1950_Chatham_Cup> <http://xmlns.com/foaf/0.1/name> "Chatham Cup"@en .`;
-    const nt = x.parseNTriple;
+    const nt = x.parseNTriple_inout;
     assert(nt.subject == `http://dbpedia.org/resource/1950_Chatham_Cup`);
     assert(nt.subjectType == SubjectFormat.IRI);
     assert(nt.predicate == `http://xmlns.com/foaf/0.1/name`);
@@ -186,7 +314,7 @@ auto parseNTriple(scope return inout(char)[] line) @safe pure
 @safe pure unittest
 {
     const x = `<http://dbpedia.org/resource/1950_Chatham_Cup> <http://xmlns.com/foaf/0.1/name> "Chatham Cup" .`;
-    const nt = x.parseNTriple;
+    const nt = x.parseNTriple_inout;
     assert(nt.subject == `http://dbpedia.org/resource/1950_Chatham_Cup`);
     assert(nt.subjectType == SubjectFormat.IRI);
     assert(nt.predicate == `http://xmlns.com/foaf/0.1/name`);
@@ -200,7 +328,7 @@ auto parseNTriple(scope return inout(char)[] line) @safe pure
 @safe pure unittest
 {
     const x = `<http://dbpedia.org/resource/007:_Quantum_of_Solace> <http://dbpedia.org/ontology/releaseDate> "2008-10-31"^^<http://www.w3.org/2001/XMLSchema#date> .`;
-    const nt = x.parseNTriple;
+    const nt = x.parseNTriple_inout;
     assert(nt.subject == `http://dbpedia.org/resource/007:_Quantum_of_Solace`);
     assert(nt.subjectType == SubjectFormat.IRI);
     assert(nt.predicate == `http://dbpedia.org/ontology/releaseDate`);
@@ -214,7 +342,7 @@ auto parseNTriple(scope return inout(char)[] line) @safe pure
 @safe pure unittest
 {
     const x = `<http://dbpedia.org/resource/Ceremony_(song)> <http://dbpedia.org/ontology/bSide> "\"In a Lonely Place\"".`;
-    const nt = x.parseNTriple;
+    const nt = x.parseNTriple_inout;
     assert(nt.subject == `http://dbpedia.org/resource/Ceremony_(song)`);
     assert(nt.subjectType == SubjectFormat.IRI);
     assert(nt.predicate == `http://dbpedia.org/ontology/bSide`);
@@ -228,7 +356,7 @@ auto parseNTriple(scope return inout(char)[] line) @safe pure
 @safe pure unittest
 {
     const x = `<http://dbpedia.org/resource/16_@_War> <http://xmlns.com/foaf/0.1/name> "16 @ War"@en .`;
-    auto nt = x.parseNTriple;
+    auto nt = x.parseNTriple_inout;
     assert(nt.subject == `http://dbpedia.org/resource/16_@_War`);
     assert(nt.subjectType == SubjectFormat.IRI);
     assert(nt.predicate == `http://xmlns.com/foaf/0.1/name`);
@@ -242,7 +370,7 @@ auto parseNTriple(scope return inout(char)[] line) @safe pure
 @safe pure unittest
 {
     const x = `<http://dbpedia.org/resource/CT_Rei_Pel%C3%A9> <http://xmlns.com/foaf/0.1/homepage> <http://www.santosfc.com.br/clube/default.asp?c=Sedes&st=CT%20Rei%20Pel%E9> .`;
-    auto nt = x.parseNTriple;
+    auto nt = x.parseNTriple_inout;
     assert(nt.subjectType == SubjectFormat.IRI);
     assert(nt.subject == `http://dbpedia.org/resource/CT_Rei_Pel%C3%A9`);
     assert(nt.predicate == `http://xmlns.com/foaf/0.1/homepage`);

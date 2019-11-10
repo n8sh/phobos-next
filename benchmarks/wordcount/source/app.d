551afd13f1697d1d4328a9ede14252589db6133f
Per Nordlöw <per.nordlow@gmail.com>
2019-11-10 23:10:09 +0100

    *** empty log message ***

--- benchmarks/wordcount/source/app.d
+++ benchmarks/wordcount/source/app.d
@@ -2,37 +2,21 @@ void main()
 {
     import std.stdio;
     import std.datetime : MonoTime;
-    import std.algorithm : max;
-    import digestx.fnv : FNV;
 
-    import open_hashmap_or_hashset : HashSet = OpenHashSet;
-    import open_hashmap_or_hashset : HashMap = OpenHashMap;
-    import basic_array : Array = BasicArray;
-
-    auto strs = Strs.withCapacity(72800);
-
-    size_t maxLength = 0;
+    size_t count = 0;
     immutable before = MonoTime.currTime();
     foreach (line; File("/usr/share/dict/words").byLine) // TODO make const and fix HashSet.insert
     {
-        import array_algorithm : endsWith;
-        if (!line.endsWith(`'s`))
-        {
-            strs.insert(String(line));
-            maxLength = max(maxLength, line.length);
-        }
+        count += 1;
     }
     immutable after = MonoTime.currTime();
 
     immutable secs = (after - before).total!"msecs";
     immutable nsecs = (after - before).total!"nsecs";
 
-    immutable n = strs.length;
-
-    writef("Insertion: n:%s maxLength:%s %1.2smsecs, %3.1fnsecs/op\n",
-           n,
-           maxLength,
+    writef("Insertion: count:%s %1.2smsecs, %3.1fnsecs/op\n",
+           count,
            secs,
-           cast(double)nsecs / n);
+           cast(double)nsecs / count);
 
 }

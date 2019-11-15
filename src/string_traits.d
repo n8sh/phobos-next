0e08d480665854e991352034fc0de3782c01fb80
Per Nordlöw <per.nordlow@gmail.com>
2019-11-15 11:59:47 +0100

    *** empty log message ***

--- src/string_traits.d
+++ src/string_traits.d
@@ -5,6 +5,7 @@ module string_traits;
 /** Returns: `true` iff `x` is an ASCII (7-bit clean) set of `char`s.
  *
  * See_Also: `std.ascii.isASCII`.
+ * See_Also: https://forum.dlang.org/post/syaqzkpybhvdehbhffjn@forum.dlang.org
  */
 bool isASCII(scope const(char)[] input)
 {

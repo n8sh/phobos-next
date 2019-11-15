f08c5f08fe2d407717ba086fa6f151182ad789cf
Per Nordlöw <per.nordlow@gmail.com>
2019-11-15 11:56:54 +0100

    *** empty log message ***

---
+++ src/string_traits.d
@@ -0,0 +1,16 @@
+module string_traits;
+
+@safe pure nothrow @nogc:
+
+/** Returns: `true` iff `x` is an ASCII (7-bit clean) set of `char`s.
+ *
+ * See_Also: `std.ascii.isASCII`.
+ */
+bool isASCII(scope const(char)[] input)
+{
+    foreach (e; cast(const(ubyte)[])input)
+    {
+        if (e >= 0x7F) { return false; }
+    }
+    return true;
+}

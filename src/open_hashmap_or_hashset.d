803d94005d375c8c91dc8ae13d4bc7e4a34c2e53
Per Nordlöw <per.nordlow@gmail.com>
2019-11-22 14:29:58 +0100

    *** empty log message ***

--- src/open_hashmap_or_hashset.d
+++ src/open_hashmap_or_hashset.d
@@ -139,6 +139,7 @@ struct OpenHashMapOrSet(K, V = void,
     enum hasAddressLikeKey = (isAddress!K ||
                               isSlice!K);
 
+    /** Use linear search instead probing when store is small than `_linearSearchMaxSize`. */
     private enum _doSmallLinearSearch = false;
 
     /** Stores less than or equal to this size will be searched using linear

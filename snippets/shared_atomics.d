610a05e1b79fb2117f1cf4559a4123f29e629166
nordlow <per.nordlow@gmail.com>
2019-12-11 12:37:28 +0100

    *** empty log message ***

---
+++ snippets/shared_atomics.d
@@ -0,0 +1,35 @@
+shared int x;
+shared int y;
+shared int* ptr;
+shared static this() { ptr = new int; } // silence null-dereference errors
+class NS { shared int x; }
+shared class S { int sx; }
+
+void main()
+{
+    ++x;
+    x++;
+    --x;
+    x--;
+    x += 1;
+    x += 2;
+    x -= 3;
+    x |= y;
+    x *= y;
+    x /= y;
+    x %= y;
+    x &= y;
+    x ^= y;
+    x <<= y;
+    x >>= y;
+    x >>>= y;
+    x ^^= y;
+    ++ptr;
+    ptr++;
+    --ptr;
+    ptr--;
+    NS ns = new NS;
+    ns.x++;
+    S s = new S;
+    s.sx++;
+}

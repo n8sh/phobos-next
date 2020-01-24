e6de8236d3a09077541070dcb24d2257b940d6c0
Per Nordlöw <per.nordlow@gmail.com>
2020-01-24 09:52:23 +0100

    *** empty log message ***

--- src/nxt/conv_ex.d
+++ src/nxt/conv_ex.d
@@ -29,7 +29,7 @@ T toDefaulted(T)(scope const(char)[] value,
                  const T defaultValue) @safe pure nothrow @nogc
 if (is(T == enum))
 {
-    switch (value)              // instead of slower `std.conv.to`
+    switch (value)              // instead of slower `std.conv.to`:
     {
         static foreach (member; __traits(allMembers, T)) // instead of slower `EnumMembers`
         {

d4ed1977958d8cc4fc27a1e1c933f8270cac846e
Per Nordlöw <per.nordlow@gmail.com>
2020-01-27 10:51:07 +0100

    *** empty log message ***

---
+++ src/nxt/color.d
@@ -0,0 +1,10 @@
+module knet.color;
+
+/** RGB 24-bit color.
+ */
+struct ColorRGB
+{
+    ubyte red;                  ///< Red component.
+    ubyte green;                ///< Green component.
+    ubyte blue;                 ///< Blue component.
+}

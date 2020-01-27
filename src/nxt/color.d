2623f24a8045c9d5af6e1e6df03d5ba00aab69ec
nordlow <per.nordlow@gmail.com>
2020-01-27 13:05:30 +0100

    *** empty log message ***

--- src/nxt/color.d
+++ src/nxt/color.d
@@ -9,11 +9,19 @@ module nxt.color;
 /** RGB 24-bit color, where each color component has 8-bit precision.
  *
  * See_Also: Implements the $(LINK2 https://en.wikipedia.org/wiki/RGB_color_space, RGB) _color type.
- * See_Also: https://github.com/TurkeyMan/color
  */
 struct ColorRGB8
 {
     ubyte red;                  ///< Red component.
     ubyte green;                ///< Green component.
     ubyte blue;                 ///< Blue component.
+}
+
+/** BGR 24-bit color, where each color component has 8-bit precision.
+ */
+struct ColorBGR8
+{
+    ubyte blue;                 ///< Blue component.
+    ubyte green;                ///< Green component.
+    ubyte red;                  ///< Red component.
 }

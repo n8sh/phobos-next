db62f738b9376d43801354dc12a7e195308c8c36
nordlow <per.nordlow@gmail.com>
2020-01-30 14:33:11 +0100

    *** empty log message ***

---
+++ src/nxt/sumo_kif.d
@@ -0,0 +1,42 @@
+/** SU(M)O-KIF file format.
+ *
+ * Used as an encoding for the SUMO ontology.
+ */
+module nxt.suo_kif;
+
+@safe:
+
+bool isFormat(scope const(char)[] chars) pure nothrow @nogc
+{
+    import nxt.array_algorithm : findSkip;
+    if (chars.findSkip('%'))
+    {
+        if (chars.length >= 1)
+        {
+            import std.ascii: isDigit;
+            return isDigit(chars[0]) || chars[0] == '*';
+        }
+    }
+    return false;
+}
+
+@safe pure unittest
+{
+    assert("%1".isFormat);
+    assert(" %1 ".isFormat);
+
+    assert("%2".isFormat);
+    assert(" %2 ".isFormat);
+
+    assert("%*".isFormat);
+    assert(" %* ".isFormat);
+
+    assert(!"%".isFormat);
+    assert(!"% ".isFormat);
+    assert(!" % ".isFormat);
+}
+
+bool isTermFormat(scope const(char)[] chars) pure nothrow @nogc
+{
+    return !isFormat(chars);
+}

fcc5b03aa244b6d66626a7810b3fe26eaae9b426
per_nordlow <per.nordlow@gmail.com>
2020-07-10 23:53:35 +0200

    *** empty log message ***

--- src/nxt/attributes.d
+++ src/nxt/attributes.d
@@ -1,5 +1,6 @@
-/* Attributes.
-   Only make T args const when they have value semantics (allAre!hasIndirections!T).
+/** Attributes.
+ *
+ * Only make T args const when they have value semantics (allAre!hasIndirections!T).
  */
 module nxt.attributes;
 

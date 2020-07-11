91b9e53ec3b1d484fe7404ff04d357cf9a84e0a2
per_nordlow <per.nordlow@gmail.com>
2020-07-11 11:49:39 +0200

    *** empty log message ***

--- src/nxt/color.d
+++ src/nxt/color.d
@@ -19,8 +19,14 @@ struct ColorRGB8
 
 alias Color = ColorRGB8;
 
-static immutable white = Color(0xff, 0xff, 0xff); ///< White.
-static immutable black = Color(0x00, 0x00, 0x00); ///< Black.
+static immutable white   = Color(0xff, 0xff, 0xff); ///< White.
+static immutable black   = Color(0x00, 0x00, 0x00); ///< Black.
+static immutable red     = Color(0xff, 0x00, 0x00); ///< Red.
+static immutable green   = Color(0x00, 0xff, 0x00); ///< Green.
+static immutable blue    = Color(0x00, 0x00, 0xff); ///< Blue.
+static immutable cyan    = Color(0x00, 0xff, 0xff); ///< Cyan.
+static immutable magenta = Color(0xff, 0x00, 0xff); ///< Magenta.
+static immutable yellow  = Color(0xff, 0xff, 0x00); ///< Yellow.
 
 /** BGR 24-bit color, where each color component has 8-bit precision.
  */

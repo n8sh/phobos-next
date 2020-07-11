6d44c8077ebe28d4c93fe4656a9bb3592d592f1f
per_nordlow <per.nordlow@gmail.com>
2020-07-11 11:20:20 +0200

    *** empty log message ***

--- src/nxt/pretty.d
+++ src/nxt/pretty.d
@@ -8,6 +8,9 @@
  * `void toString(scope void delegate(scope const(char)[]) @safe sink)`
  * for independency on arsd.
  *
+ * Rename `pp1` to `ppL` and `pp` to `pp1` where relevant to reduce number of
+ * template instantiations.
+ *
  * TODO Add tester that prints types from `nxt.geometry`
  *
  * TODO Remove all restrictions on pp.*Raw.* and call them using ranges such as repeat

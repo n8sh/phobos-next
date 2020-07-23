f9f8b8a7534a9f8a69679c119ca7876a81335db6
per_nordlow <per.nordlow@gmail.com>
2020-07-23 17:01:39 +0200

    *** empty log message ***

--- src/nxt/dlang_traits.d
+++ src/nxt/dlang_traits.d
@@ -6,10 +6,12 @@ module nxt.dlang_traits;
 
 @safe pure nothrow @nogc:
 
+/// Is `true` if D supports `foreach (const ref e; range)`.
 private enum hasRefForeach = __traits(compiles, {
         mixin(`void f() { int[2] _ = [1, 2]; foreach (const ref e; _) {} }`);
     });
 
+/// Is `true` if D supports `foreach (const auto ref e; range)`.
 private enum hasAutoRefForeach = __traits(compiles, () {
         mixin(`void f() { int[2] _ = [1, 2]; foreach (const auto ref e; _) {} }`);
     });

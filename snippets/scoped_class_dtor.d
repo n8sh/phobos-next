dc64b2d32f21b5c4cf77b9d2e127817edf8bcc46
per_nordlow <per.nordlow@gmail.com>
2020-07-23 20:43:18 +0200

    *** empty log message ***

--- snippets/scoped_class_dtor.d
+++ snippets/scoped_class_dtor.d
@@ -9,14 +9,8 @@ bool g_dtor_called = false;
 class C
 {
 @safe nothrow @nogc:
-    this(int x)
-    {
-        this.x = x;
-    }
-    ~this()
-    {
-        g_dtor_called = true;
-    }
+    this(int x) { this.x = x; }
+    ~this() { g_dtor_called = true; }
     int x;
 }
 

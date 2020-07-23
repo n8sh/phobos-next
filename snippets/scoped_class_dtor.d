3cf8858b07f75d18506fd5825c3d5641d41d89b7
per_nordlow <per.nordlow@gmail.com>
2020-07-23 20:16:35 +0200

    *** empty log message ***

---
+++ snippets/scoped_class_dtor.d
@@ -0,0 +1,29 @@
+bool g_dtor_called = false;
+
+class C
+{
+@safe nothrow @nogc:
+    this(int x)
+    {
+        this.x = x;
+    }
+    ~this()
+    {
+        g_dtor_called = true;
+    }
+    int x;
+}
+
+void scopedC() @safe nothrow
+{
+    scope x = new C(42);
+}
+
+unittest
+{
+    import core.memory : GC;
+    GC.disable();
+    scopedC();
+    assert(g_dtor_called);
+    GC.enable();
+}

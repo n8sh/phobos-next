271808fba14d15534f4ae2065a9311ca3157f8fe
per_nordlow <per.nordlow@gmail.com>
2020-07-26 04:19:56 +0200

    *** empty log message ***

--- benchmarks/containers-cxx/app.cpp
+++ benchmarks/containers-cxx/app.cpp
@@ -18,6 +18,7 @@ using namespace std;
 
 /// Copied from: https://gist.github.com/maddouri/0da889b331d910f35e05ba3b7b9d869b
 /// Alternative solution for C++14: https://medium.com/@LoopPerfect/c-17-vs-c-14-if-constexpr-b518982bb1e2
+/// Alternative solution for C++20: https://brevzin.github.io/c++/2019/01/15/if-constexpr-isnt-broken/
 #define define_has_member(member_name)                                  \
     template <typename T>                                                      \
     class has_member_##member_name                                             \

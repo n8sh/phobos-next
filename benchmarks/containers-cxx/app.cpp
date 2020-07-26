5b43eef6da18d9023aeb577d7dae04934db7a6fe
per_nordlow <per.nordlow@gmail.com>
2020-07-26 14:04:02 +0200

    *** empty log message ***

--- benchmarks/containers-cxx/app.cpp
+++ benchmarks/containers-cxx/app.cpp
@@ -44,7 +44,7 @@ void showTime(const string& tag, const Durs& durs, size_t elementCount, bool okF
     const auto min_dur = *min_element(begin(durs), end(durs));
     const auto dur_ns = cr::duration_cast<cr::nanoseconds>(min_dur).count();
     cout << tag << ": "
-         << (static_cast<double>(dur_ns)) / elementCount << " ns/op"
+         << (static_cast<double>(dur_ns)) / elementCount << "ns"
          << (okFlag ? "" : " ERR")
          << ", ";
 }

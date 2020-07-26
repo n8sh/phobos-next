fcc73f5df9cbe07b90d144ac6a8be4bb0a71088e
per_nordlow <per.nordlow@gmail.com>
2020-07-26 23:20:56 +0200

    *** empty log message ***

--- snippets/walters_stack_trick.d
+++ snippets/walters_stack_trick.d
@@ -3,5 +3,5 @@
     enum n = 100;
     uint[n] x;                  // stack allocated
     const length = n;
-    auto y = x[0 .. length + 1]; // range violation with
+    auto y = x[0 .. length + 1]; // range violation
 }

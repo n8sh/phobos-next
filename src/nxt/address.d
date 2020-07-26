a7ba56b608cb25ed72a13b435c6654234be2604d
per_nordlow <per.nordlow@gmail.com>
2020-07-26 23:04:18 +0200

    *** empty log message ***

--- src/nxt/address.d
+++ src/nxt/address.d
@@ -9,9 +9,9 @@ module nxt.address;
  *
  * TODO is `holeValue` suitably chosen?
  */
-struct AlignedAddress(uint alignment_ = 1) // TODO adjust alignment here
+struct ByteAlignedAddress(uint byteAlignment_) // TODO adjust alignment here
 {
-    enum alignement = alignment_; ///< Alignment in bytes.
+    enum byteAlignment = byteAlignment_; ///< Alignment in bytes.
 
     /// Null value.
     static immutable typeof(this) nullValue = typeof(this).init;
@@ -19,13 +19,45 @@ struct AlignedAddress(uint alignment_ = 1) // TODO adjust alignment here
     /// Hole/Deletion value. Prevent hole bitmap from being used.
     static immutable typeof(this) holeValue = typeof(this)(_ptrValue.max);
 
+    /** Get hash of `this`, with extra fast computation for the small case.
+     */
+    @property hash_t toHash() const scope @trusted
+    {
+        version(LDC) pragma(inline, true);
+        // TODO cativate import nxt.hash_functions : lemireHash64;
+        import core.internal.hash : hashOf;
+        static if (byteAlignment == 1)
+        {
+            return _ptrValue;   // as is
+        }
+        else static if (byteAlignment == 2)
+        {
+            assert((_ptrValue & 0x1) == 0); // 1 least significant bit cleared
+            return _ptrValue >> 1;
+        }
+        else static if (byteAlignment == 4)
+        {
+            assert((_ptrValue & 0x3) == 0); // 2 least significant bits cleared
+            return _ptrValue >> 2;
+        }
+        else static if (byteAlignment == 8)
+        {
+            assert((_ptrValue & 0x7) == 0); // 3 least significant bits cleared
+            return _ptrValue >> 3;
+        }
+        else
+        {
+            static assert(0, "Unsupported byteAlignment");
+        }
+    }
+
     size_t _ptrValue;           ///< Actual pointer value.
     alias _ptrValue this;
 }
-alias Address1 = AlignedAddress!(1);
-alias Address2 = AlignedAddress!(2);
-alias Address4 = AlignedAddress!(4);
-alias Address8 = AlignedAddress!(8);
+alias Address1 = ByteAlignedAddress!(1);
+alias Address2 = ByteAlignedAddress!(2);
+alias Address4 = ByteAlignedAddress!(4);
+alias Address8 = ByteAlignedAddress!(8);
 
 alias Address = Address1;
 

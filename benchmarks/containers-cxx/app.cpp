f6465101e14f14af80144cd8e6334eafb420391b
per_nordlow <per.nordlow@gmail.com>
2020-07-26 04:02:53 +0200

    *** empty log message ***

--- benchmarks/containers-cxx/app.cpp
+++ benchmarks/containers-cxx/app.cpp
@@ -16,6 +16,26 @@
 
 using namespace std;
 
+#define define_has_member(member_name)                                  \
+    template <typename T>                                                      \
+    class has_member_##member_name                                             \
+    {                                                                          \
+        typedef char yes_type;                                                 \
+        typedef long no_type;                                                  \
+        template <typename U> static yes_type test(decltype(&U::member_name)); \
+        template <typename U> static no_type  test(...);                       \
+    public:                                                                    \
+        static constexpr bool value = sizeof(test<T>(0)) == sizeof(yes_type);  \
+    }
+
+define_has_member(reserve);
+
+/// Shorthand for testing if "class_" has a member called "member_name"
+///
+/// @note "define_has_member(member_name)" must be used
+///       before calling "has_member(class_, member_name)"
+#define has_member(class_, member_name)  has_member_##member_name<class_>::value
+
 // save some typing
 namespace cr = chrono;
 
@@ -86,12 +106,43 @@ void benchmarkUnorderedSet(size_t elementCount)
     x.clear();
 }
 
+template<class Set>
+void benchmarkOrderedSet(size_t elementCount)
+{
+    showHeader<Set>();
+    Set x;
+
+    auto beg = Clock::now();
+    for (size_t i = 0; i < elementCount; ++i)
+    {
+        x.insert(i);
+    }
+    auto end = Clock::now();
+    showTime("insert", end - beg, elementCount, true);
+
+    bool allHit = true;
+    beg = Clock::now();
+    for (size_t i = 0; i < elementCount; ++i)
+    {
+        const auto hit = x.find(i);
+        if (hit == x.end()) { allHit = false; }
+    }
+    end = Clock::now();
+
+    showTime("find", end - beg, elementCount, allHit);
+    cout << endl << endl;
+    x.clear();
+}
+
 template<class Map>
 void benchmarkUnorderedMap(size_t elementCount)
 {
     showHeader<Map>();
     Map x;
-    x.reserve(elementCount);
+    if constexpr (has_member(Map, reserve))
+    {
+        x.reserve(elementCount);
+    }
 
     auto beg = Clock::now();
     for (size_t i = 0; i < elementCount; ++i)
@@ -136,7 +187,7 @@ int main(__attribute__((unused)) int argc,
     benchmarkUnorderedSet<std::unordered_set<E>>(elementCount);
 
     cout << "# Ordered Sets:" << endl;
-    // benchmarkSet<std::set<E>>(elementCount);
+    benchmarkOrderedSet<std::set<E>>(elementCount);
 
     cout << "# Unordered Maps:" << endl;
     benchmarkUnorderedMap<ska::flat_hash_map<E, E>>(elementCount);

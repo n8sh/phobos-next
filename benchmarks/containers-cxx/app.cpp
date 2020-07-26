cd95ff148002d2177a3e75ce686fecff42eab6ad
per_nordlow <per.nordlow@gmail.com>
2020-07-26 04:14:27 +0200

    *** empty log message ***

--- benchmarks/containers-cxx/app.cpp
+++ benchmarks/containers-cxx/app.cpp
@@ -17,6 +17,7 @@
 using namespace std;
 
 /// Copied from: https://gist.github.com/maddouri/0da889b331d910f35e05ba3b7b9d869b
+/// Alternative solution for C++14: https://medium.com/@LoopPerfect/c-17-vs-c-14-if-constexpr-b518982bb1e2
 #define define_has_member(member_name)                                  \
     template <typename T>                                                      \
     class has_member_##member_name                                             \

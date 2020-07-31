6309ec89167086af7e7305f7ee3fa61e64f82178
per_nordlow <per.nordlow@gmail.com>
2020-07-31 09:45:45 +0200

    *** empty log message ***

--- snippets/test_trace.d
+++ snippets/test_trace.d
@@ -2,7 +2,7 @@ enum bool isIntegral(T) = IntegralTypeOf!T && !isAggregateType!T;
 enum isAggregateType(T) = true;
 alias IntegralTypeOf(T) = func!T;
 
-int func (T) ()
+int func(T)()
 {
     static assert(0);
 }

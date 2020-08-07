2a1dc76c14323264a5f8e951806d6f07f1e23a8a
per_nordlow <per.nordlow@gmail.com>
2020-08-08 00:58:54 +0200

    *** empty log message ***

--- src/nxt/maxsize_trait.d
+++ src/nxt/maxsize_trait.d
@@ -6,6 +6,8 @@
  */
 module nxt.maxsize_trait;
 
+// version = benchmark;
+
 /** Get maximum size of types `Ts`.
  *
  * Limitation compared to `std.variant.maxSize`: `Ts` cannot contain `void`.
@@ -80,4 +82,37 @@ template maxSizeOf_2(Ts...)
     static assert(maxSizeOf_2!(byte, short, int, string) == 16);
     static assert(maxSizeOf_2!(byte, void) == 1);
     static assert(maxSizeOf_2!(byte, short, void) == 2);
+}
+
+struct W(T, size_t n)
+{
+    T value;
+}
+
+version(benchmark)
+void benchmark()
+{
+    import std.meta : AliasSeq;
+    import std.traits : isCopyable;
+    alias Ts(uint n) = AliasSeq!(W!(byte, n), W!(ubyte, n),
+                                 W!(short, n), W!(ushort, n),
+                                 W!(int, n), W!(uint, n),
+                                 W!(long, n), W!(ulong, n),
+                                 W!(float, n), W!(cfloat, n),
+                                 W!(double, n), W!(cdouble, n),
+                                 W!(real, n), W!(creal, n),
+                                 W!(string, n), W!(wstring, n), W!(dstring, n));
+
+    enum n = 100;
+    enum m = 100;
+    static foreach (i; 0 .. n)
+    {
+        foreach (T; Ts!(n))
+        {
+            static foreach (j; 0 .. m)
+            {
+                static assert(maxSizeOf!(T));
+            }
+        }
+    }
 }

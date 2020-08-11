7d79cf67814b5853acaa74f7d238d41ff176cdba
per_nordlow <per.nordlow@gmail.com>
2020-08-11 23:45:57 +0200

    *** empty log message ***

--- src/nxt/dbgio.d
+++ src/nxt/dbgio.d
@@ -56,7 +56,7 @@ void dbg(Args...)(Args args,
 {
     import std.stdio : stderr, writeln;
     try
-        debug stderr.writeln(file, "(", line, ",1):", " Info: Debug: \"", args, "\"");
+        debug stderr.writeln(file, "(", line, ",1):", " Debug: \"", args, "\"");
     catch (Exception) { }
 }
 

89996aef4e7ed01f20c08ee571824d0ad676075e
per_nordlow <per.nordlow@gmail.com>
2020-08-12 22:18:52 +0200

    *** empty log message ***

--- snippets/self_assign.d
+++ snippets/self_assign.d
@@ -1,23 +1,17 @@
-@trusted pure:
-
-unittest
+pure unittest
 {
     int x;
     x = x;                      // diagnose
-
     int y;
     y = x;                      // no diagnose
-
     y = 32;                     // no diagnose
-
     int* xp;
     xp = xp;                    // diagnose
-
     *(&x) = *(&x);              // should diagnose
 }
 
 int x;
-void test()
+void test() @safe nothrow @nogc
 {
     int x = x;                  // shouldn't this give a shadowing warning?
 }

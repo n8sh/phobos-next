dd24402864a94945801690fd7ec356fe9548d091
per_nordlow <per.nordlow@gmail.com>
2020-08-12 22:04:47 +0200

    *** empty log message ***

--- snippets/self_assign.d
+++ snippets/self_assign.d
@@ -1,4 +1,4 @@
-@safe pure unittest
+@trusted pure unittest
 {
     int x;
     x = x;                      // warn
@@ -7,4 +7,15 @@
     y = x;                      // no warn
 
     y = 32;                     // no warn
+
+    *(&x) = *(&x);              // should warn
+
+    int* xp;
+    xp = xp;
+}
+
+int x;
+void test()
+{
+    int x = x;
 }

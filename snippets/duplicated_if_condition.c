42c0ca10c1904343373c21b38aec69018717ca20
per_nordlow <per.nordlow@gmail.com>
2020-08-16 17:11:03 +0200

    *** empty log message ***

--- snippets/duplicated_if_condition.c
+++ snippets/duplicated_if_condition.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 // See_Also: https://stackoverflow.com/questions/37283128/how-to-catch-a-duplicate-like-wduplicated-cond-from-gcc-6
-int main(void) {
+int main(void)
+{
   int a = 5;
-  if (a == 5) {
+  if (a == 5)
+  {
     printf("First condition is true, a: %d\n", a);
-  } else if (a == 5) {
+  }
+  else if (a == 5)
+  {
     printf("Second condition is true, a: %d\n", a);
   }
 }

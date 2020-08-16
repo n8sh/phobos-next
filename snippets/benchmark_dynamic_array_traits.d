7dbd9b5b160dbebf96068dc97d24486e3fbe4b14
per_nordlow <per.nordlow@gmail.com>
2020-08-16 11:09:24 +0200

    *** empty log message ***

--- snippets/benchmark_dynamic_array_traits.d
+++ snippets/benchmark_dynamic_array_traits.d
@@ -1,27 +1,6 @@
 /// See_Also: https://github.com/dlang/dmd/pull/9014#issuecomment-674451700
 /// See_Also: https://github.com/dlang/phobos/pull/7574
 
-import std.traits : isStaticArray, isDynamicArray;
-
-enum C : char[1]
-{
-    a = "a",
-    b = "b",
-}
-
-static assert(isStaticArray!C);
-static assert(!is(D == T[1], T));
-static assert(__traits(isStaticArray, C));
-
-enum D : string
-{
-    a = "a",
-    b = "b",
-}
-
-static assert(isDynamicArray!D);
-static assert(!is(D == T[], T));
-
 import std.meta : AliasSeq;
 
 static foreach (T; AliasSeq!(bool,

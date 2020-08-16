b51042796377ff4b75b75fd085b2f3dc677331a6
per_nordlow <per.nordlow@gmail.com>
2020-08-16 16:56:46 +0200

    *** empty log message ***

---
+++ snippets/duplicated_if_condition.cpp
@@ -0,0 +1,21 @@
+/*! \file duplicated_if_condition.cpp
+ * \brief
+ */
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::hex;
+using std::dec;
+
+int main(int argc,
+         __attribute__((unused)) const char * argv[],
+         __attribute__((unused)) const char * envp[])
+{
+    if (argc > 3)
+        return 0;
+    if (argc > 3)
+        return 0;
+}

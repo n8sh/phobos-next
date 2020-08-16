fe2cf62c30d16a3d33da912dcf24a38021392cc3
per_nordlow <per.nordlow@gmail.com>
2020-08-16 09:55:59 +0200

    *** empty log message ***

--- snippets/benchmark_array_traits.d
+++ snippets/benchmark_array_traits.d
@@ -1,14 +1,22 @@
-import std.traits : isDynamicArray;
+//// See_Also: https://github.com/dlang/phobos/pull/7574
 
-enum E : string
+import std.traits : isStaticArray, isDynamicArray;
+
+enum C : char[1]
 {
     a = "a",
     b = "b",
 }
 
-@safe pure unittest
+static assert(isStaticArray!C);
+static assert(!is(D == T[1], T));
+
+enum D : string
 {
-    static assert(isDynamicArray!E);
-    static assert(!is(E == T[], T));
-    // static assert(__traits(isDynamicArray, E));
+    a = "a",
+    b = "b",
 }
+
+static assert(isDynamicArray!D);
+static assert(!is(D == T[], T));
+// static assert(__traits(isDynamicArray, D));

2ccb63b53476c579db480722dd35b3d328bdfb94
per_nordlow <per.nordlow@gmail.com>
2020-08-18 00:16:39 +0200

    *** empty log message ***

---
+++ src/nxt/align_reorder.d
@@ -0,0 +1,20 @@
+module nxt.align_reorder;
+
+struct S
+{
+    bool x;
+    bool y;
+    int i;
+    bool z;
+}
+
+template realigned(T)
+{
+    alias realigned = typeof(T.tupleof);
+}
+
+@safe pure unittest
+{
+    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", S.sizeof);
+    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", realigned!(S));
+}

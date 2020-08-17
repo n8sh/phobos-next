3da659503d97084068f2660ce48067e51eee18dd
per_nordlow <per.nordlow@gmail.com>
2020-08-17 23:16:03 +0200

    *** empty log message ***

--- snippets/pragma_msg_with_location.d
+++ snippets/pragma_msg_with_location.d
@@ -1,5 +1,6 @@
 @safe pure unittest
 {
     int x;
-    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", "A useful debug message: ", typeof(x));
+    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ",
+           "A useful debug message: ", typeof(x));
 }

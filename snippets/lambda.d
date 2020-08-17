0baa6c82afe9b0123fe01d108e759dbe3a2201f3
per_nordlow <per.nordlow@gmail.com>
2020-08-17 23:51:16 +0200

    *** empty log message ***

--- snippets/lambda.d
+++ snippets/lambda.d
@@ -3,7 +3,7 @@ int main(string[] args)
     auto twice = function (int x) => x * 2;
 
     auto thrice = (int x) { return 3*x; };
-    pragma(msg, typeof(thrice));
+    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", typeof(thrice));
 
     import std.stdio;
     writeln(thrice(3));

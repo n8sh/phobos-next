f70a679573cb0d91e51e12910547930e32be0cb8
per_nordlow <per.nordlow@gmail.com>
2020-08-18 00:33:45 +0200

    *** empty log message ***

--- src/nxt/align_reorder.d
+++ src/nxt/align_reorder.d
@@ -15,8 +15,26 @@ template realigned(T)
     alias realigned = typeof(T.tupleof);
 }
 
+enum alignOf(T) = T.alignof;
+
+template sortBy(alias pred, Ts...)
+{
+    import std.meta : AliasSeq;
+    static if (Ts.length <= 1)
+        alias sortBy = Ts;
+    else
+    {
+        static if (pred!(Ts[0]) <
+                   pred!(Ts[1]))
+            alias sortBy = AliasSeq!(Ts[0], Ts[1], sortBy!(Ts[2 .. $]));
+        else
+            alias sortBy = AliasSeq!(Ts[1], Ts[0], sortBy!(Ts[2 .. $]));
+    }
+}
+
 @safe pure unittest
 {
     pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", S.sizeof);
     pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", realigned!(S));
+    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", sortBy!(alignOf, typeof(S.tupleof)));
 }

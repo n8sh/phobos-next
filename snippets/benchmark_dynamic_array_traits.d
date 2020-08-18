33518dbb68359286b55b421263d077a3000cb6de
per_nordlow <per.nordlow@gmail.com>
2020-08-18 11:49:10 +0200

    *** empty log message ***

--- snippets/benchmark_dynamic_array_traits.d
+++ snippets/benchmark_dynamic_array_traits.d
@@ -3,7 +3,7 @@
 
 import std.meta : AliasSeq;
 
-version = useBuiltin;           ///< Use new builtin trait __traits(isDynamicArray, ...)
+// version = useBuiltin;           ///< Use new builtin trait __traits(isDynamicArray, ...)
 
 private static alias ScalarTypes = AliasSeq!(bool,
                                              char, wchar, dchar,
@@ -28,31 +28,40 @@ static private template isDynamicArray(T)
         enum bool isDynamicArray = false;
 }
 
-static foreach (T; ScalarTypes)
+@safe pure unittest
 {
-    static foreach (U; ScalarTypes)
+    static foreach (T; ScalarTypes)
     {
-        static foreach (V; ScalarTypes)
+        static foreach (U; ScalarTypes)
         {
-            mixin("struct ",
-                  T, "_" ,U, "_" ,V,
-                  " {",
-                  T, " t; ",
-                  U, " u; ",
-                  V, " v; ",
-                  "}");
-            static foreach (qualifier; qualifiers)
+            static foreach (V; ScalarTypes)
             {
+                mixin("struct ",
+                      T, "_" ,U, "_" ,V,
+                      " {",
+                      T, " t; ",
+                      U, " u; ",
+                      V, " v; ",
+                      "}");
+                mixin("enum ",
+                      T, "_", U, "_", V, // type
+                      " ",
+                      "e_", "_", T, "_", U, "_", V, // name
+                      " = ",
+                      T, "_", U, "_", V, ".init",
+                      ";");
                 version(useBuiltin)
-                    static assert(__traits(isDynamicArray, mixin(qualifier, "(", T, "_", U, "_", V, ")")[])); // min over 10 runs: 2.62s.
+                static assert(__traits(isDynamicArray, mixin(T, "_", U, "_", V)[])); // min over 10 runs: 2.62s.
                 else
+                {
                     // static assert(is(mixin(qualifier, "(", T, "_", U, "_", V, ")")[] == X[], X)); // min over 10 runs: 2.75s
-                    static assert(isDynamicArray!(mixin(qualifier, "(", T, "_", U, "_", V, ")")[])); // min over 10 runs: 2.75s
+                    static assert(isDynamicArray!(mixin(T, "_", U, "_", V)[])); // min over 10 runs: 3.19s
+                }
             }
         }
     }
-}
 
+}
 version(none)                   // this is slower than above
 template ctBenchmark(Types = ScalarTypes)
 {

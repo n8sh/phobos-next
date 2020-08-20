2250dfe6e1eeca2a6a276db60731ecb930c7d4eb
per_nordlow <per.nordlow@gmail.com>
2020-08-21 01:32:45 +0200

    *** empty log message ***

--- snippets/unused_test.d
+++ snippets/unused_test.d
@@ -3,10 +3,12 @@ module unused_test;
 import std.range;
 import std.algorithm.iteration : map2 = map;
 import std.algorithm.iteration : filter;
+import std.stdio : writeln;
 import io = std.stdio;
 
 alias UsedInt = int;
-alias UnusedInt = int;
+alias PublicUnusedInt = int;
+private alias PrivateUnusedInt = int;
 
 int x = 42;
 private int px = 42;

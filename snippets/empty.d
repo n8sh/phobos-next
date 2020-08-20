0d003a680cea77d1d10bf0e373f0954faffb83ea
per_nordlow <per.nordlow@gmail.com>
2020-08-20 14:21:49 +0200

    *** empty log message ***

---
+++ snippets/empty.d
@@ -0,0 +1,13 @@
+module unused_test;
+// import std;
+
+@safe pure unittest
+{
+    // string x;
+
+    alias Int = int;
+    Int x;
+
+    // import std.traits : isDA = isDynamicArray;
+    // static assert(isDA!(int[]));
+}

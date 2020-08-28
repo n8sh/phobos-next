71ae13077263c0e32201d3258838ea29770868ce
per_nordlow <per.nordlow@gmail.com>
2020-08-28 11:43:00 +0200

    *** empty log message ***

--- snippets/pragma_printf_samples.d
+++ snippets/pragma_printf_samples.d
@@ -2,13 +2,14 @@ import core.stdc.stdarg;
 
 struct Loc { int x; }
 
-pragma(printf) extern (C++) int warning(in Loc, const(char)*, ...) { return 0; }
+pragma(printf) extern (C) int warningC(in Loc, const(char)*, ...) { return 0; }
+pragma(printf) extern (C++) int warningCxx(in Loc, const(char)*, ...) { return 0; }
 
 unittest
 {
     Loc loc;
-    loc.warning("%d", 32);      // ok
-    loc.warning("%p", null);    // ok
-    loc.warning("%s", "".ptr);  // ok
-    loc.warning("%s", 32);      // warn
+    loc.warningCxx("%d", 32);      // ok
+    loc.warningCxx("%p", null);    // ok
+    loc.warningCxx("%s", "".ptr);  // ok
+    loc.warningCxx("%s", 32);      // warn
 }

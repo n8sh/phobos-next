b3e9afdb04f28ba260cb83e310b362b5a75c1ad3
per_nordlow <per.nordlow@gmail.com>
2020-09-06 19:28:29 +0200

    *** empty log message ***

--- snippets/real_imag_cast.d
+++ snippets/real_imag_cast.d
@@ -1,6 +1,13 @@
 @safe pure unittest
 {
     idouble x = 2.0i;
-    double y = cast(double)x; // y is now 0
+    double y = cast(double)x; // TODO: should warn
+    assert(y == 0);
+}
+
+@safe pure unittest
+{
+    double x = 2.0;
+    idouble y = cast(idouble)x; // TODO: should warn
     assert(y == 0);
 }

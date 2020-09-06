9b38f64dedf38557fd6f5d143f8e78b3ecdd53e8
per_nordlow <per.nordlow@gmail.com>
2020-09-06 21:18:10 +0200

    *** empty log message ***

--- snippets/real_imag_cast.d
+++ snippets/real_imag_cast.d
@@ -1,11 +1,13 @@
-@safe pure unittest
+// Compile with: gdc-10 -Wcast-result real_imag_cast.d
+
+@safe pure test1()
 {
     idouble x = 2.0i;
     double y = cast(double)x; // TODO: should warn
     assert(y == 0);
 }
 
-@safe pure unittest
+@safe pure test2()
 {
     double x = 2.0;
     idouble y = cast(idouble)x; // TODO: should warn

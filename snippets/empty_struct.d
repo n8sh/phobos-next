3b4f16fc3e99729aa8f546616444a5ef86a04548
per_nordlow <per.nordlow@gmail.com>
2020-09-09 00:00:54 +0200

    *** empty log message ***

--- snippets/empty_struct.d
+++ snippets/empty_struct.d
@@ -1,15 +1,19 @@
+/** Empty structs have size zero.
+ *
+ * See_Also: https://dlang.org/spec/struct.html#struct_layout
+ */
+
 struct S
 {
-    bool x;
 }
 
 extern(C) struct CS
 {
-    bool x;
 }
 
 @safe pure unittest
 {
+    static assert(S.tupleof.length == 0);
     pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", S.sizeof);
     pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", CS.sizeof);
 }

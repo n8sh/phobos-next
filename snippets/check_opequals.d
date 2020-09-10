b16a91c6ce1cbb1dd2a827f4f2498bdf8a90f8f5
per_nordlow <per.nordlow@gmail.com>
2020-09-10 23:02:01 +0200

    *** empty log message ***

--- snippets/check_opequals.d
+++ snippets/check_opequals.d
@@ -9,16 +9,12 @@ struct S
 
 struct T
 {
-    @property bool opEquals(const scope typeof(this) rhs) const
-    {
-        return x == rhs.x;
-    }
     int x;
 }
 
 int main(string[])
 {
     static assert(__traits(hasMember, S, "opEquals"));
-    static assert(__traits(hasMember, T, "opEquals"));
+    static assert(!__traits(hasMember, T, "opEquals"));
     return 0;
 }

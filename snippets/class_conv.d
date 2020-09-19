5018a384d33267dcfa30b1ec569244fffbfa6b01
per_nordlow <per.nordlow@gmail.com>
2020-09-19 21:58:44 +0200

    *** empty log message ***

--- snippets/class_conv.d
+++ snippets/class_conv.d
@@ -4,21 +4,21 @@ class Derived : Base {}
 // classes
 @safe pure unittest
 {
-    Base bd = new Derived();              // implicit conversion
-    Derived db = cast(Derived)new Base(); // explicit conversion
+    Base _bd = new Derived();              // implicit conversion
+    Derived _db = cast(Derived)new Base(); // explicit conversion
 }
 
 // dynamic array of `const` or `immutable` class elements
 @safe pure unittest
 {
-    const(Base)[] ca = (const(Derived)[]).init;
-    immutable(Base)[] ia = (immutable(Derived)[]).init;
+    const(Base)[] _ca = (const(Derived)[]).init;
+    immutable(Base)[] _ia = (immutable(Derived)[]).init;
 }
 
 // static array of mutable, `const` or `immutable` class elements
 @safe pure unittest
 {
-    const(Base)[3] ca = (const(Derived)[3]).init;
-    immutable(Base)[3] ia = (immutable(Derived)[3]).init;
-    Base[3] ma = (Derived[3]).init;
+    const(Base)[3] _ca = (const(Derived)[3]).init;
+    immutable(Base)[3] _ia = (immutable(Derived)[3]).init;
+    Base[3] _ma = (Derived[3]).init;
 }

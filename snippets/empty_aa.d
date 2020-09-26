c5c34ed1197691ee57a503af6509121dcb6a086d
per_nordlow <per.nordlow@gmail.com>
2020-09-26 21:22:07 +0200

    *** empty log message ***

--- snippets/empty_aa.d
+++ snippets/empty_aa.d
@@ -2,8 +2,12 @@
 {
     alias A = int[int];
     A x;
+    x[0] = 0;
+    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", A.sizeof);
+    assert(x is null);
     assert(!x);
     assert(1 !in x);
     x[31] = 13;
+    assert(x !is null);
     assert(x);
 }

3ccbbf9783df18cd6e17f5c6bf4394500a742984
per_nordlow <per.nordlow@gmail.com>
2020-10-12 11:27:55 +0200

    Use case-range

--- src/nxt/antlr.d
+++ src/nxt/antlr.d
@@ -677,14 +677,7 @@ private:
                     error("unexpected character");
                 break;
             case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
+                ..
             case '9':
                 _token = Token(TOK.number, getNumber());
                 break;

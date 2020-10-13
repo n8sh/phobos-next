1c566599aa38f0200b9204c5447217467df24b13
per_nordlow <per.nordlow@gmail.com>
2020-10-13 23:20:58 +0200

    *** empty log message ***

--- src/nxt/antlr.d
+++ src/nxt/antlr.d
@@ -985,7 +985,7 @@ struct G4Parser
                 parserFlag = true;
                 _lexer.popFront();
                 if (_lexer.front.tok != TOK.GRAMMAR)
-                    _lexer.error("expected `grammar` after `parser`");
+                    _lexer.error("`parser` must be followed by `grammar`");
             }
 
             const token = _lexer.frontPop;

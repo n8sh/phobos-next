55114deeefcc2e73dd15e68f8f2f593f45d6f895
per_nordlow <per.nordlow@gmail.com>
2020-10-23 10:38:11 +0200

    *** empty log message ***

--- src/nxt/gxbnf.d
+++ src/nxt/gxbnf.d
@@ -37,7 +37,7 @@
  *
  * - Emacs click on link in `compilation-mode` doesn't navigate to correct offset on lines containing tabs before offset
  *
- * - Avoid casts by compared with `head.tok` for `isA!NodeType`
+ * - Avoid casts and instead compare against `head.tok` for `isA!NodeType`
  */
 module nxt.gxbnf;
 

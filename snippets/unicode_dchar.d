6dbb690728d94ee75f9e6e5916a18b6f6ebc97fa
per_nordlow <per.nordlow@gmail.com>
2020-11-08 12:18:50 +0100

    *** empty log message ***

--- snippets/unicode_dchar.d
+++ snippets/unicode_dchar.d
@@ -1,6 +1,6 @@
 @safe pure unittest
 {
-    const dch1 = cast(dchar)0x000000a0;
-    const dch2 = '\u00a0';
+    const dch1 = cast(dchar)0x0000a0a0;
+    const dch2 = '\ua0a0';
     assert(dch1 == dch2);
 }

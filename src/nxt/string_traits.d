0621bf432aa735f4ea928a25ffd2808689c34118
per_nordlow <per.nordlow@gmail.com>
2020-11-12 15:01:07 +0100

    *** empty log message ***

--- src/nxt/string_traits.d
+++ src/nxt/string_traits.d
@@ -1,13 +1,13 @@
 module nxt.string_traits;
 
-@safe pure nothrow @nogc:
+@safe
 
 /** Returns: `true` iff `x` is an ASCII (7-bit clean) set of `char`s.
  *
  * See_Also: `std.ascii.isASCII`.
  * See_Also: https://forum.dlang.org/post/syaqzkpybhvdehbhffjn@forum.dlang.org
  */
-bool isASCIIString(scope const(char)[] input)
+bool isASCIIString(scope const(char)[] input) pure nothrow @nogc
 {
     foreach (e; cast(const(ubyte)[])input) // no decoding to `dchar` needed
         if (e >= 0x7F)

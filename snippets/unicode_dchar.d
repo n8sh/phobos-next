6e7ee8c49936f1856c904a60e728ab24eac8771a
per_nordlow <per.nordlow@gmail.com>
2020-11-12 15:11:45 +0100

    *** empty log message ***

--- snippets/unicode_dchar.d
+++ snippets/unicode_dchar.d
@@ -1,6 +1,9 @@
+/// See_Also: https://forum.dlang.org/post/bmzybbvdelgspizwfcmq@forum.dlang.org
 @safe pure unittest
 {
+    enum dch0 = dchar(0xa0a0);
     enum dch1 = cast(dchar)0xa0a0;
     enum dch2 = '\ua0a0';
+    assert(dch0 == dch1);
     assert(dch1 == dch2);
 }
